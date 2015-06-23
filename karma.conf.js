568701479540b352196282639bc9877929bc130e
Joey Parrish <joeyparrish@google.com>
2015-06-23 14:37:53 -0700

    Add Karma config
    This should make testing in Karma easier, and in particular, easier
    for use in automated systems.  We will not package Karma into
    third_party at this time, but may reevaluate that decision later.
    
    Change-Id: Idf407c6ec0b3b876284768f2bc37e0edd032c32e

---
+++ karma.conf.js
@@ -0,0 +1,57 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '.',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine-ajax', 'jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      // closure base
+      'third_party/closure/goog/base.js',
+
+      // included files - tests
+      'spec/*.js',
+
+      // source files - these are only watched and served
+      {pattern: 'lib/*/*.js', included: false},
+      {pattern: 'third_party/closure/goog/*/*.js', included: false},
+
+      // additional assets
+      {pattern: 'assets/*', included: false},
+
+      // closure's generated deps file
+      {pattern: 'third_party/closure/goog/deps.js', included: false}
+    ],
+
+    // handle requests for /assets
+    proxies: {
+      '/assets/': '/base/assets/'
+    },
+
+    // do not panic about "no activity" unless a test takes longer than 30s.
+    browserNoActivityTimeout: 30000,
+
+    // don't capture the client's console logs
+    client: { captureConsole: false },
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    //                  config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_WARN,
+
+    // do not execute tests whenever any file changes
+    autoWatch: false,
+  });
+};
