1a9febc9d17aa71e91a748dc4d63f4aa08d7ac47
Joey Parrish <joeyparrish@google.com>
2015-07-13 12:06:22 -0700

    Reverse nullability of BufferSource
    This brings our externs in line with newer versions of the Closure
    Compiler, where BufferSource is both defined and defaults to
    non-nullable.
    
    b/21559591
    
    Change-Id: I5d51043e54b1b3490c25c4bb362025fa51260c07

--- externs/mediakeys.js
+++ externs/mediakeys.js
@@ -19,7 +19,7 @@
  */
 
 
-/** @typedef {ArrayBufferView|ArrayBuffer} */
+/** @typedef {!ArrayBufferView|!ArrayBuffer} */
 var BufferSource;
 
 
@@ -88,7 +88,7 @@ MediaKeys.prototype.createSession = function(opt_sessionType) {};
 
 
 /**
- * @param {BufferSource} serverCertificate
+ * @param {?BufferSource} serverCertificate
  * @return {!Promise}
  */
 MediaKeys.prototype.setServerCertificate = function(serverCertificate) {};
@@ -107,7 +107,7 @@ MediaKeyStatusMap.prototype.size;
 
 /**
  * Array entry 0 is the key, 1 is the value.
- * @return {Iterator.<Array.<!BufferSource|string>>}
+ * @return {Iterator.<Array.<BufferSource|string>>}
  */
 MediaKeyStatusMap.prototype.entries = function() {};
 
@@ -120,21 +120,21 @@ MediaKeyStatusMap.prototype.forEach = function(fn) {};
 
 
 /**
- * @param {!BufferSource} keyId
+ * @param {BufferSource} keyId
  * @return {string|undefined}
  */
 MediaKeyStatusMap.prototype.get = function(keyId) {};
 
 
 /**
- * @param {!BufferSource} keyId
+ * @param {BufferSource} keyId
  * @return {boolean}
  */
 MediaKeyStatusMap.prototype.has = function(keyId) {};
 
 
 /**
- * @return {Iterator.<!BufferSource>}
+ * @return {Iterator.<BufferSource>}
  */
 MediaKeyStatusMap.prototype.keys = function() {};
 
@@ -171,7 +171,7 @@ MediaKeySession.prototype.keyStatuses;
 
 /**
  * @param {string} initDataType
- * @param {BufferSource} initData
+ * @param {?BufferSource} initData
  * @return {!Promise}
  */
 MediaKeySession.prototype.generateRequest = function(initDataType, initData) {};
@@ -185,7 +185,7 @@ MediaKeySession.prototype.load = function(sessionId) {};
 
 
 /**
- * @param {BufferSource} response
+ * @param {?BufferSource} response
  * @return {!Promise}
  */
 MediaKeySession.prototype.update = function(response) {};
