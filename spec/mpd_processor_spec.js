shaka-project/shaka-player

commit 689c2a47aa31f99b3d2a6da9631cb7c8a0111a5b

Use 'keystatuseschange' events and support @group.
Use 'keystatuseschange' events from EME together with cenc:default_KID
from the MPD so StreamVideoSource can determine if the key system
can/will decrypt a stream before it attempts to play it. This enables
the Player to safely (and automatically) switch between streams that
are encrypted with different keys.

Support AdaptationSet @group, which is the preferred approach to safely
use multiple encryption keys over multiple Representations.

* Squash AdaptationSets from the same group into one StreamSetInfo.
  This enables support for @group without introducing special case code
  into StreamVideoSource and EmeManager.
* Fire 'trackchanged' events when tracks becomes available/unavailable
  either from application restrictions or key status changes.
* Pull key IDs out of DrmInfo and put them into StreamInfo so key status
  changes can drive per stream availability instead of per StreamSet
  (AdaptationSet) availability.

Closes #67
Issue #160

Change-Id: Ife0814deb65715923a572b45880137a99b378035

lib/dash/mpd_processor.js
@@ -349,51 +349,35 @@ shaka.dash.MpdProcessor.prototype.createManifestInfo_ = function(
 
     var periodInfo = new shaka.media.PeriodInfo();
     periodInfo.id = period.id;
-
-    shaka.asserts.assert(period.start != null);
-    periodInfo.start = period.start || 0;
+    periodInfo.start = period.start;
     periodInfo.duration = period.duration;
 
-    for (var j = 0; j < period.adaptationSets.length; ++j) {
-      var adaptationSet = period.adaptationSets[j];
-      shaka.asserts.assert(adaptationSet.group != null);
+    // First group AdaptationSets by type.
+    var setsByType = new shaka.util.MultiMap();
+    period.adaptationSets.forEach(
+        function(set) { setsByType.push(set.contentType || '', set); });
 
-      var streamSetInfo = new shaka.media.StreamSetInfo();
-      streamSetInfo.id = adaptationSet.id;
-      streamSetInfo.group = /** @type {number} */(adaptationSet.group);
-      streamSetInfo.lang = adaptationSet.lang || '';
-      streamSetInfo.contentType = adaptationSet.contentType || '';
-      streamSetInfo.main = adaptationSet.main;
+    var setTypes = setsByType.keys();
+    for (var typeIdx = 0; typeIdx < setTypes.length; ++typeIdx) {
+      var type = setTypes[typeIdx];
 
-      for (var k = 0; k < adaptationSet.representations.length; ++k) {
-        var representation = adaptationSet.representations[k];
+      // Now group AdaptationSets of the same type by group to squash multiple
+      // AdaptationSets into a single StreamSetInfo.
+      var setsByGroup = new shaka.util.MultiMap();
+      setsByType.get(type).forEach(
+          function(set) { setsByGroup.push(set.group, set); });
 
-        // Get common DRM infos.
-        var commonDrmInfos = streamSetInfo.drmInfos.slice(0);
-        this.updateCommonDrmInfos_(representation, commonDrmInfos);
-        if (commonDrmInfos.length == 0 &&
-            streamSetInfo.drmInfos.length > 0) {
-          shaka.log.warning(
-              'Representation does not contain any ContentProtection elements',
-              'that are compatible with other Representations within its',
-              'AdaptationSet.',
-              representation);
-          continue;
-        }
+      var setGroups = setsByGroup.keys();
+      for (var groupIdx = 0; groupIdx < setGroups.length; ++groupIdx) {
+        var group = setGroups[groupIdx];
 
-        var streamInfo = this.createStreamInfo_(
-            mpd, period, representation, manifestCreationTime, networkCallback);
-        if (!streamInfo) {
-          // An error has already been logged.
-          continue;
-        }
-
-        streamSetInfo.streamInfos.push(streamInfo);
-        streamSetInfo.drmInfos = commonDrmInfos;
-      }  // for k
-
-      periodInfo.streamSetInfos.push(streamSetInfo);
-    }  // for j
+        var sets = /** @type {!Array.<shaka.dash.mpd.AdaptationSet>} */(
+            setsByGroup.get(group));
+        var streamSetInfo = this.createStreamSetInfo_(
+            mpd, period, sets, manifestCreationTime, networkCallback);
+        periodInfo.streamSetInfos.push(streamSetInfo);
+      }
+    }  // for typeIdx
 
     manifestInfo.periodInfos.push(periodInfo);
   }  // for i
@@ -403,26 +387,179 @@ shaka.dash.MpdProcessor.prototype.createManifestInfo_ = function(
 
 
 /**
- * Updates |commonDrmInfos|.
+ * Creates a StreamSetInfo from AdaptationSets from the same group.
  *
- * If |commonDrmInfos| is empty then after this function is called
- * |commonDrmInfos| will equal |representation|'s application provided
- * DrmInfos.
+ * @param {!shaka.dash.mpd.Mpd} mpd
+ * @param {!shaka.dash.mpd.Period} period
+ * @param {Array.<!shaka.dash.mpd.AdaptationSet>} adaptationSets
+ *     AdaptationSets from the same group.
+ * @param {number} manifestCreationTime The time, in seconds, when the manifest
+ *     was created.
+ * @param {shaka.util.FailoverUri.NetworkCallback} networkCallback
+ * @return {!shaka.media.StreamSetInfo}
+ * @private
+ */
+shaka.dash.MpdProcessor.prototype.createStreamSetInfo_ = function(
+    mpd, period, adaptationSets, manifestCreationTime, networkCallback) {
+  shaka.asserts.assert(adaptationSets.length > 0);
+
+  var streamSetInfo = new shaka.media.StreamSetInfo();
+
+  // Construct a StreamSetInfo ID from the AdaptationSet IDs. Consider the
+  // entire StreamSetInfo unidentifiable if an AdaptationSet (in the group)
+  // does not have an ID. StreamSetInfo IDs must remain identical between
+  // manifest updates, see shaka.media.ManifestUpdater.
+  var identifiable = adaptationSets
+      .filter(function(set) { return set.id != null; });
+  if (identifiable.length == adaptationSets.length) {
+    streamSetInfo.id = identifiable
+        .map(function(set) { return set.id; })
+        .sort()
+        .reduce(function(all, part) { return all + ',' + part; });
+  }
+
+  streamSetInfo.lang = adaptationSets[0].lang || '';
+  streamSetInfo.contentType = adaptationSets[0].contentType || '';
+
+  // Maps each StreamInfo to its AdaptationSet, which we use below to enable
+  // certain StreamInfos.
+  var streamInfoUidToAdaptationSet = {};
+
+  for (var i = 0; i < adaptationSets.length; ++i) {
+    var adaptationSet = adaptationSets[i];
+
+    if ((adaptationSet.lang || '') != streamSetInfo.lang) {
+      shaka.log.warning(
+          'AdaptationSet groups should have the same language.',
+          adaptationSets);
+    }
+
+    if ((adaptationSet.contentType || '') != streamSetInfo.contentType) {
+      shaka.log.error(
+          'AdaptationSet groups must have the same content type:',
+          'ignoring AdaptationSet.',
+          adaptationSet);
+      continue;
+    }
+
+    for (var j = 0; j < adaptationSet.representations.length; ++j) {
+      var representation = adaptationSet.representations[j];
+      var drmInfos = this.getDrmInfos_(representation);
+
+      // Update the common DrmInfos.
+      var commonDrmInfos = streamSetInfo.drmInfos.slice(0);
+      this.updateCommonDrmInfos_(drmInfos, commonDrmInfos);
+      if (commonDrmInfos.length == 0 && streamSetInfo.drmInfos.length > 0) {
+        shaka.log.warning(
+            'Representation does not contain any ContentProtection elements',
+            'that are compatible with other Representations within its',
+            'AdaptationSet or AdaptationSet group.',
+            representation);
+        continue;
+      }
+
+      var streamInfo = this.createStreamInfo_(
+          mpd, period, representation, manifestCreationTime, networkCallback);
+      if (!streamInfo) {
+        // An error has already been logged.
+        continue;
+      }
+
+      streamSetInfo.streamInfos.push(streamInfo);
+      streamSetInfo.drmInfos = commonDrmInfos;
+
+      streamInfoUidToAdaptationSet[streamInfo.uniqueId] = adaptationSet;
+    }  // for j
+  }  // for i
+
+  // If the streams are unencrypted then we don't require a key system.
+  var unencrypted = streamSetInfo.drmInfos.some(
+      function(drmInfo) { return drmInfo.keySystem == ''; });
+  if (unencrypted) {
+    streamSetInfo.streamInfos.forEach(
+        function(streamInfo) { streamInfo.allowedByKeySystem = true; });
+    return streamSetInfo;
+  }
+
+  // If the streams are encrypted then assume the key system can probably
+  // decrypt the lowest quality streams.  This enables the VideoSource to
+  // select at least one stream for stream startup without having to query the
+  // key system first.
+  //
+  // The lowest quality streams are the streams that are contained within
+  // the AdaptationSet which contains THE lowest quality stream.
+  //
+  // For example, given an MPD that looks like
+  // <AdaptationSet id="A1" group="1">
+  //   <Representation id="R1" width="360" height="240" />
+  //   <Representation id="R2" width="640" height="480" />
+  // </AdaptationSet>
+  // <AdaptationSet id="A2' group="1">
+  //   <ContentProtection cenc:default_KID="01234567890ABCDEF" />
+  //   <Representation id="R3" width="1280" height="720" />
+  //   <Representation id="R4" width="1920" height="1080" />
+  // </AdaptationSet>
+  // the lowest quality streams are R1 and R2.
+  var lowestQualitySet = this.findLowestQualityAdaptationSet_(adaptationSets);
+
+  for (var i = 0; i < streamSetInfo.streamInfos.length; ++i) {
+    var streamInfo = streamSetInfo.streamInfos[i];
+    var adaptationSet = streamInfoUidToAdaptationSet[streamInfo.uniqueId];
+    if (adaptationSet == lowestQualitySet) {
+      shaka.log.v1(
+          'Assuming the key system can decrypt stream', streamInfo.id + '.');
+      streamInfo.allowedByKeySystem = true;
+    }
+  }
+
+  return streamSetInfo;
+};
+
+
+/**
+ * Returns the AdaptationSet that contains the lowest quality Representation.
  *
- * Otherwise, if |commonDrmInfos| is non-empty then after this function is
- * called |commonDrmInfos| will equal the intersection between
- * |representation|'s application provided DrmInfos and |commonDrmInfos| at the
- * time this function was called.
+ * @param {!Array.<!shaka.dash.mpd.AdaptationSet>} adaptationSets
+ * @return {shaka.dash.mpd.AdaptationSet}
+ * @private
+ */
+shaka.dash.MpdProcessor.prototype.findLowestQualityAdaptationSet_ = function(
+    adaptationSets) {
+  var lowestQuality = null;
+  var lowestQualitySet = null;
+
+  for (var i = 0; i < adaptationSets.length; ++i) {
+    var adaptationSet = adaptationSets[i];
+
+    for (var j = 0; j < adaptationSet.representations.length; ++j) {
+      var representation = adaptationSet.representations[j];
+
+      var quality = (representation.width || 1) *
+                    (representation.height || 1) *
+                    (representation.bandwidth || 1);
+      if ((lowestQuality == null) || (quality < lowestQuality)) {
+        lowestQuality = quality;
+        lowestQualitySet = adaptationSet;
+      }
+    }
+  }
+
+  return lowestQualitySet;
+};
+
+
+/**
+ * Sets |commonDrmInfos| to |drmInfos| if |commonDrmInfos| is empty; otherwise,
+ * sets |commonDrmInfos| to the intersection between |commonDrmInfos| and
+ * |drmInfos|.
  *
- * @param {!shaka.dash.mpd.Representation} representation
+ * @param {!Array.<!shaka.player.DrmInfo>} drmInfos
  * @param {!Array.<!shaka.player.DrmInfo>} commonDrmInfos
  *
  * @private
  */
 shaka.dash.MpdProcessor.prototype.updateCommonDrmInfos_ = function(
-    representation, commonDrmInfos) {
-  var drmInfos = this.getDrmInfos_(representation);
-
+    drmInfos, commonDrmInfos) {
   if (commonDrmInfos.length == 0) {
     Array.prototype.push.apply(commonDrmInfos, drmInfos);
     return;
@@ -453,8 +590,7 @@ shaka.dash.MpdProcessor.prototype.updateCommonDrmInfos_ = function(
  *     is used for unencrypted content.
  * @private
  */
-shaka.dash.MpdProcessor.prototype.getDrmInfos_ =
-    function(representation) {
+shaka.dash.MpdProcessor.prototype.getDrmInfos_ = function(representation) {
   var drmInfos = [];
   if (representation.contentProtections.length == 0) {
     // Return a single item which indicates that the content is unencrypted.
@@ -506,10 +642,6 @@ shaka.dash.MpdProcessor.prototype.createDrmInfos_ = function(
           'initDataType': 'cenc'
         };
         drmInfo.addInitDatas([initData]);
-      }
-
-      if (contentProtection.defaultKeyId) {
-        drmInfo.addKeyIds([contentProtection.defaultKeyId]);
       }
 
       drmInfos.push(drmInfo);
@@ -612,6 +744,15 @@ shaka.dash.MpdProcessor.prototype.createStreamInfo_ = function(
   streamInfo.height = representation.height;
   streamInfo.mimeType = representation.mimeType || '';
   streamInfo.codecs = representation.codecs || '';
+
+  for (var i = 0; i < representation.contentProtections.length; ++i) {
+    // Since we don't know which key system we will use yet, map each
+    // cenc:default_KID that the manifest specifies.
+    var contentProtection = representation.contentProtections[i];
+    if (contentProtection.defaultKeyId) {
+      streamInfo.keyIds.push(contentProtection.defaultKeyId);
+    }
+  }
 
   return streamInfo;
 };

app.js
@@ -928,6 +928,7 @@ app.initPlayer_ = function() {
       playerControls.onBuffering.bind(null, false));
   app.player_.addEventListener('seekrangechanged',
       playerControls.onSeekRangeChanged);
+  app.player_.addEventListener('trackschanged', app.displayMetadata_);
 
   app.estimator_ = new shaka.util.EWMABandwidthEstimator();
   playerControls.setPlayer(app.player_);

commit 26bfade2dff58132937af0f9168de8f73c0d7220

Fix multiple audio language support.
* Ensure AdaptationSets with different languages but with the same group
  end up in different StreamSetInfos.
* Set the main flag in a StreamSetInfo if any one of the source
  AdaptationSets has their main flag set (commit
  689c2a47aa31f99b3d2a6da9631cb7c8a0111a5b broke Role 'main' support).

Closes #182

Change-Id: Ia017e294e0b359c62133adc42a8d701d614f3b0d

assets/angel_one.mpd
@@ -2,7 +2,15 @@
 <MPD xmlns="urn:mpeg:DASH:schema:MPD:2011" type="static" mediaPresentationDuration="PT59.994S" minBufferTime="PT1S" profiles="urn:webm:dash:profile:webm-on-demand:2012">
   <BaseURL>//shaka-player-demo.appspot.com/assets/angel_one/</BaseURL>
   <Period id="0" start="PT0S" duration="PT59.994S">
-    <AdaptationSet id="6" mimeType="audio/webm" codecs="vorbis" lang="en" audioSamplingRate="48000" subsegmentStartsWithSAP="1">
+    <AdaptationSet id="7" mimeType="audio/webm" codecs="vorbis" lang="de" audioSamplingRate="48000" subsegmentStartsWithSAP="1" group="1">
+      <Representation id="7" bandwidth="91060">
+        <BaseURL>a-de.webm</BaseURL>
+        <SegmentBase indexRange="682068-682263">
+          <Initialization range="0-4521"/>
+        </SegmentBase>
+      </Representation>
+    </AdaptationSet>
+    <AdaptationSet id="6" mimeType="audio/webm" codecs="vorbis" lang="en" audioSamplingRate="48000" subsegmentStartsWithSAP="1" group="1">
       <Role value="main" />
       <Representation id="6" bandwidth="112375">
         <BaseURL>a-en.webm</BaseURL>
@@ -11,15 +19,7 @@
         </SegmentBase>
       </Representation>
     </AdaptationSet>
-    <AdaptationSet id="7" mimeType="audio/webm" codecs="vorbis" lang="de" audioSamplingRate="48000" subsegmentStartsWithSAP="1">
-      <Representation id="7" bandwidth="91060">
-        <BaseURL>a-de.webm</BaseURL>
-        <SegmentBase indexRange="682068-682263">
-          <Initialization range="0-4521"/>
-        </SegmentBase>
-      </Representation>
-    </AdaptationSet>
-    <AdaptationSet id="8" mimeType="audio/webm" codecs="vorbis" lang="es" audioSamplingRate="48000" subsegmentStartsWithSAP="1">
+    <AdaptationSet id="8" mimeType="audio/webm" codecs="vorbis" lang="es" audioSamplingRate="48000" subsegmentStartsWithSAP="1" group="1">
       <Representation id="8" bandwidth="96408">
         <BaseURL>a-es.webm</BaseURL>
         <SegmentBase indexRange="728008-728202">
@@ -27,7 +27,7 @@
         </SegmentBase>
       </Representation>
     </AdaptationSet>
-    <AdaptationSet id="9" mimeType="audio/webm" codecs="vorbis" lang="fr" audioSamplingRate="48000" subsegmentStartsWithSAP="1">
+    <AdaptationSet id="9" mimeType="audio/webm" codecs="vorbis" lang="fr" audioSamplingRate="48000" subsegmentStartsWithSAP="1" group="1">
       <Representation id="9" bandwidth="94640">
         <BaseURL>a-fr.webm</BaseURL>
         <SegmentBase indexRange="721029-721223">
@@ -35,7 +35,7 @@
         </SegmentBase>
       </Representation>
     </AdaptationSet>
-    <AdaptationSet id="10" mimeType="audio/webm" codecs="vorbis" lang="it" audioSamplingRate="48000" subsegmentStartsWithSAP="1">
+    <AdaptationSet id="10" mimeType="audio/webm" codecs="vorbis" lang="it" audioSamplingRate="48000" subsegmentStartsWithSAP="1" group="1">
       <Representation id="10" bandwidth="94290">
         <BaseURL>a-it.webm</BaseURL>
         <SegmentBase indexRange="723532-723726">

lib/dash/mpd_processor.js
@@ -346,47 +346,80 @@ shaka.dash.MpdProcessor.prototype.createManifestInfo_ = function(
       break;
     }
 
-    var periodInfo = new shaka.media.PeriodInfo();
-    periodInfo.id = period.id;
-    periodInfo.start = period.start;
-    periodInfo.duration = period.duration;
-
-    // First group AdaptationSets by type.
-    var setsByType = new shaka.util.MultiMap();
-    period.adaptationSets.forEach(
-        function(set) { setsByType.push(set.contentType || '', set); });
-
-    var setTypes = setsByType.keys();
-    for (var typeIdx = 0; typeIdx < setTypes.length; ++typeIdx) {
-      var type = setTypes[typeIdx];
-
-      // Now group AdaptationSets of the same type by group to squash multiple
-      // AdaptationSets into a single StreamSetInfo.
-      var setsByGroup = new shaka.util.MultiMap();
-      setsByType.get(type).forEach(
-          function(set) { setsByGroup.push(set.group, set); });
-
-      var setGroups = setsByGroup.keys();
-      for (var groupIdx = 0; groupIdx < setGroups.length; ++groupIdx) {
-        var group = setGroups[groupIdx];
-
-        var sets = /** @type {!Array.<shaka.dash.mpd.AdaptationSet>} */(
-            setsByGroup.get(group));
-        var streamSetInfo = this.createStreamSetInfo_(
-            mpd, period, sets, manifestCreationTime, networkCallback);
-        periodInfo.streamSetInfos.push(streamSetInfo);
-      }
-    }  // for typeIdx
-
+    var periodInfo = this.createPeriodInfo_(
+        mpd, period, manifestCreationTime, networkCallback);
     manifestInfo.periodInfos.push(periodInfo);
-  }  // for i
+  }
 
   return manifestInfo;
 };
 
 
 /**
- * Creates a StreamSetInfo from AdaptationSets from the same group.
+ * Creates a PeriodInfo.
+ *
+ * @param {!shaka.dash.mpd.Mpd} mpd
+ * @param {!shaka.dash.mpd.Period} period
+ * @param {number} manifestCreationTime The time, in seconds, when the manifest
+ *     was created.
+ * @param {shaka.util.FailoverUri.NetworkCallback} networkCallback
+ * @return {!shaka.media.PeriodInfo}
+ * @private
+ */
+shaka.dash.MpdProcessor.prototype.createPeriodInfo_ = function(
+    mpd, period, manifestCreationTime, networkCallback) {
+  shaka.asserts.assert(period.start != null);
+
+  var periodInfo = new shaka.media.PeriodInfo();
+  periodInfo.id = period.id;
+  periodInfo.start = /** @type {number} */(period.start);
+  periodInfo.duration = period.duration;
+
+  // First group AdaptationSets by type.
+  var setsByType = new shaka.util.MultiMap();
+  period.adaptationSets.forEach(
+      function(set) { setsByType.push(set.contentType || '', set); });
+
+  var setTypes = setsByType.keys();
+  for (var typeIdx = 0; typeIdx < setTypes.length; ++typeIdx) {
+    var type = setTypes[typeIdx];
+
+    // Then group AdaptationSets of the same type by group.
+    var setsByGroup = new shaka.util.MultiMap();
+    setsByType.get(type).forEach(
+        function(set) { setsByGroup.push(set.group, set); });
+
+    var setGroups = setsByGroup.keys();
+    for (var groupIdx = 0; groupIdx < setGroups.length; ++groupIdx) {
+      var group = setGroups[groupIdx];
+      shaka.asserts.assert(group != null);
+
+      // Finally group AdaptationSets of the same type and group by language,
+      // then squash them into the same StreamSetInfo.
+      var setsByLang = new shaka.util.MultiMap();
+      setsByGroup.get(group).forEach(
+          function(set) { setsByLang.push(set.lang, set); });
+
+      var setLangs = setsByLang.keys();
+      for (var langIdx = 0; langIdx < setLangs.length; ++langIdx) {
+        var lang = setLangs[langIdx];
+
+        var sets = /** @type {!Array.<shaka.dash.mpd.AdaptationSet>} */(
+            setsByLang.get(lang));
+        var streamSetInfo = this.createStreamSetInfo_(
+            mpd, period, sets, manifestCreationTime, networkCallback);
+        periodInfo.streamSetInfos.push(streamSetInfo);
+      }  // for langIdx
+    }  // for groupIdx
+  }  // for typeIdx
+
+  return periodInfo;
+};
+
+
+/**
+ * Creates a StreamSetInfo from AdaptationSets of the same type, group, and
+ * language.
  *
  * @param {!shaka.dash.mpd.Mpd} mpd
  * @param {!shaka.dash.mpd.Period} period
@@ -401,6 +434,11 @@ shaka.dash.MpdProcessor.prototype.createManifestInfo_ = function(
 shaka.dash.MpdProcessor.prototype.createStreamSetInfo_ = function(
     mpd, period, adaptationSets, manifestCreationTime, networkCallback) {
   shaka.asserts.assert(adaptationSets.length > 0);
+  shaka.asserts.assert(adaptationSets.every(function(set) {
+    return (set.group == adaptationSets[0].group) &&
+           ((set.lang || '') == (adaptationSets[0].lang || '')) &&
+           ((set.contentType || '') == (adaptationSets[0].contentType || ''));
+  }));
 
   var streamSetInfo = new shaka.media.StreamSetInfo();
 
@@ -419,6 +457,8 @@ shaka.dash.MpdProcessor.prototype.createStreamSetInfo_ = function(
 
   streamSetInfo.lang = adaptationSets[0].lang || '';
   streamSetInfo.contentType = adaptationSets[0].contentType || '';
+  streamSetInfo.main = adaptationSets.some(
+      function(set) { return set.main; });
 
   // Maps each StreamInfo to its AdaptationSet, which we use below to enable
   // certain StreamInfos.
@@ -426,20 +466,6 @@ shaka.dash.MpdProcessor.prototype.createStreamSetInfo_ = function(
 
   for (var i = 0; i < adaptationSets.length; ++i) {
     var adaptationSet = adaptationSets[i];
-
-    if ((adaptationSet.lang || '') != streamSetInfo.lang) {
-      shaka.log.warning(
-          'AdaptationSet groups should have the same language.',
-          adaptationSets);
-    }
-
-    if ((adaptationSet.contentType || '') != streamSetInfo.contentType) {
-      shaka.log.error(
-          'AdaptationSet groups must have the same content type:',
-          'ignoring AdaptationSet.',
-          adaptationSet);
-      continue;
-    }
 
     for (var j = 0; j < adaptationSet.representations.length; ++j) {
       var representation = adaptationSet.representations[j];

spec/mpd_processor_spec.js
@@ -779,6 +779,126 @@ describe('MpdProcessor', function() {
       shaka.player.Player.isTypeSupported = originalIsTypeSupported;
     });
 
+    describe('creates StreamSetInfos from multiple AdaptationSets', function() {
+      var m;
+      var p;
+      var as1;
+      var as2;
+      var r1;
+      var r2;
+      var st1;
+      var st2;
+
+      beforeEach(function() {
+        m = new mpd.Mpd();
+        p = new mpd.Period();
+        as1 = new mpd.AdaptationSet();
+        as2 = new mpd.AdaptationSet();
+        r1 = new mpd.Representation();
+        r2 = new mpd.Representation();
+        st1 = new mpd.SegmentTemplate();
+        st2 = new mpd.SegmentTemplate();
+
+        r1.segmentTemplate = st1;
+        r1.baseUrl = [new goog.Uri('http://example.com')];
+        r1.bandwidth = 250000;
+
+        r2.segmentTemplate = st2;
+        r2.baseUrl = [new goog.Uri('http://example.com')];
+        r2.bandwidth = 500000;
+
+        as1.representations.push(r1);
+        as2.representations.push(r2);
+
+        p.start = 0;
+        p.adaptationSets.push(as1);
+        p.adaptationSets.push(as2);
+
+        m.periods.push(p);
+        m.url = [new goog.Uri('http://example.com/mpd')];
+      });
+
+      it('by merging ones with the same type, group, and language', function() {
+        as1.contentType = 'video';
+        as2.contentType = 'video';
+        as1.group = 1;
+        as2.group = 1;
+        as1.lang = 'en-US';
+        as2.lang = 'en-US';
+
+        r1.mimeType = 'video/mp4';
+        r2.mimeType = 'video/mp4';
+
+        var periodInfo = processor.process(m).periodInfos[0];
+        expect(periodInfo.streamSetInfos.length).toBe(1);
+        expect(periodInfo.streamSetInfos[0].contentType).toBe('video');
+        expect(periodInfo.streamSetInfos[0].lang).toBe('en-US');
+      });
+
+      it('by separating ones with different types', function() {
+        as1.contentType = 'audio';
+        as2.contentType = 'video';
+        as1.group = 1;
+        as2.group = 1;
+        as1.lang = 'en-US';
+        as2.lang = 'en-US';
+
+        r1.mimeType = 'audio/mp4';
+        r2.mimeType = 'video/mp4';
+
+        var periodInfo = processor.process(m).periodInfos[0];
+        expect(periodInfo.streamSetInfos.length).toBe(2);
+
+        expect(periodInfo.streamSetInfos[0].contentType).toBe('audio');
+        expect(periodInfo.streamSetInfos[0].lang).toBe('en-US');
+
+        expect(periodInfo.streamSetInfos[1].contentType).toBe('video');
+        expect(periodInfo.streamSetInfos[1].lang).toBe('en-US');
+      });
+
+      it('by separating ones with different groups', function() {
+        as1.contentType = 'audio';
+        as2.contentType = 'audio';
+        as1.group = 1;
+        as2.group = 2;
+        as1.lang = 'en-US';
+        as2.lang = 'en-US';
+
+        r1.mimeType = 'audio/mp4';
+        r2.mimeType = 'audio/mp4';
+
+        var periodInfo = processor.process(m).periodInfos[0];
+        expect(periodInfo.streamSetInfos.length).toBe(2);
+
+        expect(periodInfo.streamSetInfos[0].contentType).toBe('audio');
+        expect(periodInfo.streamSetInfos[0].lang).toBe('en-US');
+
+        expect(periodInfo.streamSetInfos[1].contentType).toBe('audio');
+        expect(periodInfo.streamSetInfos[1].lang).toBe('en-US');
+      });
+
+      it('by separating ones with different languages', function() {
+        as1.contentType = 'audio';
+        as2.contentType = 'audio';
+        as1.group = 1;
+        as2.group = 1;
+        as1.lang = 'en-US';
+        as2.lang = 'fr';
+
+        r1.mimeType = 'audio/mp4';
+        r2.mimeType = 'audio/mp4';
+
+        var periodInfo = processor.process(m).periodInfos[0];
+        expect(periodInfo.streamSetInfos.length).toBe(2);
+
+        expect(periodInfo.streamSetInfos[0].contentType).toBe('audio');
+        expect(periodInfo.streamSetInfos[0].lang).toBe('en-US');
+
+        expect(periodInfo.streamSetInfos[1].contentType).toBe('audio');
+        expect(periodInfo.streamSetInfos[1].lang).toBe('fr');
+      });
+    });
+
     describe('SegmentTemplate', function() {
       var m;
       var p;

spec/player_integration.js
@@ -936,6 +936,22 @@ describe('Player', function() {
         done();
       });
     });
+
+    it('respects the \'main\' attribute', function(done) {
+      // There are no Thai audio AdaptationSets in the MPD, but the English
+      // audio AdaptationSet is marked as main, so even though it is not the
+      // first AdaptationSet, it should be preferred.
+      player.configure({'preferredLanguage': 'th'});
+
+      player.load(newSource(languagesManifest)).then(function() {
+        var activeAudioTrack = getActiveAudioTrack();
+        expect(activeAudioTrack.lang).toBe('en');
+        done();
+      }).catch(function(error) {
+        fail(error);
+        done();
+      });
+    });
   });
 
   describe('configuring restrictions', function() {