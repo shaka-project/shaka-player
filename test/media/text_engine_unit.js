shaka-project/shaka-player

commit ab64c76ee90eafe6f8813c400a54eedfdf22bf81

Fix MP4 text parser integration
 * removes an assertion which is no longer valid
 * corrects the registration of the VTT in MP4 parser
 * fixes some type issues in TextEngine

Change-Id: I0b7f41f01e5d7be2932c86a56f3cdfd6a53d04cb

lib/media/text_engine.js
@@ -59,7 +59,7 @@ shaka.media.TextEngine = function(track, mimeType) {
 /**
  * Parses a text buffer into an array of cues.
  *
- * @typedef {function(ArrayBuffer, number, number):!Array.<!TextTrackCue>}
+ * @typedef {function(ArrayBuffer, ?number, ?number):!Array.<!TextTrackCue>}
  * @exportDoc
  */
 shaka.media.TextEngine.TextParser;
@@ -112,8 +112,8 @@ shaka.media.TextEngine.prototype.destroy = function() {
 
 /**
  * @param {ArrayBuffer} buffer
- * @param {number} startTime
- * @param {number} endTime
+ * @param {?number} startTime
+ * @param {?number} endTime
  * @return {!Promise}
  */
 shaka.media.TextEngine.prototype.appendBuffer =
@@ -124,6 +124,14 @@ shaka.media.TextEngine.prototype.appendBuffer =
   return Promise.resolve().then(function() {
     // Parse the buffer and add the new cues.
     var cues = this.parser_(buffer, startTime, endTime);
+
+    if (!cues.length) {
+      // Init segments have no cues and no times.
+      return;
+    }
+    // If cues were parsed, startTime and endTime should be non-null.
+    goog.asserts.assert(startTime != null && endTime != null,
+                        'start and end times should be non-null!');
 
     for (var i = 0; i < cues.length; ++i) {
       cues[i].startTime += offset;

lib/media/media_source_engine.js
@@ -352,8 +352,6 @@ shaka.media.MediaSourceEngine.prototype.getBuffered_ = function(contentType) {
 shaka.media.MediaSourceEngine.prototype.appendBuffer =
     function(contentType, data, startTime, endTime) {
   if (contentType == 'text') {
-    goog.asserts.assert(startTime != null && endTime != null,
-                        'text streams do not have init segments!');
     return this.textEngine_.appendBuffer(data, startTime, endTime);
   }
   return this.enqueueOperation_(

lib/media/mp4_vtt_parser.js
@@ -193,4 +193,4 @@ shaka.media.Mp4VttParser.BOX_TYPE_STTG = 0x73747467;
 
 
 shaka.media.TextEngine.registerParser(
-    'application/mp4; codecs="wvtt"', shaka.media.Mp4TtmlParser);
+    'application/mp4; codecs="wvtt"', shaka.media.Mp4VttParser);

commit 89e354807489d97109f115df60a5f4d450e42f87

Consider empty text cues buffered
Empty WebVTT cues are valid, and should still be considered buffered.
By using a cues object with 0 length as a proxy to the init segment,
we lose valid empty WebVTT segments. Instead, use existence of start/end
as a proxy to init segments.

This regression was introduced in ab64c76ee90eafe6f8813c400a54eedfdf22bf81

lib/media/text_engine.js
@@ -125,13 +125,10 @@ shaka.media.TextEngine.prototype.appendBuffer =
     // Parse the buffer and add the new cues.
     var cues = this.parser_(buffer, startTime, endTime);
 
-    if (!cues.length) {
-      // Init segments have no cues and no times.
+    if (startTime == null || endTime == null) {
+      // Init segments will not have start/end times passed
       return;
     }
-    // If cues were parsed, startTime and endTime should be non-null.
-    goog.asserts.assert(startTime != null && endTime != null,
-                        'start and end times should be non-null!');
 
     for (var i = 0; i < cues.length; ++i) {
       cues[i].startTime += offset;

test/media/text_engine_unit.js
@@ -60,6 +60,22 @@ describe('TextEngine', function() {
       expect(mockTrack.addCue).not.toHaveBeenCalled();
     });
 
+    it('considers empty cues buffered', function(done) {
+      mockParser.and.returnValue([]);
+
+      textEngine.appendBuffer(dummyData, 0, 3).then(function() {
+        expect(mockParser).toHaveBeenCalledWith(dummyData, 0, 3);
+        expect(mockTrack.addCue).not.toHaveBeenCalled();
+        expect(mockTrack.removeCue).not.toHaveBeenCalled();
+
+        expect(textEngine.bufferStart()).toBe(0);
+        expect(textEngine.bufferEnd()).toBe(3);
+
+        mockTrack.addCue.calls.reset();
+        mockParser.calls.reset();
+      }).catch(fail).then(done);
+    });
+
     it('adds cues to the track', function(done) {
       mockParser.and.returnValue([1, 2, 3]);
 