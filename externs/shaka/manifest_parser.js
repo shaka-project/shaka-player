21f7fb0ce2f95eb60aa2875f841f9135b590a46e
Joey Parrish <joeyparrish@google.com>
2016-09-15 16:47:28 -0700

    Remove FakeEvent from exported methods
    This makes it so we do not have to export FakeEvent and include it in
    our generated externs (coming soon, after a compiler upgrade).
    
    Change-Id: I7ccdfe94b989a498c85681ad9166bc184eb611c9

--- externs/shaka/manifest_parser.js
+++ externs/shaka/manifest_parser.js
@@ -67,8 +67,8 @@ shakaExtern.ManifestParser.prototype.configure = function(config) {};
  *     on all new Periods so that they can be filtered.
  * @param {function(!shaka.util.Error)} onError A callback to be invoked on
  *     errors.
- * @param {function(!shaka.util.FakeEvent)} onEvent A callback to be invoked to
- * dispatch events to application.
+ * @param {function(!Event)} onEvent A callback to be invoked to dispatch events
+ *     to the application.
  * @return {!Promise.<shakaExtern.Manifest>}
  * @exportDoc
  */
