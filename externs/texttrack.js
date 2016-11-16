a61fa06503f3bc6b241f653e8928e4cb68260743
birme <jonas.birme@eyevinn.se>
2016-11-16 15:05:09 +0100

    A number of TextTrackCue properties that is in use was not included in the compiled version causing VTT default values to be incorrectly used instead.

--- externs/texttrack.js
+++ externs/texttrack.js
@@ -33,3 +33,15 @@ TextTrackCue.prototype.positionAlign;
 
 /** @type {string} */
 TextTrackCue.prototype.lineAlign;
+
+
+/** @type {number|null|string} */
+TextTrackCue.prototype.line;
+
+
+/** @type {string} */
+TextTrackCue.prototype.vertical;
+
+
+/** @type {boolean} */
+TextTrackCue.prototype.snapToLines;
