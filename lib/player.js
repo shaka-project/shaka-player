shaka-project/shaka-player

commit 5cb720436c2b34ab14486a6d1c4127252cd88157

Allow setting opt_startTime to 0.
Closes #1069

Change-Id: I345626ec5418cc3d15935a8a0bc6f1d4449832e7

lib/player.js
@@ -813,9 +813,10 @@ shaka.Player.prototype.createNetworkingEngine = function() {
  */
 shaka.Player.prototype.createPlayhead = function(opt_startTime) {
   goog.asserts.assert(this.manifest_, 'Must have manifest');
+  var startTime = opt_startTime == undefined ? null : opt_startTime;
   return new shaka.media.Playhead(
       this.video_, this.manifest_, this.config_.streaming,
-      opt_startTime || null, this.onSeek_.bind(this), this.onEvent_.bind(this));
+      startTime, this.onSeek_.bind(this), this.onEvent_.bind(this));
 };
 
 

lib/media/playhead.js
@@ -198,7 +198,7 @@ shaka.media.Playhead.prototype.getTime = function() {
  * @private
  */
 shaka.media.Playhead.prototype.getStartTime_ = function() {
-  if (this.startTime_) {
+  if (this.startTime_ != null) {
     return this.clampTime_(this.startTime_);
   }
 

test/media/playhead_unit.js
@@ -216,6 +216,21 @@ describe('Playhead', function() {
       video.currentTime = 6;
       expect(playhead.getTime()).toBe(6);
     });
+
+    it('allows using startTime of 0', function() {
+      video.readyState = HTMLMediaElement.HAVE_METADATA;
+      timeline.isLive.and.returnValue(true);
+      timeline.getDuration.and.returnValue(Infinity);
+      timeline.getSegmentAvailabilityStart.and.returnValue(0);
+      timeline.getSegmentAvailabilityEnd.and.returnValue(60);
+      timeline.getSeekRangeEnd.and.returnValue(60);
+
+      playhead = new shaka.media.Playhead(
+          video, manifest, config, 0 /* startTime */, Util.spyFunc(onSeek),
+          Util.spyFunc(onEvent));
+
+      expect(playhead.getTime()).toBe(0);
+    });
   });  // getTime
 
   it('clamps playhead after seeking for live', function() {

commit dcb382ecc2fc8893c2e3c80a7839bd1c90b07dc7

Fix start time in live streams.
Commit 5cb720436 broke the start time calculations for live streams.
We don't need to set the start time with non-0 play-range start
values since Playhead will clamp the start time anyway.

Issue #1069

Change-Id: I40915cc5ecd81085c55149e7267d5025745ddb47

lib/player.js
@@ -639,8 +639,7 @@ shaka.Player.prototype.load = function(manifestUri, opt_startTime,
     }
     // MediaSource is open, so create the Playhead, MediaSourceEngine, and
     // StreamingEngine.
-    var startTime = opt_startTime || this.config_.playRangeStart;
-    this.playhead_ = this.createPlayhead(startTime);
+    this.playhead_ = this.createPlayhead(opt_startTime);
     this.playheadObserver_ = this.createPlayheadObserver();
     this.mediaSourceEngine_ = this.createMediaSourceEngine();
 