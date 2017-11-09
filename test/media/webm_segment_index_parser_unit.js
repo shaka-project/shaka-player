6cf57fd8539de27626c06be5c28e64a40499652e
Joey Parrish <joeyparrish@google.com>
2017-11-08 16:07:33 -0800

    Do not assume same timescale in manifest and media
    According to the DASH spec, the timescale in the manifest need not match the
    timescale in the media.  Therefore, we should be applying scaled
    presentationTimeOffsets in segment index parsers, since the two scales might
    differ.
    
    Issue #1098
    
    Change-Id: Ic191d1bba399b30a656ab5060d7bb226e659b79a

--- test/media/webm_segment_index_parser_unit.js
+++ test/media/webm_segment_index_parser_unit.js
@@ -82,4 +82,24 @@ describe('WebmSegmentIndexParser', function() {
       expect(result[i].endByte).toBe(references[i].endByte);
     }
   });
+
+  it('takes a scaled presentationTimeOffset in seconds', function() {
+    var result = parser.parse(indexSegment, initSegment, [], 2);
+    var references =
+        [
+         {startTime: -2, endTime: 10},
+         {startTime: 10, endTime: 22},
+         {startTime: 22, endTime: 34},
+         {startTime: 34, endTime: 46},
+         {startTime: 46, endTime: 58}
+        ];
+
+    expect(result).toBeTruthy();
+    expect(result.length).toBe(references.length);
+    for (var i = 0; i < result.length; i++) {
+      expect(result[i].position).toBe(i);
+      expect(result[i].startTime).toBe(references[i].startTime);
+      expect(result[i].endTime).toBe(references[i].endTime);
+    }
+  });
 });
