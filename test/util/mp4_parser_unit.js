88857c59aacb8ac0dbc6f9b6eaea683f08f7a17b
Joey Parrish <joeyparrish@google.com>
2017-11-10 16:23:26 -0800

    Fix HLS playback where moof box is larger than 1k
    When we started fetching partial HLS segments to find the start time,
    we were no longer able to start playback for content with a moof box
    larger than the partial segment.  This is because we parsed the boxes
    hierarchically, which required the entire payload.
    
    Now, we can selectively instruct the parser to tolerate a partial box
    if the part we have includes the child box we are looking for.
    
    This fixes playback of several pieces of HLS content in our demo app.
    
    Change-Id: I956c8b8905dc9f1707f2b24b8248b984b1c036c6

--- test/util/mp4_parser_unit.js
+++ test/util/mp4_parser_unit.js
@@ -43,9 +43,12 @@ describe('Mp4Parser', function() {
     boxWithChildData = new Uint8Array([
       0x00, 0x00, 0x00, 0x14, // size
       0x62, 0x30, 0x30, 0x33, // type
-      0x00, 0x00, 0x00, 0x0C, // child size
-      0x62, 0x30, 0x33, 0x31, // child type
-      0x00, 0x11, 0x22, 0x33  // child payload
+      0x00, 0x00, 0x00, 0x0C, // child [0] size
+      0x62, 0x30, 0x33, 0x31, // child [0] type
+      0x00, 0x11, 0x22, 0x33, // child [0] payload
+      0x00, 0x00, 0x00, 0x0C, // child [1] size
+      0x62, 0x30, 0x33, 0x32, // child [1] type
+      0x44, 0x55, 0x66, 0x77  // child [1] payload
     ]).buffer;
 
     boxWithSampleDescription = new Uint8Array([
@@ -134,7 +137,14 @@ describe('Mp4Parser', function() {
       var parentBox = jasmine.createSpy('parent box').and.callFake(
           shaka.util.Mp4Parser.children);
 
-      var childBox = jasmine.createSpy('child box').and.callFake(
+      var childBox1 = jasmine.createSpy('child box 1').and.callFake(
+          function(box) {
+            expect(box.size).toEqual(12);
+            expect(box.version).toEqual(null);
+            expect(box.flags).toEqual(null);
+          });
+
+      var childBox2 = jasmine.createSpy('child box 2').and.callFake(
           function(box) {
             expect(box.size).toEqual(12);
             expect(box.version).toEqual(null);
@@ -143,10 +153,33 @@ describe('Mp4Parser', function() {
 
       new shaka.util.Mp4Parser()
           .box('b003', Util.spyFunc(parentBox))
-          .box('b031', Util.spyFunc(childBox)).parse(boxWithChildData);
+          .box('b031', Util.spyFunc(childBox1))
+          .box('b032', Util.spyFunc(childBox2)).parse(boxWithChildData);
 
       expect(parentBox).toHaveBeenCalled();
-      expect(childBox).toHaveBeenCalled();
+      expect(childBox1).toHaveBeenCalled();
+      expect(childBox2).toHaveBeenCalled();
+    });
+
+    it('stops reading children when asked to', function() {
+      var parentBox = jasmine.createSpy('parent box').and.callFake(
+          shaka.util.Mp4Parser.children);
+
+      var childBox1 = jasmine.createSpy('child box 1').and.callFake(
+          function(box) {
+            box.parser.stop();
+          });
+
+      var childBox2 = jasmine.createSpy('child box 2');
+
+      new shaka.util.Mp4Parser()
+          .box('b003', Util.spyFunc(parentBox))
+          .box('b031', Util.spyFunc(childBox1))
+          .box('b032', Util.spyFunc(childBox2)).parse(boxWithChildData);
+
+      expect(parentBox).toHaveBeenCalled();
+      expect(childBox1).toHaveBeenCalled();
+      expect(childBox2).not.toHaveBeenCalled();
     });
 
     it('reads all data definition', function() {
@@ -166,7 +199,6 @@ describe('Mp4Parser', function() {
     });
 
     it('reads sample description definition', function() {
-
       var parentBox = jasmine.createSpy('parent box').and.callFake(
           shaka.util.Mp4Parser.sampleDescription);
       var childBox1 = jasmine.createSpy('child box 1');
@@ -180,6 +212,25 @@ describe('Mp4Parser', function() {
       expect(parentBox).toHaveBeenCalledTimes(1);
       expect(childBox1).toHaveBeenCalledTimes(1);
       expect(childBox2).toHaveBeenCalledTimes(1);
+    });
+
+    it('stops reading sample description when asked to', function() {
+      var parentBox = jasmine.createSpy('parent box').and.callFake(
+          shaka.util.Mp4Parser.sampleDescription);
+      var childBox1 = jasmine.createSpy('child box 1').and.callFake(
+          function(box) {
+            box.parser.stop();
+          });
+      var childBox2 = jasmine.createSpy('child box 2');
+
+      new shaka.util.Mp4Parser()
+          .box('b003', Util.spyFunc(parentBox))
+          .box('b032', Util.spyFunc(childBox1))
+          .box('b033', Util.spyFunc(childBox2)).parse(boxWithSampleDescription);
+
+      expect(parentBox).toHaveBeenCalledTimes(1);
+      expect(childBox1).toHaveBeenCalledTimes(1);
+      expect(childBox2).not.toHaveBeenCalled();
     });
   });
 
@@ -229,6 +280,46 @@ describe('Mp4Parser', function() {
       expect(box1).toHaveBeenCalled();
       expect(box2Child).not.toHaveBeenCalled();
       expect(box3).toHaveBeenCalled();
+    });
+
+    it('can parse partial parent box and find first child', function() {
+      var parentBox = jasmine.createSpy('parent box').and.callFake(
+          shaka.util.Mp4Parser.sampleDescription);
+
+      var childBox1 = jasmine.createSpy('child box 1').and.callFake(
+          function(box) {
+            // We found what we were looking for, so stop parsing.
+            box.parser.stop();
+          });
+
+      // This only contains the parent and the first child (12 bytes each).
+      var partialBoxWithSampleDescription =
+          (new Uint8Array(boxWithSampleDescription)).slice(0, 24).buffer;
+
+      try {
+        new shaka.util.Mp4Parser()
+            .box('b003', Util.spyFunc(parentBox))
+            .box('b032', Util.spyFunc(childBox1))
+            .parse(partialBoxWithSampleDescription, false /* partialOkay */);
+        fail('Should not have been able to parse!');
+      } catch (error) {
+        Util.expectToEqualError(error, new shaka.util.Error(
+            shaka.util.Error.Severity.CRITICAL,
+            shaka.util.Error.Category.MEDIA,
+            shaka.util.Error.Code.BUFFER_READ_OUT_OF_BOUNDS));
+      }
+
+      parentBox.calls.reset();
+      childBox1.calls.reset();
+
+      // With the partialOkay flag set to true, this should succeed.
+      new shaka.util.Mp4Parser()
+          .box('b003', Util.spyFunc(parentBox))
+          .box('b032', Util.spyFunc(childBox1))
+          .parse(partialBoxWithSampleDescription, true /* partialOkay */);
+
+      expect(parentBox).toHaveBeenCalledTimes(1);
+      expect(childBox1).toHaveBeenCalledTimes(1);
     });
   });
 });
