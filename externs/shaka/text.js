186e3e69c437623ecf1e93cd40fcf7811f79e745
Joey Parrish <joeyparrish@google.com>
2017-12-18 16:15:57 -0800

    Consistently take Uint8Array in text plugins
    Instead of taking Uint8Array for media segments and ArrayBuffer for
    init segments, take Uint8Array for everything.
    
    Clean-up after #1022, discovered while preparing the upgrade guide for
    v2.3.
    
    Change-Id: I1f284f6f51f345e663c06d96d788bd9cfb941c52

--- externs/shaka/text.js
+++ externs/shaka/text.js
@@ -261,7 +261,7 @@ shakaExtern.TextParser.TimeContext;
  * Parse an initialization segment. Some formats do not have init
  * segments so this won't always be called.
  *
- * @param {!ArrayBuffer} data
+ * @param {!Uint8Array} data
  *    The data that makes up the init segment.
  *
  * @exportDoc
