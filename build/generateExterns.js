b15d4d3f4ef6c5bb3a20b4c425e15132c1edea54
Joey Parrish <joeyparrish@google.com>
2018-04-04 12:12:38 -0700

    Add support for generating externs for ES6 classes
    This is necessary to allow the exporting of AbortableOperation.
    
    Change-Id: Id343fe147b0ec5c1f653a679bb6472c222b2c59a

--- build/generateExterns.js
+++ build/generateExterns.js
@@ -328,7 +328,7 @@ function createExternFromExportNode(names, node) {
       //   }
       // }
       name = getIdentifierString(node.expression.left);
-      assignment = createExternAssignment(node.expression.right);
+      assignment = createExternAssignment(name, node.expression.right);
       break;
 
     case 'MemberExpression':
@@ -362,11 +362,70 @@ function createExternFromExportNode(names, node) {
 
 
 /**
+ * @param {ASTNode} node A method node from the abstract syntax tree.
+ * @return {string} The extern string for this method.
+ */
+function createExternMethod(node) {
+  // Example code: foo.bar = class {
+  //   baz() { ... }
+  // };
+  // Example node: {
+  //   leadingComments: [ ... ],
+  //   static: false,
+  //   key: Identifier,
+  //   value: FunctionExpression,
+  // }
+  let comment = getLeadingBlockComment(node);
+  comment = removeExportAnnotationsFromComment(comment);
+
+  const params = getFunctionParameters(node.value);
+  const id = getIdentifierString(node.key);
+
+  let methodString = '  ' + comment + '\n  ';
+  if (node.static) methodString += 'static ';
+  methodString += id + '(' + params + ') {}';
+  return methodString;
+}
+
+
+/**
+ * @param {string} name The name of the thing we are assigning.
  * @param {ASTNode} node An assignment node from the abstract syntax tree.
  * @return {string} The assignment part of the extern string for this node.
  */
-function createExternAssignment(node) {
+function createExternAssignment(name, node) {
   switch (node.type) {
+    case 'ClassExpression':
+      // Example code: foo.bar = class bar2 extends foo.baz { /* ... */ };
+      // Example node: {
+      //   id: { name: 'bar' },   // or null
+      //   superClass: { type: 'MemberExpression', ... },  // or null
+      //   body: { body: [ ... ] },
+      // }
+      let classString = ' = class ';
+      if (node.id) {
+        classString += getIdentifierString(node.id) + ' ';
+      }
+      if (node.superClass) {
+        classString += 'extends ' + getIdentifierString(node.superClass) + ' ';
+      }
+      classString += '{\n';
+      node.body.body.forEach((member) => {
+        // Only look at exported members.  Constructors are exported implicitly
+        // when the class is exported.
+        var comment = getLeadingBlockComment(member);
+        if (!EXPORT_REGEX.test(comment) && member.key.name != 'constructor') {
+          return;
+        }
+
+        console.assert(member.type == 'MethodDefinition',
+                       'Unexpected exported member type in exported class!');
+
+        classString += createExternMethod(member) + '\n';
+      });
+      classString += '}';
+      return classString;
+
     case 'FunctionExpression':
       // Example code: foo.square = function(x) { return x * x; };
       // Example node: { params: [ { type: 'Identifier', name: 'x' } ] }
