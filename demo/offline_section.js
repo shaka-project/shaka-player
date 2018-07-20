e65cf48d096b6668e3fe1026f20e8b75f261dcad
theodab <theodab@google.com>
2018-07-20 09:18:12 -0700

    Better errors for no persistent license support.
    Beforehand, the demo would have an error saying 'This browser does
    not support persistent licenses.' if the drm systems in the asset
    were not supported. This error makes sense on a platform with no
    persistent license support at all, but is misleading on a platform
    which has persistent license support but not for the specific drm
    system in the asset.
    This adds separate error messages for those two cases.
    
    Change-Id: Ib7b0eb9f7fc635867344b00fe3951abde59f538b

--- demo/offline_section.js
+++ demo/offline_section.js
@@ -49,27 +49,37 @@ shakaDemo.updateButtons_ = function(canHide) {
 
   let option = assetList.options[assetList.selectedIndex];
   let storedContent = option.storedContent;
-  // True if there is no DRM or if the browser supports persistent licenses for
-  // any given DRM system.
-  let supportsDrm = !option.asset || !option.asset.drm ||
-      !option.asset.drm.length || option.asset.drm.some(function(drm) {
+  let hasDrm = option.asset && option.asset.drm && option.asset.drm.length;
+  let supportsPersistentStateForDrm = option.asset.drm.some(function(drm) {
         return shakaDemo.support_.drm[drm] &&
             shakaDemo.support_.drm[drm].persistentState;
       });
+  let supportsPersistentStateForAnyDrm =
+      Object.keys(shakaDemo.support_.drm).some((drm) => {
+    return shakaDemo.support_.drm[drm] &&
+           shakaDemo.support_.drm[drm].persistentState;
+  });
 
   // Only show when the custom asset option is selected.
   document.getElementById('offlineNameDiv').style.display =
       option.asset ? 'none' : 'block';
 
   let button = document.getElementById('storeDeleteButton');
-  button.disabled = (inProgress || !supportsDrm || option.isStored);
+  button.disabled = false;
   button.textContent = storedContent ? 'Delete' : 'Store';
   let helpText = document.getElementById('storeDeleteHelpText');
   if (inProgress) {
+    button.disabled = true;
     helpText.textContent = 'Operation is in progress...';
-  } else if (!supportsDrm) {
+  } else if (hasDrm && !supportsPersistentStateForAnyDrm) {
+    button.disabled = true;
     helpText.textContent = 'This browser does not support persistent licenses.';
-  } else if (button.disabled) {
+  } else if (hasDrm && !supportsPersistentStateForDrm) {
+    button.disabled = true;
+    helpText.textContent = 'This browser does not support persistent ' +
+                           'licenses for any DRM system in this asset.';
+  } else if (option.isStored) {
+    button.disabled = true;
     helpText.textContent = 'The asset is stored offline. ' +
         'Checkout the "Offline" section in the "Asset" list';
   } else {
