2e0ebba1052af09307b93567daece9a04115bf71
Michelle Zhuo <michellezhuo@google.com>
2018-08-31 18:24:45 -0700

    Add CaptionParser to mux.js externs
    We're going to use Caption Parser from mux.js for parsing closed
    captions for Dash. Adding the it to mux.js externs.
    
    Change-Id: I82266944b4cfeed4abdb36df75c67b3e870ba537

--- externs/mux.js
+++ externs/mux.js
@@ -88,3 +88,50 @@ muxjs.mp4.Transmuxer.prototype.dispose = function() {};
  * @exportDoc
  */
 muxjs.mp4.Transmuxer.Segment;
+
+
+/**
+ * Parser for CEA closed captions embedded in video streams for Dash.
+ * @constructor
+ * @struct
+ */
+muxjs.mp4.CaptionParser = function() {};
+
+
+/**
+ * Initializes the closed caption parser.
+ */
+muxjs.mp4.CaptionParser.prototype.init = function() {};
+
+
+/**
+ * Return true if a new video track is selected or if the timescale is
+ * changed.
+ * @param {!Array.<number>} videoTrackIds A list of video tracks found in the
+ *    init segment.
+ * @param {!Object.<number, number>} timescales The map of track Ids and the
+ *    tracks' timescales in the init segment.
+ * @return {boolean}
+ */
+muxjs.mp4.CaptionParser.prototype.isNewInit = function(
+    videoTrackIds, timescales) {};
+
+
+/**
+ * Parses embedded CEA closed captions and interacts with the underlying
+ * CaptionStream, and return the parsed captions.
+ * @param {!Uint8Array} segment The fmp4 segment containing embedded captions
+ * @param {!Array.<number>} videoTrackIds A list of video tracks found in the
+ *    init segment.
+ * @param {!Object.<number, number>} timescales The timescales found in the
+ *    init segment.
+ * @return {Object} Parsed captions
+ */
+muxjs.mp4.CaptionParser.prototype.parse = function(
+    segment, videoTrackIds, timescales) {};
+
+
+/**
+ * Clear the parsed closed captions data for new data.
+ */
+muxjs.mp4.CaptionParser.prototype.clearParsedCaptions = function() {};
