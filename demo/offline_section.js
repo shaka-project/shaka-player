517730970d1ab858635f8024e3d147e03efbc1e0
Gi Gi <gigon@users.noreply.github.com>
2018-10-24 23:56:58 +0300

    Set server certificate before Store and Delete (#1639)
    Call setServerCertificate before store and
    delete operations, when serverCertificate
    exists.
    In demo application, request and
    configure certificate when certificateUri
    is given in asset.
    
    Fixes #1623

--- demo/offline_section.js
+++ demo/offline_section.js
@@ -202,23 +202,33 @@ shakaDemo.storeDeleteAsset_ = function() {
       return shakaDemo.refreshAssetList_();
     });
   } else {
+    let configureCertificate = Promise.resolve();
+
     let asset = shakaDemo.preparePlayer_(option.asset);
-    let nameField = document.getElementById('offlineName').value;
-    let assetName = asset.name ? '[OFFLINE] ' + asset.name : null;
-    let metadata = {name: assetName || nameField || asset.manifestUri};
-    p = storage.store(asset.manifestUri, metadata).then(function() {
-      if (option.asset) {
-        option.isStored = true;
-      }
-      return shakaDemo.refreshAssetList_().then(function() {
-        // Auto-select offline copy of asset after storing.
-        let group = shakaDemo.offlineOptGroup_;
-        for (let i = 0; i < group.childNodes.length; i++) {
-          let option = group.childNodes[i];
-          if (option.textContent == assetName) {
-            assetList.selectedIndex = option.index;
-          }
+
+    if (asset.certificateUri) {
+      configureCertificate = shakaDemo.requestCertificate_(asset.certificateUri)
+        .then(shakaDemo.configureCertificate_);
+    }
+
+    p = configureCertificate.then(function() {
+      let nameField = document.getElementById('offlineName').value;
+      let assetName = asset.name ? '[OFFLINE] ' + asset.name : null;
+      let metadata = {name: assetName || nameField || asset.manifestUri};
+        return storage.store(asset.manifestUri, metadata).then(function() {
+        if (option.asset) {
+          option.isStored = true;
         }
+        return shakaDemo.refreshAssetList_().then(function() {
+          // Auto-select offline copy of asset after storing.
+          let group = shakaDemo.offlineOptGroup_;
+          for (let i = 0; i < group.childNodes.length; i++) {
+            let option = group.childNodes[i];
+            if (option.textContent == assetName) {
+              assetList.selectedIndex = option.index;
+            }
+          }
+        });
       });
     });
   }
