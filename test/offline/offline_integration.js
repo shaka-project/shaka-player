8bbf0bb4c04324052a0463e1158a12bd53971a3d
Joey Parrish <joeyparrish@google.com>
2018-12-13 09:35:43 -0800

    Work around offline integration test failure
    This adds a workaround for a Chrome bug so that the test can be
    unquarantined.
    
    Backported to v2.4.x
    
    Change-Id: I3b076af346db008f3b59e7897c79e78f29fc72df

--- test/offline/offline_integration.js
+++ test/offline/offline_integration.js
@@ -91,7 +91,7 @@ describe('Offline', /** @suppress {accessControls} */ function() {
   //   http://crbug.com/887535
   //   http://crbug.com/887635
   //   http://crbug.com/883895
-  quarantined_it(
+  drm_it(
       'stores, plays, and deletes protected content with a persistent license',
       async function() {
         if (!supportsStorage()) {
@@ -111,6 +111,13 @@ describe('Offline', /** @suppress {accessControls} */ function() {
 
         storage.configure({usePersistentLicense: true});
         let content = await storage.store('test:sintel-enc');
+
+        // Work around http://crbug.com/887535 in which load cannot happen right
+        // after close.  Experimentally, we seem to need a ~1s delay, so we're
+        // using a 3s delay to ensure it doesn't flake.  Without this, we get
+        // error 6005 (FAILED_TO_CREATE_SESSION) with system code 70.
+        // TODO: Remove when Chrome is fixed
+        await shaka.test.Util.delay(3);
 
         let contentUri = content.offlineUri;
         goog.asserts.assert(
