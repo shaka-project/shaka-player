shaka-project/shaka-player

commit 4d41b7b90cb763ae4fd544c7dee801b2d8f610e7

Refactor the overflow menu to use components design.
Closes #1673.

Change-Id: I030745def928796a6abc813a91fb163cb2d76175

karma.conf.js
@@ -111,6 +111,7 @@ module.exports = function(config) {
       {pattern: 'ui/**/*.css', included: false},
       {pattern: 'ui/**/*.less', included: false},
       {pattern: 'third_party/closure/goog/**/*.js', included: false},
+      {pattern: 'third_party/language-mapping-list/**/*.js', included: false},
       {pattern: 'test/test/assets/*', included: false},
       {pattern: 'dist/shaka-player.ui.js', included: false},
       {pattern: 'node_modules/**/*.js', included: false},

build/types/ui
@@ -1,21 +1,27 @@
 # UI library.
 
 +../../third_party/language-mapping-list/language-mapping-list.js
++../../ui/audio_language_selection.js
 +../../ui/externs/ui.js
++../../ui/cast_button.js
 +../../ui/controls.js
 +../../ui/constants.js
 +../../ui/enums.js
 +../../ui/element.js
 +../../ui/fast_forward_button.js
 +../../ui/fullscreen_button.js
++../../ui/language_utils.js
 +../../ui/localization.js
 +../../ui/locales.js
 +../../ui/mute_button.js
 +../../ui/overflow_menu.js
++../../ui/pip_button.js
 +../../ui/presentation_time.js
++../../ui/resolution_selection.js
 +../../ui/rewind_button.js
 +../../ui/spacer.js
 +../../ui/text_displayer.js
++../../ui/text_selection.js
 +../../ui/ui.js
 +../../ui/ui_utils.js
 +../../ui/volume_bar.js

shaka-player.uncompiled.js
@@ -61,16 +61,22 @@ goog.require('shaka.text.SimpleTextDisplayer');
 goog.require('shaka.text.TextEngine');
 goog.require('shaka.text.TtmlTextParser');
 goog.require('shaka.text.VttTextParser');
-goog.require('shaka.ui.Overlay');
-goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.Controls');
+goog.require('shaka.ui.OverflowMenu');
+goog.require('shaka.ui.AudioLanguageSelection');
+goog.require('shaka.ui.CastButton');
 goog.require('shaka.ui.Element');
 goog.require('shaka.ui.FastForwardButton');
 goog.require('shaka.ui.FullscreenButton');
+goog.require('shaka.ui.Localization');
 goog.require('shaka.ui.MuteButton');
-goog.require('shaka.ui.OverflowMenu');
+goog.require('shaka.ui.Overlay');
+goog.require('shaka.ui.PipButton');
 goog.require('shaka.ui.PresentationTimeTracker');
+goog.require('shaka.ui.ResolutionSelection');
 goog.require('shaka.ui.RewindButton');
 goog.require('shaka.ui.Spacer');
+goog.require('shaka.ui.TextSelection');
 goog.require('shaka.ui.VolumeBar');
 goog.require('shaka.util.Error');
 goog.require('shaka.util.Iterables');

ui/audio_language_selection.js
@@ -1,0 +1,209 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+goog.provide('shaka.ui.AudioLanguageSelection');
+
+goog.require('shaka.ui.Element');
+goog.require('shaka.ui.Enums');
+goog.require('shaka.ui.LanguageUtils');
+goog.require('shaka.ui.Locales');
+goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.OverflowMenu');
+
+
+/**
+ * @extends {shaka.ui.Element}
+ * @final
+ * @export
+ */
+shaka.ui.AudioLanguageSelection = class extends shaka.ui.Element {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls);
+
+    this.addLanguagesButton_();
+
+    this.addAudioLangMenu_();
+
+    this.eventManager.listen(
+      this.localization, shaka.ui.Localization.LOCALE_UPDATED, () => {
+        this.updateLocalizedStrings_();
+        // TODO: is there a more efficient way of updating just the strings
+        // we need instead of running the whole language update?
+        this.updateAudioLanguages_();
+      });
+
+    this.eventManager.listen(
+      this.localization, shaka.ui.Localization.LOCALE_CHANGED, () => {
+        this.updateLocalizedStrings_();
+        // TODO: is there a more efficient way of updating just the strings
+        // we need instead of running the whole language update?
+        this.updateAudioLanguages_();
+      });
+
+
+    this.eventManager.listen(this.player, 'trackschanged', () => {
+        this.updateAudioLanguages_();
+      });
+
+    this.eventManager.listen(this.player, 'variantchanged', () => {
+        this.updateAudioLanguages_();
+      });
+
+    this.eventManager.listen(this.languagesButton_, 'click', () => {
+          this.onLanguagesClick_();
+      });
+
+    // Set up all the strings in the user's preferred language.
+    this.updateLocalizedStrings_();
+  }
+
+
+  /**
+   * @private
+   */
+  addAudioLangMenu_() {
+    /** @private {!HTMLElement} */
+    this.audioLangMenu_ = shaka.ui.Utils.createHTMLElement('div');
+    this.audioLangMenu_.classList.add('shaka-audio-languages');
+    this.audioLangMenu_.classList.add('shaka-no-propagation');
+    this.audioLangMenu_.classList.add('shaka-show-controls-on-mouse-over');
+    this.audioLangMenu_.classList.add('shaka-settings-menu');
+
+    /** @private {!HTMLElement} */
+    this.backFromLanguageButton_ = shaka.ui.Utils.createHTMLElement('button');
+    this.backFromLanguageButton_.classList.add('shaka-back-to-overflow-button');
+    this.audioLangMenu_.appendChild(this.backFromLanguageButton_);
+
+    const backIcon = shaka.ui.Utils.createHTMLElement('i');
+    backIcon.classList.add('material-icons');
+    backIcon.textContent = shaka.ui.Enums.MaterialDesignIcons.BACK;
+    this.backFromLanguageButton_.appendChild(backIcon);
+
+    /** @private {!HTMLElement} */
+    this.backFromLanguageSpan_ = shaka.ui.Utils.createHTMLElement('span');
+    this.backFromLanguageButton_.appendChild(this.backFromLanguageSpan_);
+
+    const controlsContainer = this.controls.getControlsContainer();
+    controlsContainer.appendChild(this.audioLangMenu_);
+  }
+
+
+  /**
+   * @private
+   */
+  addLanguagesButton_() {
+    /** @private {!HTMLElement} */
+    this.languagesButton_ = shaka.ui.Utils.createHTMLElement('button');
+    this.languagesButton_.classList.add('shaka-language-button');
+
+    const icon = shaka.ui.Utils.createHTMLElement('i');
+    icon.classList.add('material-icons');
+    icon.textContent = shaka.ui.Enums.MaterialDesignIcons.LANGUAGE;
+    this.languagesButton_.appendChild(icon);
+
+    const label = shaka.ui.Utils.createHTMLElement('label');
+    label.classList.add('shaka-overflow-button-label');
+
+    /** @private {!HTMLElement} */
+    this.languageNameSpan_ = shaka.ui.Utils.createHTMLElement('span');
+    this.languageNameSpan_.classList.add('languageSpan');
+    label.appendChild(this.languageNameSpan_);
+
+    /** @private {!HTMLElement} */
+    this.currentAudioLanguage_ = shaka.ui.Utils.createHTMLElement('span');
+    this.currentAudioLanguage_.classList.add('shaka-current-selection-span');
+    const language = this.player.getConfiguration().preferredAudioLanguage;
+    this.currentAudioLanguage_.textContent =
+      shaka.ui.LanguageUtils.getLanguageName(language, this.localization);
+    label.appendChild(this.currentAudioLanguage_);
+
+    this.languagesButton_.appendChild(label);
+
+    this.parent.appendChild(this.languagesButton_);
+  }
+
+  /** @private */
+  onLanguagesClick_() {
+    this.controls.dispatchEvent(new shaka.util.FakeEvent('submenuopen'));
+    shaka.ui.Utils.setDisplay(this.audioLangMenu_, true);
+    // Focus on the currently selected language button.
+    shaka.ui.Utils.focusOnTheChosenItem(this.audioLangMenu_);
+  }
+
+  /** @private */
+  updateAudioLanguages_() {
+    const tracks = this.player.getVariantTracks();
+
+    const languagesAndRoles = this.player.getAudioLanguagesAndRoles();
+    const languages = languagesAndRoles.map((langAndRole) => {
+      return langAndRole.language;
+    });
+
+    shaka.ui.LanguageUtils.updateLanguages(tracks, this.audioLangMenu_,
+      languages,
+      this.onAudioLanguageSelected_.bind(this), /* updateChosen */ true,
+      this.currentAudioLanguage_,
+      this.localization);
+    shaka.ui.Utils.focusOnTheChosenItem(this.audioLangMenu_);
+  }
+
+
+  /**
+   * @param {string} language
+   * @private
+   */
+  onAudioLanguageSelected_(language) {
+    this.player.selectAudioLanguage(language);
+  }
+
+
+  /**
+   * @private
+   */
+  updateLocalizedStrings_() {
+    const LocIds = shaka.ui.Locales.Ids;
+
+    this.backFromLanguageButton_.setAttribute(shaka.ui.Constants.ARIA_LABEL,
+        this.localization.resolve(LocIds.ARIA_LABEL_BACK));
+    this.languagesButton_.setAttribute(shaka.ui.Constants.ARIA_LABEL,
+        this.localization.resolve(LocIds.ARIA_LABEL_LANGUAGE));
+    this.languageNameSpan_.textContent =
+      this.localization.resolve(LocIds.LABEL_LANGUAGE);
+    this.backFromLanguageSpan_.textContent =
+      this.localization.resolve(LocIds.LABEL_LANGUAGE);
+  }
+};
+
+
+/**
+ * @implements {shaka.extern.IUIElement.Factory}
+ * @final
+ */
+shaka.ui.AudioLanguageSelection.Factory = class {
+  /** @override */
+  create(rootElement, controls) {
+    return new shaka.ui.AudioLanguageSelection(rootElement, controls);
+  }
+};
+
+shaka.ui.OverflowMenu.registerElement(
+  'language', new shaka.ui.AudioLanguageSelection.Factory());

ui/cast_button.js
@@ -1,0 +1,184 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+goog.provide('shaka.ui.CastButton');
+
+goog.require('shaka.ui.Element');
+goog.require('shaka.ui.Locales');
+goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.OverflowMenu');
+goog.require('shaka.ui.Utils');
+
+
+/**
+ * @extends {shaka.ui.Element}
+ * @final
+ * @export
+ */
+shaka.ui.CastButton = class extends shaka.ui.Element {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls);
+
+    /** @private {!shaka.cast.CastProxy} */
+    this.castProxy_ = this.controls.getCastProxy();
+
+    /** @private {!HTMLElement} */
+    this.castButton_ = shaka.ui.Utils.createHTMLElement('button');
+    this.castButton_.classList.add('shaka-cast-button');
+    this.castButton_.setAttribute('aria-pressed', 'false');
+
+    /** @private {!HTMLElement} */
+    this.castIcon_ = shaka.ui.Utils.createHTMLElement('i');
+    this.castIcon_.classList.add('material-icons');
+    this.castIcon_.textContent = shaka.ui.Enums.MaterialDesignIcons.CAST;
+    this.castButton_.appendChild(this.castIcon_);
+
+    const label = shaka.ui.Utils.createHTMLElement('label');
+    label.classList.add('shaka-overflow-button-label');
+    this.castNameSpan_ = shaka.ui.Utils.createHTMLElement('span');
+    label.appendChild(this.castNameSpan_);
+
+    this.castCurrentSelectionSpan_ =
+      shaka.ui.Utils.createHTMLElement('span');
+    this.castCurrentSelectionSpan_.classList.add(
+      'shaka-current-selection-span');
+    label.appendChild(this.castCurrentSelectionSpan_);
+    this.castButton_.appendChild(label);
+    this.parent.appendChild(this.castButton_);
+
+    // Setup strings in the correct language
+    this.updateLocalizedStrings_();
+
+    // Setup button display and state according to the current cast status
+    this.onCastStatusChange_();
+
+    this.eventManager.listen(
+      this.localization, shaka.ui.Localization.LOCALE_UPDATED, () => {
+        this.updateLocalizedStrings_();
+    });
+
+    this.eventManager.listen(
+      this.localization, shaka.ui.Localization.LOCALE_CHANGED, () => {
+        this.updateLocalizedStrings_();
+    });
+
+    this.eventManager.listen(this.castButton_, 'click', () => {
+        this.onCastClick_();
+      });
+
+    this.eventManager.listen(this.controls, 'caststatuschanged', () => {
+        this.onCastStatusChange_();
+      });
+  }
+
+
+    /** @private */
+  async onCastClick_() {
+    if (this.castProxy_.isCasting()) {
+      this.castProxy_.suggestDisconnect();
+    } else {
+      try {
+        this.castButton_.disabled = true;
+        await this.castProxy_.cast();
+        this.castButton_.disabled = false;
+      } catch (error) {
+        this.castButton_.disabled = false;
+        if (error.code != shaka.util.Error.Code.CAST_CANCELED_BY_USER) {
+          this.controls.dispatchEvent(new shaka.util.FakeEvent('error', {
+            errorDetails: error,
+          }));
+        }
+      }
+    }
+  }
+
+
+  /**
+   * @private
+   */
+  onCastStatusChange_() {
+    const canCast = this.castProxy_.canCast() && this.controls.isCastAllowed();
+    const isCasting = this.castProxy_.isCasting();
+    const materialDesignIcons = shaka.ui.Enums.MaterialDesignIcons;
+    shaka.ui.Utils.setDisplay(this.castButton_, canCast);
+    this.castIcon_.textContent = isCasting ?
+                                   materialDesignIcons.EXIT_CAST :
+                                   materialDesignIcons.CAST;
+
+    // Aria-pressed set to true when casting, set to false otherwise.
+    if (canCast) {
+      if (isCasting) {
+        this.castButton_.setAttribute('aria-pressed', 'true');
+      } else {
+        this.castButton_.setAttribute('aria-pressed', 'false');
+      }
+    }
+
+    this.setCurrentCastSelection_();
+  }
+
+
+  /**
+   * @private
+   */
+  setCurrentCastSelection_() {
+    if (this.castProxy_.isCasting()) {
+      this.castCurrentSelectionSpan_.textContent =
+          this.castProxy_.receiverName();
+    } else {
+      this.castCurrentSelectionSpan_.textContent =
+          this.localization.resolve(shaka.ui.Locales.Ids.LABEL_NOT_CASTING);
+    }
+  }
+
+
+  /**
+   * @private
+   */
+  updateLocalizedStrings_() {
+    const LocIds = shaka.ui.Locales.Ids;
+
+    this.castButton_.setAttribute(shaka.ui.Constants.ARIA_LABEL,
+        this.localization.resolve(LocIds.ARIA_LABEL_CAST));
+    this.castNameSpan_.textContent =
+      this.localization.resolve(LocIds.LABEL_CAST);
+
+    // If we're not casting, string "not casting" will be displayed,
+    // which needs localization.
+    this.setCurrentCastSelection_();
+  }
+};
+
+
+/**
+ * @implements {shaka.extern.IUIElement.Factory}
+ * @final
+ */
+shaka.ui.CastButton.Factory = class {
+  /** @override */
+  create(rootElement, controls) {
+    return new shaka.ui.CastButton(rootElement, controls);
+  }
+};
+
+shaka.ui.OverflowMenu.registerElement(
+  'cast', new shaka.ui.CastButton.Factory());

ui/controls.js
@@ -17,6 +17,7 @@
 
 
 goog.provide('shaka.ui.Controls');
+goog.provide('shaka.ui.ControlsPanel');
 
 goog.require('goog.asserts');
 goog.require('shaka.ui.Constants');
@@ -109,7 +110,7 @@ shaka.ui.Controls = function(player, videoContainer, video, config) {
   this.hideSettingsMenusTimer_ = new shaka.util.Timer(() => {
     /** type {function(!HTMLElement)} */
     const hide = (control) => {
-      shaka.ui.Controls.setDisplay(control, /* visible= */ false);
+      shaka.ui.Utils.setDisplay(control, /* visible= */ false);
     };
 
     for (const menu of this.settingsMenus_) {
@@ -170,7 +171,7 @@ goog.inherits(shaka.ui.Controls, shaka.util.FakeEventTarget);
 
 
 /** @private {!Map.<string, !shaka.extern.IUIElement.Factory>} */
-shaka.ui.Controls.elementNamesToFactories_ = new Map();
+shaka.ui.ControlsPanel.elementNamesToFactories_ = new Map();
 
 
 /**
@@ -216,7 +217,7 @@ shaka.ui.Controls.prototype.destroy = function() {
  * @export
  */
 shaka.ui.Controls.registerElement = function(name, factory) {
-  shaka.ui.Controls.elementNamesToFactories_.set(name, factory);
+  shaka.ui.ControlsPanel.elementNamesToFactories_.set(name, factory);
 };
 
 
@@ -255,7 +256,7 @@ shaka.ui.Controls.prototype.loadComplete = function() {
 shaka.ui.Controls.prototype.setEnabledShakaControls = function(enabled) {
   this.enabled_ = enabled;
   if (enabled) {
-    shaka.ui.Controls.setDisplay(
+    shaka.ui.Utils.setDisplay(
       this.controlsButtonPanel_.parentElement, true);
 
     // If we're hiding native controls, make sure the video element itself is
@@ -263,7 +264,7 @@ shaka.ui.Controls.prototype.setEnabledShakaControls = function(enabled) {
     this.video_.tabIndex = -1;
     this.video_.controls = false;
   } else {
-    shaka.ui.Controls.setDisplay(
+    shaka.ui.Utils.setDisplay(
       this.controlsButtonPanel_.parentElement, false);
   }
 
@@ -403,29 +404,6 @@ shaka.ui.Controls.prototype.getDisplayTime = function() {
  */
 shaka.ui.Controls.prototype.setLastTouchEventTime = function(time) {
   this.lastTouchEventTime_ = time;
-};
-
-
-/**
- * Depending on the value of display, sets/removes css class of element to
- * either display it or hide.
- *
- * @param {Element} element
- * @param {boolean} display
- * @export
- */
-shaka.ui.Controls.setDisplay = function(element, display) {
-  if (!element) return;
-  if (display) {
-    element.classList.add('shaka-displayed');
-    // Removing a non-existent class doesn't throw, so, even if
-    // the element is not hidden, this should be fine. Same for displayed
-    // below.
-    element.classList.remove('shaka-hidden');
-  } else {
-    element.classList.add('shaka-hidden');
-    element.classList.remove('shaka-displayed');
-  }
 };
 
 
@@ -607,14 +585,8 @@ shaka.ui.Controls.prototype.addControlsButtonPanel_ = function() {
   // Create the elements specified by controlPanelElements
   for (let i = 0; i < this.config_.controlPanelElements.length; i++) {
     const name = this.config_.controlPanelElements[i];
-    if (shaka.ui.Controls.elementNamesToFactories_.get(name)) {
-      if (shaka.ui.Controls.controlPanelElements_.indexOf(name) == -1) {
-        // Not a control panel element, skip
-        shaka.log.warning('Element is not part of control panel ' +
-          'elements and will be skipped', name);
-        continue;
-      }
-      const factory = shaka.ui.Controls.elementNamesToFactories_.get(name);
+    if (shaka.ui.ControlsPanel.elementNamesToFactories_.get(name)) {
+      const factory = shaka.ui.ControlsPanel.elementNamesToFactories_.get(name);
       this.elements_.push(factory.create(this.controlsButtonPanel_, this));
     }
   }
@@ -745,15 +717,6 @@ shaka.ui.Controls.prototype.addSeekBar_ = function() {
 
   this.seekBarContainer_.appendChild(this.seekBar_);
   this.controlsContainer_.appendChild(this.seekBarContainer_);
-};
-
-
-/**
- * Checks if the cast proxy can cast.
- * @return {boolean}
- */
-shaka.ui.Controls.prototype.canCast = function() {
-  return this.castProxy_.canCast();
 };
 
 
@@ -1135,12 +1098,12 @@ shaka.ui.Controls.prototype.updateTimeAndSeekRange_ = function() {
     const seekRange = this.player_.seekRange();
     const seekWindow = seekRange.end - seekRange.start;
     if (seekWindow < Constants.MIN_SEEK_WINDOW_TO_SHOW_SEEKBAR) {
-      shaka.ui.Controls.setDisplay(this.seekBarContainer_, false);
+      shaka.ui.Utils.setDisplay(this.seekBarContainer_, false);
       for (let menu of this.settingsMenus_) {
         menu.classList.add('shaka-low-position');
       }
     } else {
-      shaka.ui.Controls.setDisplay(this.seekBarContainer_, true);
+      shaka.ui.Utils.setDisplay(this.seekBarContainer_, true);
       for (let menu of this.settingsMenus_) {
         menu.classList.remove('shaka-low-position');
       }
@@ -1321,16 +1284,3 @@ shaka.ui.Controls.createLocalization_ = function() {
 
   return localization;
 };
-
-
-/** @private {!Array.<string>} */
-shaka.ui.Controls.controlPanelElements_ = [
-  'time_and_duration',
-  'mute',
-  'volume',
-  'fullscreen',
-  'overflow_menu',
-  'rewind',
-  'fast_forward',
-  'spacer',
-];

ui/language_utils.js
@@ -1,0 +1,140 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+goog.provide('shaka.ui.LanguageUtils');
+
+goog.require('mozilla.LanguageMapping');
+
+
+shaka.ui.LanguageUtils = class {
+  /**
+   * @param {!Array.<shaka.extern.Track>} tracks
+   * @param {!HTMLElement} langMenu
+   * @param {!Array.<string>} languages
+   * @param {function(string)} onLanguageSelected
+   * @param {boolean} updateChosen
+   * @param {!HTMLElement} currentSelectionElement
+   * @param {shaka.ui.Localization} localization
+   */
+   // TODO: Do the benefits of having this common code in a method still
+   // outweigh the complexity of the parameter list?
+  static updateLanguages(tracks, langMenu, languages, onLanguageSelected,
+      updateChosen, currentSelectionElement, localization) {
+    // Using array.filter(f)[0] as an alternative to array.find(f) which is
+    // not supported in IE11.
+    const activeTracks = tracks.filter(function(track) {
+      return track.active == true;
+    });
+    const selectedTrack = activeTracks[0];
+
+    // Remove old languages
+    // 1. Save the back to menu button
+    const backButton = shaka.ui.Utils.getFirstDescendantWithClassName(
+      langMenu, 'shaka-back-to-overflow-button');
+
+    // 2. Remove everything
+    while (langMenu.firstChild) {
+      langMenu.removeChild(langMenu.firstChild);
+    }
+
+    // 3. Add the backTo Menu button back
+    langMenu.appendChild(backButton);
+
+    // 4. Add new buttons
+    languages.forEach((language) => {
+      let button = shaka.ui.Utils.createHTMLElement('button');
+      button.addEventListener('click', () => { onLanguageSelected(language); });
+
+      let span = shaka.ui.Utils.createHTMLElement('span');
+      span.textContent =
+        shaka.ui.LanguageUtils.getLanguageName(language, localization);
+      button.appendChild(span);
+
+      if (updateChosen && (language == selectedTrack.language)) {
+        button.appendChild(shaka.ui.Utils.checkmarkIcon());
+        span.classList.add('shaka-chosen-item');
+        button.setAttribute('aria-selected', 'true');
+        currentSelectionElement.textContent = span.textContent;
+      }
+      langMenu.appendChild(button);
+    });
+  }
+
+
+  /**
+   * Returns the language's name for itself in its own script (autoglottonym),
+   * if we have it.
+   *
+   * If the locale, including region, can be mapped to a name, we return a very
+   * specific name including the region.  For example, "de-AT" would map to
+   * "Deutsch (Österreich)" or Austrian German.
+   *
+   * If only the language part of the locale is in our map, we append the locale
+   * itself for specificity.  For example, "ar-EG" (Egyptian Arabic) would map
+   * to "ﺎﻠﻋﺮﺒﻳﺓ (ar-EG)".  In this way, multiple versions of Arabic whose
+   * regions are not in our map would not all look the same in the language
+   * list, but could be distinguished by their locale.
+   *
+   * Finally, if language part of the locale is not in our map, we label it
+   * "unknown", as translated to the UI locale, and we append the locale itself
+   * for specificity.  For example, "sjn" would map to "Unknown (sjn)".  In this
+   * way, multiple unrecognized languages would not all look the same in the
+   * language list, but could be distinguished by their locale.
+   *
+   * @param {string} locale
+   * @param {shaka.ui.Localization} localization
+   * @return {string} The language's name for itself in its own script, or as
+   *   close as we can get with the information we have.
+   */
+  static getLanguageName(locale, localization) {
+    if (!locale && !localization) {
+      return '';
+    }
+
+    // Shorthand for resolving a localization ID.
+    const resolve = (id) => localization.resolve(id);
+
+    // Handle some special cases first.  These are reserved language tags that
+    // are used to indicate something that isn't one specific language.
+    switch (locale) {
+      case 'mul':
+        return resolve(shaka.ui.Locales.Ids.LABEL_MULTIPLE_LANGUAGES);
+      case 'zxx':
+        return resolve(shaka.ui.Locales.Ids.LABEL_NOT_APPLICABLE);
+    }
+
+    // Extract the base language from the locale as a fallback step.
+    const language = shaka.util.LanguageUtils.getBase(locale);
+
+    // First try to resolve the full language name.
+    // If that fails, try the base.
+    // Finally, report "unknown".
+    // When there is a loss of specificity (either to a base language or to
+    // "unknown"), we should append the original language code.
+    // Otherwise, there may be multiple identical-looking items in the list.
+    if (locale in mozilla.LanguageMapping) {
+      return mozilla.LanguageMapping[locale].nativeName;
+    } else if (language in mozilla.LanguageMapping) {
+      return mozilla.LanguageMapping[language].nativeName +
+          ' (' + locale + ')';
+    } else {
+      return resolve(shaka.ui.Locales.Ids.LABEL_UNKNOWN_LANGUAGE) +
+          ' (' + locale + ')';
+    }
+  }
+};

commit 7d014fd195206593a06a525283c6d3382ac680fb

Fix ES6-to-ES5 translation of language mapping
Tests on IE and Tizen were broken in 4d41b7b9
when the language mapping was added to the test
environment without Babel config to translate it.

This went unnoticed because of a lab failure.

Change-Id: Ib6517d30c82376e4f828d2ceb7e63de1cb7bcc95

karma.conf.js
@@ -133,6 +133,7 @@ module.exports = function(config) {
       'lib/**/*.js': ['babel'],
       'ui/**/*.js': ['babel'],
       'test/**/*.js': ['babel'],
+      'third_party/language-mapping-list/**/*.js': ['babel'],
     },
 
     babelPreprocessor: {