1cdc1bee1e32eda276437750e0f2824c8c17772e
Kevin Scroggins <nitro404@gmail.com>
2019-03-16 13:15:39 -0400

    Fix React Native createObjectURL polyfill incompatibility
    React Native introduces its own polyfill for window.URL.createObjectURL which is not compatible with Shaka. Encapsulating a reference to the original function inside of the MediaSourceEngine circumvents this issue.
    
    Issue #1842

--- test/media/media_source_engine_unit.js
+++ test/media/media_source_engine_unit.js
@@ -161,7 +161,8 @@ describe('MediaSourceEngine', function() {
   });
 
   describe('constructor', function() {
-    const originalCreateObjectURL = window.URL.createObjectURL;
+    const originalCreateObjectURL =
+      shaka.media.MediaSourceEngine.createObjectURL;
     const originalMediaSource = window.MediaSource;
     /** @type {jasmine.Spy} */
     let createObjectURLSpy;
@@ -176,7 +177,8 @@ describe('MediaSourceEngine', function() {
 
       createObjectURLSpy = jasmine.createSpy('createObjectURL');
       createObjectURLSpy.and.returnValue('blob:foo');
-      window.URL.createObjectURL = Util.spyFunc(createObjectURLSpy);
+      shaka.media.MediaSourceEngine.createObjectURL =
+        Util.spyFunc(createObjectURLSpy);
 
       let mediaSourceSpy = jasmine.createSpy('MediaSource').and.callFake(() => {
         return mockMediaSource;
@@ -187,7 +189,7 @@ describe('MediaSourceEngine', function() {
     });
 
     afterAll(function() {
-      window.URL.createObjectURL = originalCreateObjectURL;
+      shaka.media.MediaSourceEngine.createObjectURL = originalCreateObjectURL;
       window.MediaSource = originalMediaSource;
     });
 
