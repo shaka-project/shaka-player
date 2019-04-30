0e65a4543e72a45b3efc90eaacc00a615989a8e0
Joey Parrish <joeyparrish@google.com>
2019-04-29 17:07:48 -0700

    Fix EventManager listenOnce with multiple listeners
    When listening to the same event on the same object from two places,
    it's important that both listeners get called back.
    
    This fixes EventManager's listenOnce() so that the unlisten() call
    within listenOnce() doesn't remove both listeners at once.  Now each
    listener will be called before it is removed.
    
    This bug is over two years old!
    
    Change-Id: Id99f3a8e5ab80819921b30e28aa66d8a08b29e86

--- test/util/event_manager_unit.js
+++ test/util/event_manager_unit.js
@@ -45,122 +45,156 @@ describe('EventManager', function() {
     eventManager.release();
   });
 
-  it('listens for an event', function() {
-    let listener = jasmine.createSpy('listener');
+  describe('listen', () => {
+    it('listens for an event', function() {
+      let listener = jasmine.createSpy('listener');
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener));
-    target1.dispatchEvent(event1);
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener));
+      target1.dispatchEvent(event1);
 
-    expect(listener).toHaveBeenCalled();
+      expect(listener).toHaveBeenCalled();
+    });
+
+    it('listens for an event from mutiple targets', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target2, 'eventtype1', Util.spyFunc(listener2));
+
+      target1.dispatchEvent(event1);
+      target2.dispatchEvent(event1);
+
+      expect(listener1).toHaveBeenCalled();
+      expect(listener2).toHaveBeenCalled();
+    });
+
+    it('listens for multiple events', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target1, 'eventtype2', Util.spyFunc(listener2));
+
+      target1.dispatchEvent(event1);
+      target1.dispatchEvent(event2);
+
+      expect(listener1).toHaveBeenCalled();
+      expect(listener2).toHaveBeenCalled();
+    });
+
+    it('listens for multiple events from mutiple targets', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target2, 'eventtype2', Util.spyFunc(listener2));
+
+      target1.dispatchEvent(event1);
+      target2.dispatchEvent(event2);
+
+      expect(listener1).toHaveBeenCalled();
+      expect(listener2).toHaveBeenCalled();
+    });
+
+    it('listens for an event with multiple listeners', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener2));
+
+      target1.dispatchEvent(event1);
+
+      expect(listener1).toHaveBeenCalled();
+      expect(listener2).toHaveBeenCalled();
+    });
   });
 
-  it('listens for an event from mutiple targets', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
+  describe('listenOnce', () => {
+    it('listens to an event only once', () => {
+      const listener1 = jasmine.createSpy('listener1');
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target2, 'eventtype1', Util.spyFunc(listener2));
+      eventManager.listenOnce(target1, 'eventtype1', Util.spyFunc(listener1));
 
-    target1.dispatchEvent(event1);
-    target2.dispatchEvent(event1);
+      target1.dispatchEvent(event1);
+      expect(listener1).toHaveBeenCalled();
+      listener1.calls.reset();
 
-    expect(listener1).toHaveBeenCalled();
-    expect(listener2).toHaveBeenCalled();
+      target1.dispatchEvent(event1);
+      expect(listener1).not.toHaveBeenCalled();
+    });
+
+    it('listens to an event with multiple listeners', () => {
+      const listener1 = jasmine.createSpy('listener1');
+      const listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listenOnce(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listenOnce(target1, 'eventtype1', Util.spyFunc(listener2));
+
+      target1.dispatchEvent(event1);
+
+      expect(listener1).toHaveBeenCalled();
+      expect(listener2).toHaveBeenCalled();
+    });
   });
 
-  it('listens for multiple events', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
+  describe('unlisten', () => {
+    it('stops listening to an event', function() {
+      let listener = jasmine.createSpy('listener');
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target1, 'eventtype2', Util.spyFunc(listener2));
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener));
+      eventManager.unlisten(target1, 'eventtype1');
 
-    target1.dispatchEvent(event1);
-    target1.dispatchEvent(event2);
+      target1.dispatchEvent(event1);
 
-    expect(listener1).toHaveBeenCalled();
-    expect(listener2).toHaveBeenCalled();
+      expect(listener).not.toHaveBeenCalled();
+    });
+
+    it('ignores other targets when removing listeners', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
+
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target2, 'eventtype1', Util.spyFunc(listener2));
+      eventManager.unlisten(target2, 'eventtype1');
+
+      target1.dispatchEvent(event1);
+
+      expect(listener1).toHaveBeenCalled();
+    });
   });
 
-  it('listens for multiple events from mutiple targets', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
+  describe('removeAll', () => {
+    it('stops listening to multiple events', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target2, 'eventtype2', Util.spyFunc(listener2));
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target1, 'eventtype2', Util.spyFunc(listener2));
 
-    target1.dispatchEvent(event1);
-    target2.dispatchEvent(event2);
+      eventManager.removeAll();
 
-    expect(listener1).toHaveBeenCalled();
-    expect(listener2).toHaveBeenCalled();
-  });
+      target1.dispatchEvent(event1);
+      target1.dispatchEvent(event2);
 
-  it('listens for an event with multiple listeners', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
+      expect(listener1).not.toHaveBeenCalled();
+      expect(listener2).not.toHaveBeenCalled();
+    });
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener2));
+    it('stops listening for an event with multiple listeners', function() {
+      let listener1 = jasmine.createSpy('listener1');
+      let listener2 = jasmine.createSpy('listener2');
 
-    target1.dispatchEvent(event1);
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
+      eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener2));
 
-    expect(listener1).toHaveBeenCalled();
-    expect(listener2).toHaveBeenCalled();
-  });
+      eventManager.removeAll();
 
-  it('stops listening to an event', function() {
-    let listener = jasmine.createSpy('listener');
+      target1.dispatchEvent(event1);
 
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener));
-    eventManager.unlisten(target1, 'eventtype1');
-
-    target1.dispatchEvent(event1);
-
-    expect(listener).not.toHaveBeenCalled();
-  });
-
-  it('ignores other targets when removing listeners', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
-
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target2, 'eventtype1', Util.spyFunc(listener2));
-    eventManager.unlisten(target2, 'eventtype1');
-
-    target1.dispatchEvent(event1);
-
-    expect(listener1).toHaveBeenCalled();
-  });
-
-  it('stops listening to multiple events', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
-
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target1, 'eventtype2', Util.spyFunc(listener2));
-
-    eventManager.removeAll();
-
-    target1.dispatchEvent(event1);
-    target1.dispatchEvent(event2);
-
-    expect(listener1).not.toHaveBeenCalled();
-    expect(listener2).not.toHaveBeenCalled();
-  });
-
-  it('stops listening for an event with multiple listeners', function() {
-    let listener1 = jasmine.createSpy('listener1');
-    let listener2 = jasmine.createSpy('listener2');
-
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener1));
-    eventManager.listen(target1, 'eventtype1', Util.spyFunc(listener2));
-
-    eventManager.removeAll();
-
-    target1.dispatchEvent(event1);
-
-    expect(listener1).not.toHaveBeenCalled();
-    expect(listener2).not.toHaveBeenCalled();
+      expect(listener1).not.toHaveBeenCalled();
+      expect(listener2).not.toHaveBeenCalled();
+    });
   });
 });
