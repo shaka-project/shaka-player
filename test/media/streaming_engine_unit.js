fc5f133d0dede29f071e22ed5f43df1ff7160deb
Jacob Trimble <modmaker@google.com>
2019-06-26 10:45:52 -0700

    Work around MediaSource duration bug.
    Fixes #1967
    
    Change-Id: Ie2a41cb17427316ce65cf180507c1fb1f2c6f11a

--- test/media/streaming_engine_unit.js
+++ test/media/streaming_engine_unit.js
@@ -988,6 +988,27 @@ describe('StreamingEngine', function() {
     expect(timeline.setDuration).not.toHaveBeenCalled();
   });
 
+  // https://github.com/google/shaka-player/issues/1967
+  it('does not change duration when 0', () => {
+    setupVod();
+    mediaSourceEngine = new shaka.test.FakeMediaSourceEngine(segmentData);
+    createStreamingEngine();
+
+    onStartupComplete.and.callFake(() => setupFakeGetTime(0));
+    onChooseStreams.and.callFake(defaultOnChooseStreams);
+
+    // The duration can spuriously be set to 0, so we should ignore this and not
+    // update the duration.
+    mediaSourceEngine.getDuration.and.returnValue(0);
+
+    // Here we go!
+    streamingEngine.start();
+
+    runTest();
+    expect(mediaSourceEngine.endOfStream).toHaveBeenCalled();
+    expect(timeline.setDuration).not.toHaveBeenCalled();
+  });
+
   it('applies fudge factor for appendWindowStart', function() {
     setupVod();
     mediaSourceEngine = new shaka.test.FakeMediaSourceEngine(segmentData);
