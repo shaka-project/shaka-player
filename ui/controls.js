shaka-project/shaka-player

commit fcbb72561f206dfcf76eb013ac40962cdfcc2d9a

Refactor UI range elements
This factors out common functionality and structure from the volume
bar and seek bar, both of which involved specific CSS workarounds for
IE.  The seek bar had logic to keep the value from jumping around
during casting, which now the volume bar benefits from, as well.
Finally, the seek bar code was spread out throughout controls.js, and
now it has its own class.

Closes #1913

Change-Id: I299476ccbc27f28f7b225a6e6f8b0d21abe5baf2

ui/seek_bar.js
@@ -1,0 +1,206 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+goog.provide('shaka.ui.SeekBar');
+
+goog.require('shaka.ui.Constants');
+goog.require('shaka.ui.Locales');
+goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.RangeElement');
+goog.require('shaka.ui.Utils');
+goog.require('shaka.util.Timer');
+
+
+/**
+ * @extends {shaka.ui.RangeElement}
+ * @final
+ * @export
+ */
+shaka.ui.SeekBar = class extends shaka.ui.RangeElement {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls,
+        ['shaka-seek-bar-container'],
+        [
+          'shaka-seek-bar',
+          'shaka-no-propagation',
+          'shaka-show-controls-on-mouse-over',
+        ]);
+
+    /**
+     * This timer is used to introduce a delay between the user scrubbing across
+     * the seek bar and the seek being sent to the player.
+     *
+     * @private {shaka.util.Timer}
+     */
+    this.seekTimer_ = new shaka.util.Timer(() => {
+      this.video.currentTime = this.getValue();
+    });
+
+    this.eventManager.listen(this.localization,
+        shaka.ui.Localization.LOCALE_UPDATED,
+        () => this.updateAriaLabel_());
+
+    this.eventManager.listen(this.localization,
+        shaka.ui.Localization.LOCALE_CHANGED,
+        () => this.updateAriaLabel_());
+
+    // Initialize seek state and label.
+    this.setValue(this.video.currentTime);
+    this.update();
+    this.updateAriaLabel_();
+  }
+
+  /** @override */
+  async destroy() {
+    if (this.seekTimer_) {
+      this.seekTimer_.stop();
+      this.seekTimer_ = null;
+    }
+
+    await super.destroy();
+  }
+
+  /**
+   * Called by the base class when user interaction with the input element
+   * begins.
+   *
+   * @override
+   */
+  onChangeStart() {
+    this.video.pause();
+  }
+
+  /**
+   * Update the video element's state to match the input element's state.
+   * Called by the base class when the input element changes.
+   *
+   * @override
+   */
+  onChange() {
+    if (!this.video.duration) {
+      // Can't seek yet.  Ignore.
+      return;
+    }
+
+    // Update the UI right away.
+    this.update();
+
+    // We want to wait until the user has stopped moving the seek bar for a
+    // little bit to reduce the number of times we ask the player to seek.
+    //
+    // To do this, we will start a timer that will fire in a little bit, but if
+    // we see another seek bar change, we will cancel that timer and re-start
+    // it.
+    //
+    // Calling |start| on an already pending timer will cancel the old request
+    // and start the new one.
+    this.seekTimer_.tickAfter(/* seconds= */ 0.125);
+  }
+
+  /**
+   * Called by the base class when user interaction with the input element
+   * ends.
+   *
+   * @override
+   */
+  onChangeEnd() {
+    // They just let go of the seek bar, so cancel the timer and manually
+    // call the event so that we can respond immediately.
+    this.seekTimer_.tickNow();
+
+    this.video.play();
+  }
+
+  /** @return {boolean} */
+  isShowing() {
+    // It is showing by default, so it is hidden if shaka-hidden is in the list.
+    return !this.container.classList.contains('shaka-hidden');
+  }
+
+  /**
+   * Called by Controls on a timer to update the state of the seek bar.
+   * Also called internally when the user interacts with the input element.
+   */
+  update() {
+    const Constants = shaka.ui.Constants;
+
+    const currentTime = this.getValue();
+    const bufferedLength = this.video.buffered.length;
+    const bufferedStart = bufferedLength ? this.video.buffered.start(0) : 0;
+    const bufferedEnd =
+        bufferedLength ? this.video.buffered.end(bufferedLength - 1) : 0;
+
+    const seekRange = this.player.seekRange();
+    const seekRangeSize = seekRange.end - seekRange.start;
+
+    this.setRange(seekRange.start, seekRange.end);
+
+    // Hide seekbar if the seek window is very small.
+    if (this.player.isLive() &&
+        seekRangeSize < Constants.MIN_SEEK_WINDOW_TO_SHOW_SEEKBAR) {
+      shaka.ui.Utils.setDisplay(this.container, false);
+    } else {
+      shaka.ui.Utils.setDisplay(this.container, true);
+
+      const gradient = ['to right'];
+      if (bufferedLength == 0) {
+        gradient.push('#000 0%');
+      } else {
+        const clampedBufferStart = Math.max(bufferedStart, seekRange.start);
+        const clampedBufferEnd = Math.min(bufferedEnd, seekRange.end);
+        const clampedCurrentTime = Math.min(
+            Math.max(currentTime, seekRange.start),
+            seekRange.end);
+
+        const bufferStartDistance = clampedBufferStart - seekRange.start;
+        const bufferEndDistance = clampedBufferEnd - seekRange.start;
+        const playheadDistance = clampedCurrentTime - seekRange.start;
+
+        // NOTE: the fallback to zero eliminates NaN.
+        const bufferStartFraction = (bufferStartDistance / seekRangeSize) || 0;
+        const bufferEndFraction = (bufferEndDistance / seekRangeSize) || 0;
+        const playheadFraction = (playheadDistance / seekRangeSize) || 0;
+
+        gradient.push(Constants.SEEK_BAR_BASE_COLOR + ' ' +
+                     (bufferStartFraction * 100) + '%');
+        gradient.push(Constants.SEEK_BAR_PLAYED_COLOR + ' ' +
+                     (bufferStartFraction * 100) + '%');
+        gradient.push(Constants.SEEK_BAR_PLAYED_COLOR + ' ' +
+                     (playheadFraction * 100) + '%');
+        gradient.push(Constants.SEEK_BAR_BUFFERED_COLOR + ' ' +
+                     (playheadFraction * 100) + '%');
+        gradient.push(Constants.SEEK_BAR_BUFFERED_COLOR + ' ' +
+                     (bufferEndFraction * 100) + '%');
+        gradient.push(Constants.SEEK_BAR_BASE_COLOR + ' ' +
+                     (bufferEndFraction * 100) + '%');
+      }
+      this.container.style.background =
+          'linear-gradient(' + gradient.join(',') + ')';
+    }
+  }
+
+  /** @private */
+  updateAriaLabel_() {
+    this.bar.setAttribute(shaka.ui.Constants.ARIA_LABEL,
+        this.localization.resolve(shaka.ui.Locales.Ids.SEEK));
+  }
+};

build/types/ui
@@ -17,8 +17,10 @@
 +../../ui/pip_button.js
 +../../ui/play_pause_button.js
 +../../ui/presentation_time.js
++../../ui/range_element.js
 +../../ui/resolution_selection.js
 +../../ui/rewind_button.js
++../../ui/seek_bar.js
 +../../ui/spacer.js
 +../../ui/text_displayer.js
 +../../ui/text_selection.js

ui/less/range_elements.less
@@ -157,9 +157,11 @@
   }
 }
 
-.shaka-volume-bar-container {
+.shaka-range-container {
   .range-container();
+}
 
+.shaka-volume-bar-container {
   width: 100px;
 
   /* Hide volume slider on mobile-sized screens. */
@@ -169,16 +171,10 @@
 }
 
 .shaka-seek-bar-container {
-  .range-container();
-
   /* TODO: Document why! */
   width: 96.5%;
 }
 
-.shaka-seek-bar {
-  .range-element();
-}
-
-.shaka-volume-bar {
+.shaka-range-element {
   .range-element();
 }

ui/range_element.js
@@ -1,0 +1,142 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+goog.provide('shaka.ui.RangeElement');
+
+goog.require('shaka.ui.Element');
+goog.require('shaka.util.Dom');
+
+
+/**
+ * A range element, built to work across browsers.
+ *
+ * In particular, getting styles to work right on IE requires a specific
+ * structure.
+ *
+ * This also handles the case where the range element is being manipulated and
+ * updated at the same time.  This can happen when seeking during playback or
+ * when casting.
+ *
+ * @extends {shaka.ui.Element}
+ * @export
+ */
+shaka.ui.RangeElement = class extends shaka.ui.Element {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   * @param {!Array.<string>} containerClassNames
+   * @param {!Array.<string>} barClassNames
+   */
+  constructor(parent, controls, containerClassNames, barClassNames) {
+    super(parent, controls);
+
+    /**
+     * This container is to support IE 11.  See detailed notes in
+     * less/range_elements.less for a complete explanation.
+     * @protected {!HTMLElement}
+     */
+    this.container = shaka.util.Dom.createHTMLElement('div');
+    this.container.classList.add('shaka-range-container');
+    this.container.classList.add(...containerClassNames);
+
+    /** @private {boolean} */
+    this.isChanging_ = false;
+
+    /** @protected {!HTMLInputElement} */
+    this.bar =
+      /** @type {!HTMLInputElement} */ (document.createElement('input'));
+
+    this.bar.classList.add('shaka-range-element');
+    this.bar.classList.add(...barClassNames);
+    this.bar.type = 'range';
+    // TODO(#2027): step=any causes keyboard nav problems on IE 11.
+    this.bar.step = 'any';
+    this.bar.min = '0';
+    this.bar.max = '1';
+    this.bar.value = '0';
+
+    this.container.appendChild(this.bar);
+    this.parent.appendChild(this.container);
+
+    this.eventManager.listen(this.bar, 'mousedown', () => {
+      this.isChanging_ = true;
+      this.onChangeStart();
+    });
+
+    this.eventManager.listen(this.bar, 'touchstart', () => {
+      this.isChanging_ = true;
+      this.onChangeStart();
+    }, {passive: true});
+
+    this.eventManager.listen(this.bar, 'input', () => {
+      this.onChange();
+    });
+
+    this.eventManager.listen(this.bar, 'touchend', () => {
+      this.isChanging_ = false;
+      this.onChangeEnd();
+    });
+
+    this.eventManager.listen(this.bar, 'mouseup', () => {
+      this.isChanging_ = false;
+      this.onChangeEnd();
+    });
+  }
+
+  /**
+   * @param {number} min
+   * @param {number} max
+   */
+  setRange(min, max) {
+    this.bar.min = min;
+    this.bar.max = max;
+  }
+
+  /**
+   * Called when user interaction begins.
+   * To be overridden by subclasses.
+   */
+  onChangeStart() {}
+
+  /**
+   * Called when a new value is set by user interaction.
+   * To be overridden by subclasses.
+   */
+  onChange() {}
+
+  /**
+   * Called when user interaction ends.
+   * To be overridden by subclasses.
+   */
+  onChangeEnd() {}
+
+  /** @return {number} */
+  getValue() {
+    return parseFloat(this.bar.value);
+  }
+
+  /** @param {number} value */
+  setValue(value) {
+    // The user interaction overrides any external values being pushed in.
+    if (this.isChanging_) {
+      return;
+    }
+
+    this.bar.value = value;
+  }
+};

ui/volume_bar.js
@@ -18,95 +18,51 @@
 
 goog.provide('shaka.ui.VolumeBar');
 
-goog.require('shaka.ui.Element');
+goog.require('shaka.ui.Constants');
 goog.require('shaka.ui.Locales');
 goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.RangeElement');
 
 
 /**
- * @extends {shaka.ui.Element}
+ * @extends {shaka.ui.RangeElement}
  * @final
  * @export
  */
-shaka.ui.VolumeBar = class extends shaka.ui.Element {
+shaka.ui.VolumeBar = class extends shaka.ui.RangeElement {
   /**
    * @param {!HTMLElement} parent
    * @param {!shaka.ui.Controls} controls
    */
   constructor(parent, controls) {
-    super(parent, controls);
+    super(parent, controls,
+        ['shaka-volume-bar-container'], ['shaka-volume-bar']);
 
-    // This container is to support IE 11.  See detailed notes in
-    // less/range_elements.less for a complete explanation.
-    // TODO: Factor this into a range-element component.
-    /** @private {!HTMLElement} */
-    this.container_ = shaka.util.Dom.createHTMLElement('div');
-    this.container_.classList.add('shaka-volume-bar-container');
+    this.eventManager.listen(this.video,
+        'volumechange',
+        () => this.onVolumeStateChange_());
 
-    this.bar_ =
-      /** @type {!HTMLInputElement} */ (document.createElement('input'));
-    this.bar_.classList.add('shaka-volume-bar');
-    this.bar_.setAttribute('type', 'range');
-    // NOTE: step=any causes keyboard nav problems on IE 11.
-    this.bar_.setAttribute('step', 'any');
-    this.bar_.setAttribute('min', '0');
-    this.bar_.setAttribute('max', '1');
-    this.bar_.setAttribute('value', '0');
+    this.eventManager.listen(this.localization,
+        shaka.ui.Localization.LOCALE_UPDATED,
+        () => this.updateAriaLabel_());
 
-    this.container_.appendChild(this.bar_);
-    this.parent.appendChild(this.container_);
-    this.updateAriaLabel_();
+    this.eventManager.listen(this.localization,
+        shaka.ui.Localization.LOCALE_CHANGED,
+        () => this.updateAriaLabel_());
 
-    this.eventManager.listen(this.video, 'volumechange', () => {
-      this.onVolumeStateChange_();
-    });
-
-    this.eventManager.listen(this.bar_, 'input', () => {
-      this.onVolumeInput_();
-    });
-
-    this.eventManager.listen(
-        this.localization, shaka.ui.Localization.LOCALE_UPDATED, () => {
-          this.updateAriaLabel_();
-        });
-
-    this.eventManager.listen(
-        this.localization, shaka.ui.Localization.LOCALE_CHANGED, () => {
-          this.updateAriaLabel_();
-        });
-
-    // Initialize volume display with a fake event.
+    // Initialize volume display and label.
     this.onVolumeStateChange_();
+    this.updateAriaLabel_();
   }
 
-
   /**
-   * @private
+   * Update the video element's state to match the input element's state.
+   * Called by the base class when the input element changes.
+   *
+   * @override
    */
-  onVolumeStateChange_() {
-    if (this.video.muted) {
-      this.bar_.value = 0;
-    } else {
-      this.bar_.value = this.video.volume;
-    }
-
-    // TODO: Can we do this with LESS?
-    const gradient = ['to right'];
-    gradient.push(shaka.ui.Constants.VOLUME_BAR_VOLUME_LEVEL_COLOR +
-                 (this.bar_.value * 100) + '%');
-    gradient.push(shaka.ui.Constants.VOLUME_BAR_BASE_COLOR +
-                 (this.bar_.value * 100) + '%');
-    gradient.push(shaka.ui.Constants.VOLUME_BAR_BASE_COLOR + '100%');
-    this.container_.style.background =
-        'linear-gradient(' + gradient.join(',') + ')';
-  }
-
-
-  /**
-   * @private
-   */
-  onVolumeInput_() {
-    this.video.volume = parseFloat(this.bar_.value);
+  onChange() {
+    this.video.volume = this.getValue();
     if (this.video.volume == 0) {
       this.video.muted = true;
     } else {
@@ -114,12 +70,28 @@ shaka.ui.VolumeBar = class extends shaka.ui.Element {
     }
   }
 
+  /** @private */
+  onVolumeStateChange_() {
+    if (this.video.muted) {
+      this.setValue(0);
+    } else {
+      this.setValue(this.video.volume);
+    }
 
-  /**
-   * @private
-   */
+    const gradient = ['to right'];
+    gradient.push(shaka.ui.Constants.VOLUME_BAR_VOLUME_LEVEL_COLOR +
+                 (this.getValue() * 100) + '%');
+    gradient.push(shaka.ui.Constants.VOLUME_BAR_BASE_COLOR +
+                 (this.getValue() * 100) + '%');
+    gradient.push(shaka.ui.Constants.VOLUME_BAR_BASE_COLOR + '100%');
+
+    this.container.style.background =
+        'linear-gradient(' + gradient.join(',') + ')';
+  }
+
+  /** @private */
   updateAriaLabel_() {
-    this.bar_.setAttribute(shaka.ui.Constants.ARIA_LABEL,
+    this.bar.setAttribute(shaka.ui.Constants.ARIA_LABEL,
         this.localization.resolve(shaka.ui.Locales.Ids.VOLUME));
   }
 };

commit 7c271baba529a188210405c6dac548626dfaf33b

Communicate seeking state to controls.
Regression from fcbb725. Other elements rely on controls
knowing current seek state. Now that seek bar is its own
class, it needs to update controls on that.

Change-Id: I29ae8faa9f830ef86380d6b07373dedbeebce263

ui/controls.js
@@ -491,6 +491,15 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
     return this.isSeeking_;
   }
 
+
+  /**
+   * @param {boolean} seeking
+   * @export
+   */
+  setSeeking(seeking) {
+    this.isSeeking_ = seeking;
+  }
+
   /**
    * @return {boolean}
    * @export

ui/seek_bar.js
@@ -86,6 +86,7 @@ shaka.ui.SeekBar = class extends shaka.ui.RangeElement {
    * @override
    */
   onChangeStart() {
+    this.controls.setSeeking(true);
     this.video.pause();
   }
 
@@ -126,7 +127,7 @@ shaka.ui.SeekBar = class extends shaka.ui.RangeElement {
     // They just let go of the seek bar, so cancel the timer and manually
     // call the event so that we can respond immediately.
     this.seekTimer_.tickNow();
-
+    this.controls.setSeeking(false);
     this.video.play();
   }
 