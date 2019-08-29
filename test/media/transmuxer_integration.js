d87d57e806540833cde26d398e8df8b4e544c2c2
Jacob Trimble <modmaker@google.com>
2019-08-29 09:41:08 -0700

    Fix transmuxer test with latest mux.js.
    mux.js version 5.2.0 broke our test since we were expecting a non-empty
    segment to be returned.  Since it should work fine if we append an
    empty MP4 segment, we should just allow it.  This changes the test to
    not expect any specific value for the buffer.
    
    Fixes: 140235880
    Change-Id: I18ed4347c05553e7abcb967beb684d8ad1a2a187

--- test/media/transmuxer_integration.js
+++ test/media/transmuxer_integration.js
@@ -137,8 +137,7 @@ describe('Transmuxer', function() {
       let sawMDAT = false;
       let transmuxedData = await transmuxer.transmux(emptySegment);
       expect(transmuxedData.data).toEqual(jasmine.any(Uint8Array));
-      expect(transmuxedData.data.length).toBeGreaterThan(0);
-      expect(transmuxedData.captions).toEqual(jasmine.any(Array));
+      expect(transmuxedData.captions).toEqual([]);
       new shaka.util.Mp4Parser()
           .box('mdat', shaka.util.Mp4Parser.allData(function(data) {
             sawMDAT = true;
