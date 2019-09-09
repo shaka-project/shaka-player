0403d362c80d7eb08c155aac085c7ddec195a097
Jacob Trimble <modmaker@google.com>
2019-09-09 14:05:49 -0700

    Don't generate compiled assets in some tests.
    These tests don't use the compiled assets, and this line doesn't even
    build the assets for the compiled library, it uses the uncompiled
    library.
    
    Change-Id: Iff5c74c7ef258a18c29a3011266ce459f9e5cf75

--- test/player_integration.js
+++ test/player_integration.js
@@ -696,11 +696,9 @@ describe('Player Load Path', () => {
   /** @type {!jasmine.Spy} */
   let stateIdleSpy;
 
-  beforeAll(async () => {
+  beforeAll(() => {
     video = shaka.util.Dom.createVideoElement();
     document.body.appendChild(video);
-
-    await shaka.test.TestScheme.createManifests(shaka, '_compiled');
   });
 
   afterAll(() => {
