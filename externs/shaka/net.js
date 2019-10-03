4f7383c93799b8a31f79b3eb1b36e4d424072613
alex-gusev <alex-gusev@yandex-team.ru>
2019-10-03 21:18:27 +0300

    pass total size of pending request to shouldAbortCurrentRequest_

--- externs/shaka/net.js
+++ externs/shaka/net.js
@@ -155,7 +155,7 @@ shaka.extern.SchemePlugin;
 
 
 /**
- * @typedef {function(number, number, number)}
+ * @typedef {function(number, number, number, number)}
  *
  * @description
  * A callback function to handle progress event through networking engine in
