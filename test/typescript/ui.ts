1c9f21bd287f71df3605ed96fd74c641ced974a3
Niklas Korz <korz.niklask@gmail.com>
2019-11-29 19:01:54 +0100

    Update ui ts test

--- test/typescript/ui.ts
+++ test/typescript/ui.ts
@@ -14,6 +14,7 @@ class SkipButton extends shaka.ui.Element {
     super(parent, controls);
 
     this.button.textContent = "Skip current video";
+    this.button.setAttribute("aria-label", "Skip");
     this.parent.appendChild(this.button);
     this.eventManager.listen(this.button, "click", this.onClick);
   }
