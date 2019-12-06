aadee88d9049227186ad3c3189151eef1df9e4c2
Alvaro Velad <alvaro.velad@mirada.tv>
2019-12-06 08:56:39 +0100

    Add test

--- test/dash/dash_parser_manifest_unit.js
+++ test/dash/dash_parser_manifest_unit.js
@@ -1370,4 +1370,36 @@ describe('DashParser Manifest', () => {
     expect(textStream.roles).toEqual(['captions', 'foo']);
     expect(textStream.kind).toBe('caption');
   });
+
+  // Filter image adaptation sets, until we add thumbnail track support.
+  // See: https://github.com/google/shaka-player/issues/559
+  it('Filter image adaptation sets', async () => {
+    const manifestText = [
+      '<MPD minBufferTime="PT75S">',
+      '  <Period id="1" duration="PT30S">',
+      '    <AdaptationSet id="2" mimeType="video/mp4">',
+      '      <Representation id="video-sd" width="640" height="480">',
+      '        <BaseURL>v-sd.mp4</BaseURL>',
+      '        <SegmentBase indexRange="100-200" />',
+      '      </Representation>',
+      '    </AdaptationSet>',
+      '    <AdaptationSet id="3" mimeType="audio/mp4">',
+      '      <Representation id="audio-en">',
+      '        <BaseURL>a-en.mp4</BaseURL>',
+      '        <SegmentBase indexRange="100-200" />',
+      '      </Representation>',
+      '    </AdaptationSet>',
+      '    <AdaptationSet contentType="image" id="3">',
+      '    </AdaptationSet>',
+      '  </Period>',
+      '</MPD>',
+    ].join('\n');
+
+    fakeNetEngine.setResponseText('dummy://foo', manifestText);
+
+    const manifest = await parser.start('dummy://foo', playerInterface);
+    const variant = manifest.periods[0].variants[0];
+    expect(variant.audio).toBeTruthy();
+    expect(variant.video).toBeTruthy();
+  });
 });
