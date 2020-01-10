7ed74fa8a9af64281daa8aa1428f3a74e5ff358f
Joey Parrish <joeyparrish@google.com>
2020-01-10 12:14:56 -0800

    Update asset list and metadata
    Several disabled assets can now be played, and so are re-enabled.
    Some others are buggy and had to be newly disabled.
    
    This also adds a new Nimble Streamer asset, updates comments around
    disabling, and corrects one title.
    
    See also Dash-Industry-Forum/dash-live-source-simulator#91 and
    Axinom/public-test-vectors#16.
    
    Backported to v2.5.x
    
    Change-Id: I3a112d37eb1b3a79fc8849c5480bc38b7a433022

--- demo/common/assets.js
+++ demo/common/assets.js
@@ -79,6 +79,7 @@ shakaAssets.Feature = {
   MULTIPLE_LANGUAGES: 'multiple languages',
   // Set if the asset is audio-only.
   AUDIO_ONLY: 'audio only',
+  // Set if the asset can be stored offline.
   OFFLINE: 'Downloadable',
   // A synthetic property used in the search tab. Should not be given to assets.
   STORED: 'Downloaded',
@@ -346,7 +347,7 @@ shakaAssets.testAssets = [
     .addFeature(shakaAssets.Feature.OFFLINE)
     .addLicenseServer('com.widevine.alpha', 'https://cwip-shaka-proxy.appspot.com/no_auth'),
   new ShakaDemoAssetInfo(
-      /* name= */ 'Sintel 4k (multicodec, VTT in MP4)',
+      /* name= */ 'Sintel 4k (MP4, VTT in MP4)',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/sintel.png',
       /* manifestUri= */ 'https://storage.googleapis.com/shaka-demo-assets/sintel-mp4-wvtt/dash.mpd',
       /* source= */ shakaAssets.Source.SHAKA)
@@ -559,11 +560,11 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/axinom_test.png',
       /* manifestUri= */ 'https://akamai-axtest.akamaized.net/routes/lapd-v1-acceptance/www_c4/Manifest.mpd',
       /* source= */ shakaAssets.Source.AXINOM)
+    // Disabled pending resolution of https://github.com/Axinom/public-test-vectors/issues/16
+    .markAsDisabled()
     .addFeature(shakaAssets.Feature.LIVE)
     .addFeature(shakaAssets.Feature.MP4)
-    .addFeature(shakaAssets.Feature.DASH)
-    .addFeature(shakaAssets.Feature.MULTIPERIOD)
-    .addFeature(shakaAssets.Feature.WEBVTT),
+    .addFeature(shakaAssets.Feature.DASH),
   new ShakaDemoAssetInfo(
       /* name= */ 'Clear, Live HLS',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/axinom_test.png',
@@ -657,6 +658,8 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/dash_if_test_pattern.png',
       /* manifestUri= */ 'https://livesim.dashif.org/livesim/segtimeline_1/utc_head/testpic_6s/Manifest.mpd',
       /* source= */ shakaAssets.Source.DASH_IF)
+    // Disabled pending resolution of https://github.com/Dash-Industry-Forum/dash-live-source-simulator/issues/91
+    .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.LIVE)
     .addFeature(shakaAssets.Feature.MP4),
@@ -665,6 +668,8 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/dash_if_test_pattern.png',
       /* manifestUri= */ 'https://livesim.dashif.org/livesim/segtimelinenr_1/utc_head/testpic_6s/Manifest.mpd',
       /* source= */ shakaAssets.Source.DASH_IF)
+    // Disabled pending resolution of https://github.com/Dash-Industry-Forum/dash-live-source-simulator/issues/91
+    .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.LIVE)
     .addFeature(shakaAssets.Feature.MP4),
@@ -731,7 +736,6 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/art_of_motion.png',
       /* manifestUri= */ 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
       /* source= */ shakaAssets.Source.BITCODIN)
-    .markAsDisabled()
     .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
     .addFeature(shakaAssets.Feature.HLS)
     .addFeature(shakaAssets.Feature.MP2TS)
@@ -741,27 +745,33 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/sintel.png',
       /* manifestUri= */ 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
       /* source= */ shakaAssets.Source.BITCODIN)
-    .markAsDisabled()
     .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
     .addFeature(shakaAssets.Feature.HLS)
     .addFeature(shakaAssets.Feature.MP2TS)
     .addFeature(shakaAssets.Feature.OFFLINE),
-  // }}}
+  // End bitcodin assets }}}
 
   // Nimble Streamer assets {{{
-  // Src: http://www.dash-player.com/demo/streaming-server-and-encoder-support/
-  // As of 2017-08-04, there is a common name mismatch error with this site's
-  // SSL certificate.  See https://github.com/google/shaka-player/issues/955
+  // Src: https://wmspanel.com/nimble/demo
   new ShakaDemoAssetInfo(
-      /* name= */ 'Big Buck Bunny (Nimble)',
+      /* name= */ 'Big Buck Bunny (Nimble, DASH)',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/big_buck_bunny.png',
-      /* manifestUri= */ 'https://video.wmspanel.com/local/raw/BigBuckBunny_320x180.mp4/manifest.mpd',
+      /* manifestUri= */ 'https://cf-sf-video.wmspanel.com/local/raw/BigBuckBunny_320x180.mp4/manifest.mpd',
       /* source= */ shakaAssets.Source.NIMBLE_STREAMER)
-    .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4)
-    .addFeature(shakaAssets.Feature.HIGH_DEFINITION),
-  // }}}
+    .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
+    .addFeature(shakaAssets.Feature.OFFLINE),
+  new ShakaDemoAssetInfo(
+      /* name= */ 'Big Buck Bunny (Nimble, HLS)',
+      /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/big_buck_bunny.png',
+      /* manifestUri= */ 'https://cf-sf-video.wmspanel.com/local/raw/BigBuckBunny_320x180.mp4/playlist.m3u8',
+      /* source= */ shakaAssets.Source.NIMBLE_STREAMER)
+    .addFeature(shakaAssets.Feature.HLS)
+    .addFeature(shakaAssets.Feature.MP2TS)
+    .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
+    .addFeature(shakaAssets.Feature.OFFLINE),
+  // End Nimble Streamer assets }}}
 
   // Azure Media Services assets {{{
   // Src: http://amp.azure.net/libs/amp/latest/docs/samples.html
@@ -821,15 +831,15 @@ shakaAssets.testAssets = [
   // "live streams".  The content is still static, as is the timeline.
 
   // TODO: Get actual icon?
-  // NOTE: Multiple SPS/PPS in init segment, no sample duration
-  // NOTE: Decoder errors on Mac
-  // https://github.com/gpac/gpac/issues/600
-  // https://bugs.webkit.org/show_bug.cgi?id=160459
   new ShakaDemoAssetInfo(
       /* name= */ 'live profile',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/sintel.png',
       /* manifestUri= */ 'https://download.tsi.telecom-paristech.fr/gpac/DASH_CONFORMANCE/TelecomParisTech/mp4-live/mp4-live-mpd-AV-BS.mpd',
       /* source= */ shakaAssets.Source.GPAC)
+    // NOTE: Multiple SPS/PPS in init segment, no sample duration
+    // NOTE: Decoder errors on Mac
+    // https://github.com/gpac/gpac/issues/600
+    // https://bugs.webkit.org/show_bug.cgi?id=160459
     .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4),
@@ -852,15 +862,15 @@ shakaAssets.testAssets = [
     .addFeature(shakaAssets.Feature.MP4)
     .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
     .addFeature(shakaAssets.Feature.OFFLINE),
-  // NOTE: Multiple SPS/PPS in init segment, no sample duration
-  // NOTE: Decoder errors on Mac
-  // https://github.com/gpac/gpac/issues/600
-  // https://bugs.webkit.org/show_bug.cgi?id=160459
   new ShakaDemoAssetInfo(
       /* name= */ 'main profile, multiple files',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/gpac_test_pattern.png',
       /* manifestUri= */ 'https://download.tsi.telecom-paristech.fr/gpac/DASH_CONFORMANCE/TelecomParisTech/mp4-main-multi/mp4-main-multi-mpd-AV-BS.mpd',
       /* source= */ shakaAssets.Source.GPAC)
+    // NOTE: Multiple SPS/PPS in init segment, no sample duration
+    // NOTE: Decoder errors on Mac
+    // https://github.com/gpac/gpac/issues/600
+    // https://bugs.webkit.org/show_bug.cgi?id=160459
     .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4),
@@ -873,29 +883,29 @@ shakaAssets.testAssets = [
     .addFeature(shakaAssets.Feature.MP4)
     .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
     .addFeature(shakaAssets.Feature.OFFLINE),
-  // NOTE: Segments do not start with keyframes
-  // NOTE: Decoder errors on Safari
-  // https://bugs.webkit.org/show_bug.cgi?id=160460
   new ShakaDemoAssetInfo(
       /* name= */ 'main profile, open GOP',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/gpac_test_pattern.png',
       /* manifestUri= */ 'https://download.tsi.telecom-paristech.fr/gpac/DASH_CONFORMANCE/TelecomParisTech/mp4-main-ogop/mp4-main-ogop-mpd-AV-BS.mpd',
       /* source= */ shakaAssets.Source.GPAC)
+    // NOTE: Segments do not start with keyframes
+    // NOTE: Decoder errors on Safari
+    // https://bugs.webkit.org/show_bug.cgi?id=160460
     .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4),
-  // NOTE: segments do not start with keyframes
-  // NOTE: Decoder errors on Safari
-  // https://bugs.webkit.org/show_bug.cgi?id=160460
   new ShakaDemoAssetInfo(
       /* name= */ 'full profile, gradual decoding refresh',
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/gpac_test_pattern.png',
       /* manifestUri= */ 'https://download.tsi.telecom-paristech.fr/gpac/DASH_CONFORMANCE/TelecomParisTech/mp4-full-gdr/mp4-full-gdr-mpd-AV-BS.mpd',
       /* source= */ shakaAssets.Source.GPAC)
+    // NOTE: Segments do not start with keyframes
+    // NOTE: Decoder errors on Safari
+    // https://bugs.webkit.org/show_bug.cgi?id=160460
     .markAsDisabled()
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4),
-  // }}}
+  // End GPAC assets }}}
 
   // Verizon Digital Media Services (VDMS) assets {{{
   // Reliable Playready playback requires Edge 16+
@@ -942,8 +952,6 @@ shakaAssets.testAssets = [
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/big_buck_bunny.png',
       /* manifestUri= */ 'https://content.uplynk.com/224ac8717e714b68831997ab6cea4015.mpd',
       /* source= */ shakaAssets.Source.UPLYNK)
-    // Disabled until we figure out the CORS errors and PlayReady status.
-    .markAsDisabled()
     .addKeySystem(shakaAssets.KeySystem.WIDEVINE)
     .addFeature(shakaAssets.Feature.DASH)
     .addFeature(shakaAssets.Feature.MP4)
@@ -953,27 +961,23 @@ shakaAssets.testAssets = [
     .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
     .setRequestFilter(shakaAssets.UplynkRequestFilter)
     .setResponseFilter(shakaAssets.UplynkResponseFilter),
-  // Unencrypted periods interspersed with protected periods
-  // Doesn't work on Chrome < 58
   new ShakaDemoAssetInfo(
       /* name= */ 'Widevine - 16 Byte IV - (mix of encrypted and unencrypted periods)', // eslint-disable-line max-len
       /* iconUri= */ 'https://storage.googleapis.com/shaka-asset-icons/sintel.png',
       /* manifestUri= */ 'https://content.uplynk.com/1eb40d8e64234f5c9879db7045c3d48c.mpd?ad=cleardash&rays=cdefg',
       /* source= */ shakaAssets.Source.UPLYNK)
-  // Disabled until we figure out the CORS errors and PlayReady status.
-      .markAsDisabled()
-      .addKeySystem(shakaAssets.KeySystem.WIDEVINE)
-      .addFeature(shakaAssets.Feature.DASH)
-      .addFeature(shakaAssets.Feature.MP4)
-      .addFeature(shakaAssets.Feature.MULTIPLE_LANGUAGES)
-      .addFeature(shakaAssets.Feature.MULTIPERIOD)
-      .addFeature(shakaAssets.Feature.MULTIKEY)
-      .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
-      .addFeature(shakaAssets.Feature.AESCTR_16_BYTE_IV)
-      .addFeature(shakaAssets.Feature.ENCRYPTED_WITH_CLEAR)
-      .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
-      .setRequestFilter(shakaAssets.UplynkRequestFilter)
-      .setResponseFilter(shakaAssets.UplynkResponseFilter),
+    .addKeySystem(shakaAssets.KeySystem.WIDEVINE)
+    .addFeature(shakaAssets.Feature.DASH)
+    .addFeature(shakaAssets.Feature.MP4)
+    .addFeature(shakaAssets.Feature.MULTIPLE_LANGUAGES)
+    .addFeature(shakaAssets.Feature.MULTIPERIOD)
+    .addFeature(shakaAssets.Feature.MULTIKEY)
+    .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
+    .addFeature(shakaAssets.Feature.AESCTR_16_BYTE_IV)
+    .addFeature(shakaAssets.Feature.ENCRYPTED_WITH_CLEAR)
+    .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
+    .setRequestFilter(shakaAssets.UplynkRequestFilter)
+    .setResponseFilter(shakaAssets.UplynkResponseFilter),
   // End Verizon Digital Media Services (VDMS) assets }}}
 
   // Apple assets {{{
