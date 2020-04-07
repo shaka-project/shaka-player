94f07dd410051987da41b82c0bc4b0998a260a11
Joey Parrish <joeyparrish@google.com>
2020-04-07 14:27:35 -0700

    Fix ratechange events w/ src= playback
    The Player ratechange event is a filtered version of the video
    ratechange event.  This is necessary because we manipulate
    playbackRate as part of our buffering state management.
    
    The UI uses this Player ratechange event to drive the playback rate UI
    menu, but the event was not firing for src= playbacks (native HLS or
    plain MP4s).
    
    This fixes the ratechange event for src= playbacks, and also fixes a
    small mistake in the MediaSource setup that could have resulted in the
    ratechange event being triggered before playRateController_ was
    created.
    
    Closes #2488
    
    Change-Id: If85e489d681cfbb1ae6141b490d82264e1d932bd

--- test/player_src_equals_integration.js
+++ test/player_src_equals_integration.js
@@ -160,9 +160,24 @@ describe('Player Src Equals', () => {
     video.play();
     await waitForMovementOrFailOnTimeout(eventManager, video, /* timeout= */10);
 
+    let videoRateChange = false;
+    let playerRateChange = false;
+    eventManager.listen(video, 'ratechange', () => {
+      videoRateChange = true;
+    });
+    eventManager.listen(player, 'ratechange', () => {
+      playerRateChange = true;
+    });
+
     // Enabling trick play should change our playback rate to the same rate.
     player.trickPlay(2);
     expect(video.playbackRate).toBe(2);
+
+    // It should also have fired a 'ratechange' event on both video and player.
+    // We may have to delay a short time to see the events, though.
+    await shaka.test.Util.shortDelay();
+    expect(videoRateChange).toBe(true);
+    expect(playerRateChange).toBe(true);
 
     // Let playback continue playing for a bit longer.
     await shaka.test.Util.delay(2);
