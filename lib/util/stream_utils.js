shaka-project/shaka-player

commit 6e8ab0132fa2b04d5cfc15a18165ec7c0a9b6a83

Add framerate restriction to the config (#2332)
Closes #2068

lib/util/stream_utils.js
@@ -60,6 +60,16 @@ shaka.util.StreamUtils = class {
       }
     }
 
+    // |variant.frameRate| can be undefined, which breaks
+    // the math, so make sure they are there first.
+    if (variant && variant.frameRate) {
+      if (!inRange(variant.frameRate,
+          restrictions.minFrameRate,
+          restrictions.maxFrameRate)) {
+        return false;
+      }
+    }
+
     if (!inRange(variant.bandwidth,
         restrictions.minBandwidth,
         restrictions.maxBandwidth)) {

demo/common/message_ids.js
@@ -146,6 +146,8 @@ shakaDemo.MessageIds = {
   MAX_HEIGHT: 'DEMO_MAX_HEIGHT',
   MIN_PIXELS: 'DEMO_MIN_PIXELS',
   MAX_PIXELS: 'DEMO_MAX_PIXELS',
+  MIN_FRAMERATE: 'DEMO_MIN_FRAMERATE',
+  MAX_FRAMERATE: 'DEMO_MAX_FRAMERATE',
   MIN_BANDWIDTH: 'DEMO_MIN_BANDWIDTH',
   MAX_BANDWIDTH: 'DEMO_MAX_BANDWIDTH',
   MAX_ATTEMPTS: 'DEMO_MAX_ATTEMPTS',

demo/config.js
@@ -241,6 +241,8 @@ shakaDemo.Config = class {
         .addNumberInput_(MessageIds.MAX_HEIGHT, prefix + 'maxHeight')
         .addNumberInput_(MessageIds.MIN_PIXELS, prefix + 'minPixels')
         .addNumberInput_(MessageIds.MAX_PIXELS, prefix + 'maxPixels')
+        .addNumberInput_(MessageIds.MIN_FRAMERATE, prefix + 'minFrameRate')
+        .addNumberInput_(MessageIds.MAX_FRAMERATE, prefix + 'maxFrameRate')
         .addNumberInput_(MessageIds.MIN_BANDWIDTH, prefix + 'minBandwidth')
         .addNumberInput_(MessageIds.MAX_BANDWIDTH, prefix + 'maxBandwidth');
   }

externs/shaka/player.js
@@ -287,6 +287,9 @@ shaka.extern.Track;
  *   minPixels: number,
  *   maxPixels: number,
  *
+ *   minFrameRate: number,
+ *   maxFrameRate: number,
+ *
  *   minBandwidth: number,
  *   maxBandwidth: number
  * }}
@@ -315,6 +318,11 @@ shaka.extern.Track;
  * @property {number} maxPixels
  *   The maximum number of total pixels in a video track (i.e.
  *   <code>width * height</code>).
+ *
+ * @property {number} minFrameRate
+ *   The minimum framerate of a variant track.
+ * @property {number} maxFrameRate
+ *   The maximum framerate of a variant track.
  *
  * @property {number} minBandwidth
  *   The minimum bandwidth of a variant track, in bit/sec.

lib/util/player_configuration.js
@@ -170,6 +170,8 @@ shaka.util.PlayerConfiguration = class {
         maxHeight: abrMaxHeight,
         minPixels: 0,
         maxPixels: Infinity,
+        minFrameRate: 0,
+        maxFrameRate: Infinity,
         minBandwidth: 0,
         maxBandwidth: Infinity,
       },
@@ -195,6 +197,8 @@ shaka.util.PlayerConfiguration = class {
         maxHeight: Infinity,
         minPixels: 0,
         maxPixels: Infinity,
+        minFrameRate: 0,
+        maxFrameRate: Infinity,
         minBandwidth: 0,
         maxBandwidth: Infinity,
       },

commit 0911a6b3d0074244e8f9ca856da8a9f14dae4c21

Fix bad reference to frameRate on Variant
This is only defined on Stream, not Variant.  This bug has was
introduced in PR #2332 and the v2.5.8 release and affects v2.5.8 -
v2.5.10.  The corresponding feature (frameRate restrictions) has
actually never worked.

This was caught by a compiler upgrade which is still in progress.

Change-Id: I0409d9be8d3f6eb0794775ca29f28b98208bab4f

lib/util/stream_utils.js
@@ -141,8 +141,8 @@ shaka.util.StreamUtils = class {
 
     // |variant.frameRate| can be undefined, which breaks
     // the math, so make sure they are there first.
-    if (variant && variant.frameRate) {
-      if (!inRange(variant.frameRate,
+    if (variant && variant.video && variant.video.frameRate) {
+      if (!inRange(variant.video.frameRate,
           restrictions.minFrameRate,
           restrictions.maxFrameRate)) {
         return false;