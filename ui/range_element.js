shaka-project/shaka-player

commit c9bc10d399cd6c37394d586268b32798186b1b66

Fix seek bar on iOS (#2036)
Use touch events to sync the range element's value when the bar is touched.

This means you no longer have to touch the tiny ball on the seek bar on iOS.

Fixes #1918

ui/range_element.js
@@ -78,8 +78,9 @@ shaka.ui.RangeElement = class extends shaka.ui.Element {
       this.onChangeStart();
     });
 
-    this.eventManager.listen(this.bar, 'touchstart', () => {
+    this.eventManager.listen(this.bar, 'touchstart', (e) => {
       this.isChanging_ = true;
+      this.setBarValueForTouch_(e);
       this.onChangeStart();
     }, {passive: true});
 
@@ -87,8 +88,14 @@ shaka.ui.RangeElement = class extends shaka.ui.Element {
       this.onChange();
     });
 
-    this.eventManager.listen(this.bar, 'touchend', () => {
+    this.eventManager.listen(this.bar, 'touchmove', (e) => {
+      this.setBarValueForTouch_(e);
+      this.onChange();
+    });
+
+    this.eventManager.listen(this.bar, 'touchend', (e) => {
       this.isChanging_ = false;
+      this.setBarValueForTouch_(e);
       this.onChangeEnd();
     });
 
@@ -135,6 +142,33 @@ shaka.ui.RangeElement = class extends shaka.ui.Element {
     // The user interaction overrides any external values being pushed in.
     if (this.isChanging_) {
       return;
+    }
+
+    this.bar.value = value;
+  }
+
+  /**
+   * Synchronize the touch position with the range value.
+   * Comes in handy on iOS, where users have to grab the handle in order
+   * to start seeking.
+   * @param {Event} event
+   * @private
+   */
+  setBarValueForTouch_(event) {
+    event.preventDefault();
+
+    const changedTouch = /** @type {TouchEvent} */ (event).changedTouches[0];
+    const rect = this.bar.getBoundingClientRect();
+
+    // Calculate the range value based on the touch position.
+    let value = (this.bar.max / rect.width) *
+      (changedTouch.clientX - rect.left);
+
+    // Keep value within bounds.
+    if (value < this.bar.min) {
+      value = this.bar.min;
+    } else if (value > this.bar.max) {
+      value = this.bar.max;
     }
 
     this.bar.value = value;

AUTHORS
@@ -33,6 +33,7 @@ Jonas Birmé <jonas.birme@eyevinn.se>
 Jozef Chúťka <jozefchutka@gmail.com>
 JW Player <*@jwplayer.com>
 Lucas Gabriel Sánchez <unkiwii@gmail.com>
+Matthias Van Parijs <matvp91@gmail.com>
 Mattias Wadman <mattias.wadman@gmail.com>
 Mohamed Rashid <ge_rashid@hotmail.co.uk>
 Morten Hansen <mimo@mimo-design.com>

CONTRIBUTORS
@@ -52,6 +52,7 @@ Jozef Chúťka <jozefchutka@gmail.com>
 Leandro Ribeiro Moreira <leandro.ribeiro.moreira@gmail.com>
 Lucas Gabriel Sánchez <unkiwii@gmail.com>
 Matias Russitto <russitto@gmail.com>
+Matthias Van Parijs <matvp91@gmail.com>
 Mattias Wadman <mattias.wadman@gmail.com>
 Michelle Zhuo <michellezhuo@google.com>
 Miloš Rašić <milos.rasic@gmail.com>

commit c998ce2721141332ef98b93f065c5df63a7c2d10

Fix live seek bar on touch screens
The UI seek bar on touch screen devices was broken in PR #2036 and
v2.5.4 - v2.5.11 for live content.  VOD content, which starts at 0,
was unaffected.

This fixes the range calculations for touch events to correctly
account for the minimum seek range.

Closes #2558

Change-Id: Ic3f6230d88703fd0ea1c9bc15057c0656b1dc42f

ui/range_element.js
@@ -151,7 +151,15 @@ shaka.ui.RangeElement = class extends shaka.ui.Element {
     const max = parseFloat(this.bar.max);
 
     // Calculate the range value based on the touch position.
-    let value = (max / rect.width) * (changedTouch.clientX - rect.left);
+
+    // Pixels from the left of the range element
+    const touchPosition = changedTouch.clientX - rect.left;
+
+    // Pixels per unit value of the range element.
+    const scale = (max - min) / rect.width;
+
+    // Touch position in units, which may be outside the allowed range.
+    let value = min + scale * touchPosition;
 
     // Keep value within bounds.
     if (value < min) {