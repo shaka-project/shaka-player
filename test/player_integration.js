6a89f27ace8ab0a67caadfc3bad776fbd67249b0
Theodore Abshire <theodab@google.com>
2020-07-21 17:56:27 -0700

    fix: Re-add a session close workaround.
    There is a new bug that is preventing the DRM engine from
    shutting down, for Widevine assets. Or, at least, new to us.
    It happens if session.generateRequest is called, but
    session.update is never called (e.g. if the network request fails).
    In this case, session.close will never resolve.
    This re-adds the old workaround we used to use, where we
    would timeout after a while, if the session fails to close.
    
    This also adds a regression test.
    
    Issue #2741
    
    Change-Id: Ieb6a64b762473edb9a3209580a84aee870e21fb5

--- test/player_integration.js
+++ test/player_integration.js
@@ -869,4 +869,28 @@ describe('Player', () => {
       await expectAsync(p).toBeRejected();  // Timeout
     });
   });  // describe('adaptation')
+
+  /** Regression test for Issue #2741 */
+  describe('unloading', () => {
+    drmIt('unloads properly after DRM error', async () => {
+      let unloadPromise = null;
+      const errorPromise = new Promise((resolve, reject) => {
+        onErrorSpy.and.callFake((event) => {
+          unloadPromise = player.unload();
+          onErrorSpy.and.callThrough();
+          resolve();
+        });
+      });
+      // Load an encrypted asset with the wrong license servers, so it errors.
+      player.configure('drm.servers', {
+        'com.widevine.alpha': 'http://foo/widevine',
+      });
+      await player.load('test:sintel-enc_compiled');
+      await errorPromise;
+      expect(unloadPromise).not.toBeNull();
+      if (unloadPromise) {
+        await unloadPromise;
+      }
+    });
+  });  // describe('unloading')
 });
