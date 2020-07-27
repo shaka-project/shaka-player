6c1b89683e91972510c66de2fff0ed782ae9f81b
Joey Parrish <joeyparrish@google.com>
2020-07-27 13:14:42 -0700

    test: Add missing license server info to cast test
    One of the cast tests which load sintel-enc had license server info,
    and the other did not.  This adds the missing license server info.
    
    Change-Id: Ibfd6f3af7a6e418f7b58deaebe5a8926bc4f07a5

--- test/cast/cast_receiver_integration.js
+++ test/cast/cast_receiver_integration.js
@@ -160,6 +160,11 @@ filterDescribe('CastReceiver', castReceiverIntegrationSupport, () => {
     drmIt('has reasonable average message size', async () => {
       // Use an encrypted asset, to make sure DRM info doesn't balloon the size.
       fakeInitState.manifest = 'test:sintel-enc';
+      fakeInitState.player.configure['drm'] = {
+        'servers': {
+          'com.widevine.alpha': 'https://cwip-shaka-proxy.appspot.com/no_auth',
+        },
+      };
 
       const p = waitForLoadedData();
 
