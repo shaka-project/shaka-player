4bf22e2359a840fe15c482de5e4671cfb72c14f0
Joey Parrish <joeyparrish@google.com>
2020-08-12 12:43:20 -0700

    fix(VTT): Fix timing of VTT in HLS without map header
    When there is no X-TIMESTAMP-MAP header, the HLS spec states that the
    client should map 0 to 0, effectively.  That means we were wrong to
    offset the cues by the segment time.
    
    Since DASH IOP states that segmented text should be packaged in MP4,
    this should not affect compliant DASH content.
    
    Closes #2714
    
    Change-Id: Iddb00f2fd1afeb4f0f2c99f92f65e5db0e3a84f1

--- test/text/cue_integration.js
+++ test/text/cue_integration.js
@@ -19,7 +19,7 @@ describe('Cue', () => {
         'WEBVTT\n\n' +
         '00:00:20.000 --> 00:00:40.000\n' +
         'Test',
-        {periodStart: 0, segmentStart: 7, segmentEnd: 0});
+        {periodStart: 7, segmentStart: 10, segmentEnd: 60});
     expect(cues.length).toBe(1);
     expect(cues[0].startTime).toBe(27);
     expect(cues[0].endTime).toBe(47);
