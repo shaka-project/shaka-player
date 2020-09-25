1840ae3d6ff10842d25514ce5e14afb43e65afa4
Alvaro Velad <alvaro.velad@mirada.tv>
2020-09-25 13:34:25 +0200

    Add SubRip (SRT) subtitle support

---
+++ test/text/srt_text_parser_unit.js
@@ -0,0 +1,99 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+describe('SrtTextParser', () => {
+  const originalLogWarning = shaka.log.warning;
+
+  /** @type {!jasmine.Spy} */
+  let logWarningSpy;
+
+  beforeEach(() => {
+    logWarningSpy = jasmine.createSpy('shaka.log.warning');
+    shaka.log.warning = shaka.test.Util.spyFunc(logWarningSpy);
+  });
+
+  afterEach(() => {
+    shaka.log.warning = originalLogWarning;
+  });
+
+  it('supports no cues', () => {
+    verifyHelper([],
+        '',
+        {periodStart: 0, segmentStart: 0, segmentEnd: 0});
+  });
+
+  it('handles a blank line at the end of the file', () => {
+    verifyHelper(
+        [
+          {startTime: 20, endTime: 40, payload: 'Test'},
+        ],
+        '1\n' +
+        '00:00:20,000 --> 00:00:40,000\n' +
+        'Test\n\n',
+        {periodStart: 0, segmentStart: 0, segmentEnd: 0});
+  });
+
+  it('handles no blank line at the end of the file', () => {
+    verifyHelper(
+        [
+          {startTime: 20, endTime: 40, payload: 'Test'},
+        ],
+        '1\n' +
+        '00:00:20,000 --> 00:00:40,000\n' +
+        'Test\n',
+        {periodStart: 0, segmentStart: 0, segmentEnd: 0,
+        });
+  });
+
+  it('handles no newline after the final text payload', () => {
+    verifyHelper(
+        [
+          {startTime: 20, endTime: 40, payload: 'Test'},
+        ],
+        '1\n' +
+        '00:00:20,000 --> 00:00:40,000\n' +
+        'Test',
+        {periodStart: 0, segmentStart: 0, segmentEnd: 0});
+  });
+
+  it('supports cues', () => {
+    verifyHelper(
+        [
+          {startTime: 20, endTime: 40, payload: 'Test', id: '1'},
+          {startTime: 40, endTime: 50, payload: 'Test2', id: '2'},
+        ],
+        '1\n' +
+        '00:00:20,000 --> 00:00:40,000\n' +
+        'Test\n\n' +
+        '2\n' +
+        '00:00:40,000 --> 00:00:50,000\n' +
+        'Test2',
+        {periodStart: 0, segmentStart: 0, segmentEnd: 0});
+  });
+
+  /**
+   * @param {!Array} cues
+   * @param {string} text
+   * @param {shaka.extern.TextParser.TimeContext} time
+   */
+  function verifyHelper(cues, text, time) {
+    const data =
+        shaka.util.BufferUtils.toUint8(shaka.util.StringUtils.toUTF8(text));
+
+    const parser = new shaka.text.SrtTextParser();
+    const result = parser.parseMedia(data, time);
+
+    const expected = cues.map((cue) => {
+      if (cue.nestedCues) {
+        cue.nestedCues = cue.nestedCues.map(
+            (nestedCue) => jasmine.objectContaining(nestedCue)
+        );
+      }
+      return jasmine.objectContaining(cue);
+    });
+    expect(result).toEqual(expected);
+  }
+});
