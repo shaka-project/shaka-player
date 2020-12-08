1c1183648cf7906c420751ef20d929ee55aeffe9
Joey Parrish <joeyparrish@google.com>
2020-12-08 10:39:24 -0800

    build: fix build failure in context of node module
    Building Shaka Player after installing it via node currently results
    in a build failure.  This is because stylelint wants to filter out any
    sources that match the glob **/node_modules/**, which in that context
    is all of our sources.
    
    To fix the failure, we add the --disable-default-ignores flag to the
    stylelint command line.
    
    We also had a similar hard-coded exclusion in build.py, which is now
    fixed, as well.
    
    Backported to v2.5.x
    
    Change-Id: I33f6619ebaccce1e1efa5da68e31cd49036addcd

--- build/build.py
+++ build/build.py
@@ -275,8 +275,12 @@ class Build(object):
     build_name = 'shaka-player.' + name
     closure = compiler.ClosureCompiler(self.include, build_name)
 
-    # Don't pass node modules to the extern generator.
-    local_include = set([f for f in self.include if 'node_modules' not in f])
+    # Don't pass local node modules to the extern generator.  But don't simply
+    # exclude the string 'node_modules', either, since Shaka Player could be
+    # rebuilt after installing it as a node module.
+    node_modules_path = os.path.join(
+        shakaBuildHelpers.get_source_base(), 'node_modules')
+    local_include = set([f for f in self.include if node_modules_path not in f])
     generator = compiler.ExternGenerator(local_include, build_name)
 
     closure_opts = common_closure_opts + common_closure_defines
