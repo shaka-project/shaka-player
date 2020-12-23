3059c47ea8bf87aa406e9688ed6223c2b14e889d
Joey Parrish <joeyparrish@google.com>
2020-12-23 10:32:42 -0800

    fix(drm): Remove legacy Edge workarounds on new Edge
    We only just gained the ability to test on new Chromium-based Edge
    (google/generic-webdriver-server@475ec746), so we did not previously
    know which workarounds were specific to legacy Edge.  Now all but one
    of those workarounds has been limited to legacy Edge only, and will
    not be applied to new Chromium-based Edge.
    
    Backported to v2.5.x
    
    Change-Id: I70b7480ccb61064b796c3d9b41bbe95cabfdf850

--- test/media/drm_engine_unit.js
+++ test/media/drm_engine_unit.js
@@ -264,7 +264,7 @@ describe('DrmEngine', function() {
       // Because DrmEngine will err on being too accepting, make sure it will
       // reject something. However, we can only check that it is actually
       // thing on non-Edge browsers because of https://bit.ly/2IcEgv0
-      if (!shaka.util.Platform.isEdge()) {
+      if (!shaka.util.Platform.isLegacyEdge()) {
         expect(drmEngine.willSupport('this-should-fail')).toBeFalsy();
       }
     });
