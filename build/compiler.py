shaka-project/shaka-player

commit 3dae4ca04186ab0fc45aad3d2651ed284134dc23

build: fix build error with stylelint paths
To run node binaries, our build scripts parse the package.json file
from a node module to find the path to its bin files.  Instead of
this:

"bin": {
    "stylelint": "path/to/stylelint.js"
},

I found this:

"bin": "path/to/stylelint.js"

Now we accept both formats.

I'm not sure why/how/when this changed, or if it has to do with our
fork of stylelint.

Change-Id: I5b804c5e90186195ad5d60536e200eb83c42e801

build/shakaBuildHelpers.py
@@ -294,7 +294,16 @@ def get_node_binary(module_name, bin_name=None):
   if os.path.isdir(path):
     json_path = os.path.join(path, 'package.json')
     package_data = json.load(open_file(json_path, 'r'))
-    bin_path = os.path.join(path, package_data['bin'][bin_name])
+    bin_data = package_data['bin']
+
+    if type(bin_data) is str or type(bin_data) is unicode:
+      # There's only one binary here.
+      bin_rel_path = bin_data
+    else:
+      # It's a dictionary, so look up the specific binary we want.
+      bin_rel_path = bin_data[bin_name]
+
+    bin_path = os.path.join(path, bin_rel_path)
     return ['node', bin_path]
 
   # Not found locally, assume it can be found in os.environ['PATH'].

commit 1e74fc2d5c632a7044e4247cb8694bb8bd43a447

fix: Fix build script errors using Python 3
Python 3 does not have a "unicode" type, having replaced it with
"str".  In one of our helpers, we checked for "unicode" and "str" both
on Python 2, but this caused an error on Python 3 where "unicode" does
not exist any more.

However, the entire helper turns out to be unnecessary, and can be
replaced with an invocation of "npx".  So we can eliminate the
"unicode" type check entirely.

This was broken in 3dae4ca0 and brought to our attention in PR #3069.
It does not affect any release versions.

Change-Id: Id04ae44c744113f8cf8fd7ff566dcd6b280eb362

build/compiler.py
@@ -276,7 +276,6 @@ class Less(object):
     if not force and not _must_build(self.output, self.all_source_files):
       return True
 
-    lessc = shakaBuildHelpers.get_node_binary('less', 'lessc')
     less_options = [
       # Enable the "clean-CSS" plugin to minify the output and strip out comments.
       '--clean-css',
@@ -284,7 +283,12 @@ class Less(object):
       '--source-map=' + self.output + '.map',
     ]
 
-    cmd_line = lessc + less_options + [self.main_source_file, self.output]
+    cmd_line = [
+      'npx', 'lessc',
+    ] + less_options + [
+      self.main_source_file,
+      self.output,
+    ]
 
     if shakaBuildHelpers.execute_get_code(cmd_line) != 0:
       logging.error('Externs generation failed')
@@ -322,8 +326,10 @@ class Linter(object):
     if not force and not _must_build(self.output, deps):
       return True
 
-    eslint = shakaBuildHelpers.get_node_binary('eslint')
-    cmd_line = eslint + ['--config', self.config_path] + self.source_files
+    cmd_line = [
+      'npx', 'eslint',
+      '--config', self.config_path,
+    ] + self.source_files
 
     if fix:
       cmd_line += ['--fix']
@@ -358,8 +364,8 @@ class CssLinter(object):
     if not force and not _must_build(self.output, deps):
       return True
 
-    stylelint = shakaBuildHelpers.get_node_binary('stylelint')
-    cmd_line = stylelint + [
+    cmd_line = [
+        'npx', 'stylelint',
         '--config', self.config_path,
         # The "default ignores" is something like **/node_modules/**, which
         # means that if we run the build scripts from inside the installed node
@@ -398,8 +404,10 @@ class HtmlLinter(object):
     if not force and not _must_build(self.output, deps):
       return True
 
-    htmlhint = shakaBuildHelpers.get_node_binary('htmlhint')
-    cmd_line = htmlhint + ['--config=' + self.config_path] + self.source_files
+    cmd_line = [
+      'npx', 'htmlhint',
+      '--config=' + self.config_path,
+    ] + self.source_files
 
     if shakaBuildHelpers.execute_get_code(cmd_line) != 0:
       return False
@@ -452,8 +460,10 @@ class Jsdoc(object):
 
     # Jsdoc expects to run from the base dir.
     with shakaBuildHelpers.InDir(base):
-      jsdoc = shakaBuildHelpers.get_node_binary('jsdoc')
-      cmd_line = jsdoc + ['-c', self.config_path]
+      cmd_line = [
+        'npx', 'jsdoc',
+        '-c', self.config_path,
+      ]
       if shakaBuildHelpers.execute_get_code(cmd_line) != 0:
         return False
 

build/generateTsDefs.py
@@ -35,9 +35,8 @@ def GenerateTsDefs(inputs, output):
     inputs: A list of paths to Closure extern files.
     output: A path to a TypeScript def output file.
   """
-  clutz = shakaBuildHelpers.get_node_binary('@teppeis/clutz', 'clutz')
-
-  command = clutz + [
+  command = [
+      'npx', 'clutz',
       '--closure_env', 'BROWSER',
       '--externs',
   ] + inputs + [

build/shakaBuildHelpers.py
@@ -270,46 +270,6 @@ def get_all_files(dir_path, exp=None):
   return ret
 
 
-def get_node_binary(module_name, bin_name=None):
-  """Returns an array to be used in the command-line execution of a node binary.
-
-  For example, this may return ['eslint'] (global install)
-  or ['node', 'path/to/node_modules/eslint/bin/eslint.js'] (local install).
-
-  Arguments:
-    module_name: A string, the name of the module.
-    bin_name: An optional string, the name of the binary, which defaults to
-              module_name if not provided.
-
-  Returns:
-    An array of strings which form the command-line to call the binary.
-  """
-
-  if not bin_name:
-    bin_name = module_name
-
-  # Check local modules first.
-  base = get_source_base()
-  path = os.path.join(base, 'node_modules', module_name)
-  if os.path.isdir(path):
-    json_path = os.path.join(path, 'package.json')
-    package_data = json.load(open_file(json_path, 'r'))
-    bin_data = package_data['bin']
-
-    if type(bin_data) is str or type(bin_data) is unicode:
-      # There's only one binary here.
-      bin_rel_path = bin_data
-    else:
-      # It's a dictionary, so look up the specific binary we want.
-      bin_rel_path = bin_data[bin_name]
-
-    bin_path = os.path.join(path, bin_rel_path)
-    return ['node', bin_path]
-
-  # Not found locally, assume it can be found in os.environ['PATH'].
-  return [bin_name]
-
-
 class InDir(object):
   """A Context Manager that changes directories temporarily and safely."""
   def __init__(self, path):

build/test.py
@@ -413,9 +413,8 @@ class Launcher:
       logging.error('Failed to update node modules')
       return 1
 
-    karma = shakaBuildHelpers.get_node_binary('karma')
     cmd = ['xvfb-run', '--auto-servernum'] if self.parsed_args.use_xvfb else []
-    cmd += karma + ['start']
+    cmd += ['npx', 'karma', 'start']
     cmd += [karma_conf] if karma_conf else []
     cmd += ['--settings', json.dumps(self.karma_config)]
 