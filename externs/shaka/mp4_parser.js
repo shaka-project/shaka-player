203b1cdff5fe217ff5e627aeb54bee880b8c3e97
Joey Parrish <joeyparrish@google.com>
2021-01-12 15:24:32 -0800

    fix: Fix header sizes for MP4 boxes with 64-bit size fields
    The MP4 box header depends on both whether the box is a "full" box
    (with version and flags) and whether the box has an extra 64-bit size
    field.  The MP4 parser was only considering the version and flags, but
    not whether the extra size field was present.  We already correctly
    parsed the 64-bit size field, but computed offsets for other fields
    without considering this.
    
    Groundwork for a fix for #2759
    
    Backported to v2.5.x
    
    Change-Id: I48f16ef073a036954b6f7d25641ccf29828b02c8

--- externs/shaka/mp4_parser.js
+++ externs/shaka/mp4_parser.js
@@ -28,7 +28,8 @@
  *    size: number,
  *    version: ?number,
  *    flags: ?number,
- *    reader: !shaka.util.DataViewReader
+ *    reader: !shaka.util.DataViewReader,
+ *    has64BitSize: boolean
  * }}
  *
  * @property {!shaka.util.Mp4Parser} parser
@@ -52,6 +53,9 @@
  * @property {!shaka.util.DataViewReader} reader
  *   The reader for this box is only for this box. Reading or not reading to
  *   the end will have no affect on the parser reading other sibling boxes.
+ * @property {boolean} has64BitSize
+ *   If true, the box header had a 64-bit size field.  This affects the offsets
+ *   of other fields.
  * @exportDoc
  */
 shaka.extern.ParsedBox;
