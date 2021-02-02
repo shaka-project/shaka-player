2fdbd6d942d06dada3cc29426f09fa49afa9cecd
Joey Parrish <joeyparrish@google.com>
2021-02-02 15:33:04 -0800

    test: Skip Axinom-based tests on Xbox One
    Axinom will not issue licenses to Xbox One, so the tests that use
    Axinom for PlayReady testing must be skipped on that platform.
    Ideally, we would switch providers for the license, so I've added a
    TODO for that.
    
    Change-Id: I93da03b19ec5e28c1f50a40edca8563d92917555

--- test/offline/offline_integration.js
+++ test/offline/offline_integration.js
@@ -12,6 +12,7 @@ goog.require('shaka.test.UiUtils');
 goog.require('shaka.test.Util');
 goog.require('shaka.test.Waiter');
 goog.require('shaka.util.EventManager');
+goog.require('shaka.util.Platform');
 
 /** @return {boolean} */
 const supportsStorage = () => shaka.offline.Storage.support();
@@ -126,6 +127,16 @@ filterDescribe('Offline', supportsStorage, () => {
 
         if (!(widevineSupport || playreadySupport)) {
           pending('Widevine and PlayReady are not supported');
+          return;
+        }
+
+        if (shaka.util.Platform.isXboxOne()) {
+          // Axinom won't issue a license for an Xbox One.  The error message
+          // from the license server says "Your DRM client's security level is
+          // 150, but the entitlement message requires 2000 or higher."
+          // TODO: Stop using Axinom's license server.  Use
+          // https://testweb.playready.microsoft.com/Server/ServiceQueryStringSyntax
+          pending('Xbox One not supported by Axinom license server');
           return;
         }
 
