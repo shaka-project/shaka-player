aec4f33752f97902613a00fa02becff806948194
ismena <ismena@google.com>
2021-03-09 10:24:12 -0800

    fix: fix reference searching when search time = segment.endTime
    Fixes #3191
    
    Change-Id: I4f2c6b3e2e67a6db1bfb71815a5ce80a3584e41e

--- test/media/segment_index_unit.js
+++ test/media/segment_index_unit.js
@@ -60,6 +60,28 @@ describe('SegmentIndex', /** @suppress {accessControls} */ () => {
       expect(ref).toBe(actual2);
     });
 
+    it('works with two references if time == first end time', () => {
+      const actual1 = makeReference(uri(10), 10, 20.12);
+      const actual2 = makeReference(uri(20), 20.13, 30);
+      const index = new shaka.media.SegmentIndex([actual1, actual2]);
+
+      const pos = index.find(20.12);
+      goog.asserts.assert(pos != null, 'Null position!');
+      const ref = index.get(pos);
+      expect(ref).toBe(actual1);
+    });
+
+    it('works with time is between first endTime and second startTime', () => {
+      const actual1 = makeReference(uri(10), 10, 20.12111);
+      const actual2 = makeReference(uri(20), 20.12113, 30);
+      const index = new shaka.media.SegmentIndex([actual1, actual2]);
+
+      const pos = index.find(20.12112);
+      goog.asserts.assert(pos != null, 'Null position!');
+      const ref = index.get(pos);
+      expect(ref).toBe(actual1);
+    });
+
     it('returns the first segment if time < first start time', () => {
       const actual = makeReference(uri(10), 10, 20);
       const index = new shaka.media.SegmentIndex([actual]);
@@ -83,15 +105,6 @@ describe('SegmentIndex', /** @suppress {accessControls} */ () => {
       const index = new shaka.media.SegmentIndex([actual]);
 
       const pos = index.find(21);
-      expect(pos).toBeNull();
-    });
-
-    it('returns null if time is within a gap', () => {
-      const actual1 = makeReference(uri(10), 10, 20);
-      const actual2 = makeReference(uri(25), 25, 30);
-      const index = new shaka.media.SegmentIndex([actual1, actual2]);
-
-      const pos = index.find(23);
       expect(pos).toBeNull();
     });
   });
