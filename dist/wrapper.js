af889ec692d2c2d78f7ce82522a36708918cf540
Ketan Patil <k2p1020@gmail.com>
2021-03-22 10:44:28 +0530

    Configurable Keyboard shortcut: toggleFullscreenOnfPress (#3223)

---
+++ dist/wrapper.js
@@ -0,0 +1,7 @@
+/*
+ @license
+ Shaka Player
+ Copyright 2016 Google LLC
+ SPDX-License-Identifier: Apache-2.0
+*/
+(function(){var innerGlobal=typeof window!="undefined"?window:global;var exportTo={};(function(window,global,module){%output%}).call(exportTo,innerGlobal,innerGlobal,undefined);if(typeof exports!="undefined")for(var k in exportTo.shaka)exports[k]=exportTo.shaka[k];else if(typeof define=="function"&&define.amd)define(function(){return exportTo.shaka});else innerGlobal.shaka=exportTo.shaka})();
