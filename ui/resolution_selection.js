af1b189ca1357553dfe48ab93652f13d08d09b16
aditmehta9 <aditmehta9@gmail.com>
2021-03-24 00:24:03 +0530

    Bug: Fix of resolution change when language changed

--- ui/resolution_selection.js
+++ ui/resolution_selection.js
@@ -48,7 +48,7 @@ shaka.ui.ResolutionSelection = class extends shaka.ui.SettingsMenu {
 
 
     this.eventManager.listen(this.player, 'variantchanged', () => {
-      this.updateResolutionSelection_();
+      this.updateResolutionSelectionnew_();
     });
 
     this.eventManager.listen(this.player, 'trackschanged', () => {
@@ -111,6 +111,132 @@ shaka.ui.ResolutionSelection = class extends shaka.ui.SettingsMenu {
       return otherIdx == idx;
     });
 
+    // Remove old shaka-resolutions
+    // 1. Save the back to menu button
+    const backButton = shaka.ui.Utils.getFirstDescendantWithClassName(
+        this.menu, 'shaka-back-to-overflow-button');
+
+    // 2. Remove everything
+    shaka.util.Dom.removeAllChildren(this.menu);
+
+    // 3. Add the backTo Menu button back
+    this.menu.appendChild(backButton);
+
+    const abrEnabled = this.player.getConfiguration().abr.enabled;
+
+    // Add new ones
+    for (const track of tracks) {
+      const button = shaka.util.Dom.createButton();
+      button.classList.add('explicit-resolution');
+      this.eventManager.listen(button, 'click',
+          () => this.onTrackSelected_(track));
+
+      const span = shaka.util.Dom.createHTMLElement('span');
+      span.textContent = track.height + 'p';
+      button.appendChild(span);
+
+      if (!abrEnabled && track == selectedTrack) {
+        // If abr is disabled, mark the selected track's resolution.
+        button.setAttribute('aria-selected', 'true');
+        button.appendChild(shaka.ui.Utils.checkmarkIcon());
+        span.classList.add('shaka-chosen-item');
+        this.currentSelection.textContent = span.textContent;
+      }
+      this.menu.appendChild(button);
+    }
+
+    // Add the Auto button
+    const autoButton = shaka.util.Dom.createButton();
+    autoButton.classList.add('shaka-enable-abr-button');
+    this.eventManager.listen(autoButton, 'click', () => {
+      const config = {abr: {enabled: true}};
+      this.player.configure(config);
+      this.updateResolutionSelection_();
+    });
+
+    /** @private {!HTMLElement}*/
+    this.abrOnSpan_ = shaka.util.Dom.createHTMLElement('span');
+    this.abrOnSpan_.classList.add('shaka-auto-span');
+    this.abrOnSpan_.textContent =
+        this.localization.resolve(shaka.ui.Locales.Ids.AUTO_QUALITY);
+    autoButton.appendChild(this.abrOnSpan_);
+
+    // If abr is enabled reflect it by marking 'Auto' as selected.
+    if (abrEnabled) {
+      autoButton.setAttribute('aria-selected', 'true');
+      autoButton.appendChild(shaka.ui.Utils.checkmarkIcon());
+
+      this.abrOnSpan_.classList.add('shaka-chosen-item');
+
+      this.currentSelection.textContent =
+          this.localization.resolve(shaka.ui.Locales.Ids.AUTO_QUALITY);
+    }
+
+    this.menu.appendChild(autoButton);
+    shaka.ui.Utils.focusOnTheChosenItem(this.menu);
+    this.controls.dispatchEvent(
+        new shaka.util.FakeEvent('resolutionselectionupdated'));
+  }
+
+
+  /** @private */
+  updateResolutionSelectionnew_() {
+    /** @type {!Array.<shaka.extern.Track>} */
+    let tracks = this.player.getVariantTracks();
+    // Hide resolution menu and button for audio-only content and src= content
+    // without resolution information.
+    // TODO: for audio-only content, this should be a bitrate selection menu
+    // instead.
+    if (tracks.length && !tracks[0].height) {
+      shaka.ui.Utils.setDisplay(this.menu, false);
+      shaka.ui.Utils.setDisplay(this.button, false);
+      return;
+    }
+    // Otherwise, restore it.
+    shaka.ui.Utils.setDisplay(this.button, true);
+
+    tracks.sort((t1, t2) => {
+      // We have already screened for audio-only content, but the compiler
+      // doesn't know that.
+      goog.asserts.assert(t1.height != null, 'Null height');
+      goog.asserts.assert(t2.height != null, 'Null height');
+
+      return t2.height - t1.height;
+    });
+
+    // If there is a selected variant track, then we filter out any tracks in
+    // a different language.  Then we use those remaining tracks to display the
+    // available resolutions.
+    let selectedTrack = tracks.find((track) => track.active);
+    if (tracks.find((track) =>
+      track.height ==
+      Number((this.currentSelection.textContent.toString()).slice(0, 3))) !==
+      undefined) {
+      selectedTrack = tracks.find((track) => track.height ==
+      Number((this.currentSelection.textContent.toString()).slice(0, 3)));
+      const tempTrack = tracks.find((track) => track.active);
+      for (let i=0; i < tracks.length; i++) {
+        if (tracks[i].id === tempTrack.id) {
+          tracks[i].active = false;
+        } else if (tracks[i].id === selectedTrack.id) {
+          tracks[i].active = true;
+        }
+      }
+    }
+    if (selectedTrack) {
+      // Filter by current audio language and channel count.
+      tracks = tracks.filter(
+          (track) => track.language == selectedTrack.language &&
+              track.channelsCount == selectedTrack.channelsCount);
+    }
+
+    // Remove duplicate entries with the same height.  This can happen if
+    // we have multiple resolutions of audio.  Pick an arbitrary one.
+    tracks = tracks.filter((track, idx) => {
+      // Keep the first one with the same height.
+      const otherIdx = tracks.findIndex((t) => t.height == track.height);
+      return otherIdx == idx;
+    });
     // Remove old shaka-resolutions
     // 1. Save the back to menu button
     const backButton = shaka.ui.Utils.getFirstDescendantWithClassName(
