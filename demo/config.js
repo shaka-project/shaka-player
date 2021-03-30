863e3455b68657cc73960022876c657498362ee5
Álvaro Velad Galván <alvaro.velad@mirada.tv>
2021-03-30 22:35:41 +0200

    feat: Network stall detection (#3227)
    Closes: #1582

--- demo/config.js
+++ demo/config.js
@@ -280,6 +280,11 @@ shakaDemo.Config = class {
         .addNumberInput_(MessageIds.FUZZ_FACTOR, prefix + 'fuzzFactor',
             /* canBeDecimal= */ true)
         .addNumberInput_(MessageIds.TIMEOUT, prefix + 'timeout',
+            /* canBeDecimal= */ true)
+        .addNumberInput_(MessageIds.STALL_TIMEOUT, prefix + 'stallTimeout',
+            /* canBeDecimal= */ true)
+        .addNumberInput_(MessageIds.CONNECTION_TIMEOUT,
+            prefix + 'connectionTimeout',
             /* canBeDecimal= */ true);
   }
 
