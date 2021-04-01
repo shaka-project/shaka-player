9489334e2bb2e837284881e68bb66939efebf8e8
Vincent Valot <valot.vince@gmail.com>
2021-04-01 12:18:39 +0200

    remove useless diffs

--- demo/common/assets.js
+++ demo/common/assets.js
@@ -1047,8 +1047,6 @@ shakaAssets.testAssets = [
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
       .addLicenseServer('com.microsoft.playready', 'https://content.uplynk.com/pr')
       .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
-      .setExtraConfig({drm: {advanced: {
-        'com.microsoft.playready': {sessionType: 'persistent-license'}}}})
       .setRequestFilter(shakaAssets.UplynkRequestFilter)
       .setResponseFilter(shakaAssets.UplynkResponseFilter),
   // Reliable Playready playback requires Edge 16+
@@ -1066,8 +1064,6 @@ shakaAssets.testAssets = [
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
       .addLicenseServer('com.microsoft.playready', 'https://content.uplynk.com/pr')
       .addLicenseServer('com.widevine.alpha', 'https://content.uplynk.com/wv')
-      .setExtraConfig({drm: {advanced: {
-        'com.microsoft.playready': {sessionType: 'persistent-license'}}}})
       .setRequestFilter(shakaAssets.UplynkRequestFilter)
       .setResponseFilter(shakaAssets.UplynkResponseFilter),
   new ShakaDemoAssetInfo(
