e248db408821568b73d62abfa58ea525b2f41ac1
Enson Choy <enson.choy@harmonicinc.com>
2021-08-20 15:55:02 +0800

    Fix: Typings & remove unrelated tests

--- test/dash/dash_parser_manifest_unit.js
+++ test/dash/dash_parser_manifest_unit.js
@@ -2151,44 +2151,6 @@ describe('DashParser Manifest', () => {
     }
   });
 
-  it('parses ServiceConfiguration', async () => {
-    const manifestText = [
-      `<MPD type="dynamic"`,
-      '     availabilityStartTime="1970-01-01T00:00:00Z"',
-      '     timeShiftBufferDepth="PT60S"',
-      '     maxSegmentDuration="PT5S"',
-      '     suggestedPresentationDelay="PT0S">',
-      '  <ServiceDescription>',
-      '    <Scope schemeIdUri="1" value="scope1" />',
-      '    <Latency target="5000" max="7000" min="4000" />',
-      '    <PlaybackRate max="1.1" min="0.9" />',
-      '  </ServiceDescription>',
-      '  <Period id="1" duration="PT30S">',
-      '    <AdaptationSet id="2" mimeType="video/mp4">',
-      '      <SegmentTemplate media="$Number$.mp4" duration="1" />',
-      '      <Representation id="3" width="640" height="480">',
-      '        <BaseURL>http://example.com/p1/</BaseURL>',
-      '      </Representation>',
-      '    </AdaptationSet>',
-      '  </Period>',
-      '</MPD>',
-    ].join('\n');
-
-    fakeNetEngine.setResponseText('dummy://foo', manifestText);
-
-    await parser.start('dummy://foo', playerInterface);
-
-    const serviceDescription = parser.getServiceDescription();
-    expect(serviceDescription).toBeDefined();
-    expect(serviceDescription.scope.schemeIdUri).toBe('1');
-    expect(serviceDescription.scope.value).toBe('scope1');
-    expect(serviceDescription.latency.target).toBe(5000);
-    expect(serviceDescription.latency.max).toBe(7000);
-    expect(serviceDescription.latency.min).toBe(4000);
-    expect(serviceDescription.playbackRate.max).toBe(1.1);
-    expect(serviceDescription.playbackRate.min).toBe(0.9);
-  });
-
   /**
      * @param {!Array.<number>} periods Start time of multiple periods
      * @return {string}
@@ -2242,11 +2204,13 @@ describe('DashParser Manifest', () => {
 
     for (const source of sources) {
       fakeNetEngine.setResponseText('dummy://foo', source);
+      /** @type {shaka.extern.Manifest} */
       // eslint-disable-next-line no-await-in-loop
       const manifest = await parser.start('dummy://foo', playerInterface);
       const video = manifest.variants[0].video;
       // eslint-disable-next-line no-await-in-loop
       await video.createSegmentIndex();
+      goog.asserts.assert(video.segmentIndex, 'Null segmentIndex!');
       segments.push(Array.from(video.segmentIndex));
     }
 
