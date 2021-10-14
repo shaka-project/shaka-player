077ea2a0aa51b0761199a1a695750c48d60353d9
Joey Parrish <joeyparrish@google.com>
2021-10-14 09:09:39 -0700

    chore: Add missing license header in test/media/closed_caption_parser_unit.js
    Change-Id: Iaa390a4415e42f2e447ba1df4842dc7446471af5

--- test/media/closed_caption_parser_unit.js
+++ test/media/closed_caption_parser_unit.js
@@ -1,3 +1,9 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 goog.require('shaka.media.ClosedCaptionParser');
 goog.require('shaka.test.Util');
 
@@ -12,4 +18,3 @@ describe('ClosedCaptionParser', () => {
     parser.parseFrom(videoSegment);
   });
 });
-
