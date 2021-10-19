e5fd7d57bff96077241d59c5d0c575cd60ffa082
Alvaro Velad <alvaro.velad@mirada.tv>
2021-10-19 21:07:35 +0200

    Update comments

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -972,18 +972,18 @@ shaka.extern.AbrConfiguration;
  *   Minimum number of bytes sampled before we trust the estimate.  If we have
  *   not sampled much data, our estimate may not be accurate enough to trust.
  * @property {number} minBytes
- * Minimum number of bytes, under which samples are discarded.  Our models
- * do not include latency information, so connection startup time (time to
- * first byte) is considered part of the download time.  Because of this, we
- * should ignore very small downloads which would cause our estimate to be
- * too low.
+ *   Minimum number of bytes, under which samples are discarded.  Our models
+ *   do not include latency information, so connection startup time (time to
+ *   first byte) is considered part of the download time.  Because of this, we
+ *   should ignore very small downloads which would cause our estimate to be
+ *   too low.
  * @property {number} fastHalfLife
- *   The quantity of prior samples (by weight) used
- *   when creating a new estimate.  Those prior samples make up half of the
+ *   The quantity of prior samples (by weight) used when creating a new
+ *   estimate, in seconds.  Those prior samples make up half of the
  *   new estimate.
  * @property {number} slowHalfLife
- *   The quantity of prior samples (by weight) used
- *   when creating a new estimate.  Those prior samples make up half of the
+ *   The quantity of prior samples (by weight) used when creating a new
+ *   estimate, in seconds.  Those prior samples make up half of the
  *   new estimate.
  * @exportDoc
  */
