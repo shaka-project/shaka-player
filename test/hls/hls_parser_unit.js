06e8e488a89fd389a18d36d52e6835710fc012c9
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2021-12-06 03:36:06 -0500

    fix(hls): Fixes parsing of HLS 'DEFAULT' attribute (#3771)
    Closes #3769

--- test/hls/hls_parser_unit.js
+++ test/hls/hls_parser_unit.js
@@ -783,7 +783,7 @@ describe('HlsParser', () => {
       'RESOLUTION=960x540,FRAME-RATE=120,AUDIO="aud2"\n',
       'video2\n',
       '#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="aud1",LANGUAGE="eng",',
-      'URI="audio"\n',
+      'DEFAULT=YES,URI="audio"\n',
       '#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="aud2",LANGUAGE="fr",',
       'URI="audio2"\n',
     ].join('');
@@ -801,6 +801,7 @@ describe('HlsParser', () => {
       manifest.anyTimeline();
       manifest.addPartialVariant((variant) => {
         variant.bandwidth = 200;
+        variant.primary = true;
         variant.addPartialStream(ContentType.VIDEO, (stream) => {
           stream.size(960, 540);
         });
@@ -810,6 +811,7 @@ describe('HlsParser', () => {
       });
       manifest.addPartialVariant((variant) => {
         variant.bandwidth = 300;
+        variant.primary = false;
         variant.addPartialStream(ContentType.VIDEO, (stream) => {
           stream.size(960, 540);
         });
