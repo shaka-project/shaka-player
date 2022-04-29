1a6e93add8bea530e80d32e6e1edd2983c15b4e3
Álvaro Velad Galván <alvaro.velad@mirada.tv>
2022-04-29 23:46:36 +0200

    fix(dash): Fix playback of Dolby Atmos (#4173)
    According to https://developerkb.dolby.com/support/solutions/articles/16000067758-what-is-dolby-digital-plus-joc-joint-object-coding- Dolby Atmos (JOC):
    
    > Joint Object Coding describes the process by which Dolby Digital Plus with Atmos decoders, receiving a legacy 5.1 mix and sideband metadata, are able to reconstruct the original Atmos mix.
    
    So there is no need to deal with a separate mimetype, it can be decoded with Dolby Digital+ (better for retrocompatibility).
    
    Closes #4171

--- test/dash/dash_parser_manifest_unit.js
+++ test/dash/dash_parser_manifest_unit.js
@@ -445,30 +445,6 @@ describe('DashParser Manifest', () => {
         expect(stream.closedCaptions).toEqual(expectedClosedCaptions);
       });
 
-  it('Detects E-AC3 JOC content by SupplementalProperty', async () => {
-    const idUri = 'tag:dolby.com,2018:dash:EC3_ExtensionType:2018';
-    const source = [
-      '<MPD>',
-      '  <Period duration="PT30M">',
-      '    <AdaptationSet mimeType="audio/mp4" lang="\u2603">',
-      '      <Representation bandwidth="500">',
-      '        <SupplementalProperty schemeIdUri="' + idUri + '" value="JOC"/>',
-      '        <BaseURL>http://example.com</BaseURL>',
-      '        <SegmentTemplate media="2.mp4" duration="1" />',
-      '      </Representation>',
-      '    </AdaptationSet>',
-      '  </Period>',
-      '</MPD>',
-    ].join('\n');
-
-    fakeNetEngine.setResponseText('dummy://foo', source);
-
-    /** @type {shaka.extern.Manifest} */
-    const manifest = await parser.start('dummy://foo', playerInterface);
-    const stream = manifest.variants[0].audio;
-    expect(stream.mimeType).toBe('audio/eac3-joc');
-  });
-
   it('Detects spatial audio', async () => {
     const idUri = 'tag:dolby.com,2018:dash:EC3_ExtensionType:2018';
     const source = [
