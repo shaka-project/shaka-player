3b534fd405ad3254d37a86fd1895ceeb96dc8094
Álvaro Velad Galván <ladvan91@hotmail.com>
2022-10-26 00:55:02 +0200

    fix: Fix ID3 parsing in TS segments (#4609)

--- test/util/ts_parser_unit.js
+++ test/util/ts_parser_unit.js
@@ -46,7 +46,9 @@ describe('TsParser', () => {
     const metadata = new shaka.util.TsParser().parse(tsSegment)
         .getMetadata();
     expect(metadata).toBeTruthy();
-    expect(metadata.length).toBe(2);
+    expect(metadata.length).toBe(1);
+    const firstMetadata = metadata[0];
+    expect(firstMetadata.frames.length).toBe(2);
   });
 
   it('get the start time from a TS segment', async () => {
