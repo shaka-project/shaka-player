d927cec58921928da9a82be8234ac091e3f18322
Joey Parrish <joeyparrish@users.noreply.github.com>
2022-11-01 11:12:22 -0700

    fix: Allow overriding special handling of 404s (#4635)
    In general, streaming.failureCallback is meant to give applications
    control over error handling at the level of streaming. However, there
    was a special case for HTTP 404s built into StreamingEngine in a way
    that applications could not override. This was in spite of the fact that
    the default failureCallback would already check for and retry on the
    error code BAD_HTTP_STATUS.
    
    This removes the special case in StreamingEngine and refactors
    failureCallback and retryStreaming to preserve the special delay imposed
    in the old 404 handler. With this, applications can override
    failureCallback to have complete control over 404 handling.
    
    Closes #4548

--- test/media/streaming_engine_unit.js
+++ test/media/streaming_engine_unit.js
@@ -857,7 +857,7 @@ describe('StreamingEngine', () => {
 
     const config = shaka.util.PlayerConfiguration.createDefault().streaming;
     config.bufferingGoal = 60;
-    config.failureCallback = () => streamingEngine.retry();
+    config.failureCallback = () => streamingEngine.retry(0.1);
     createStreamingEngine(config);
 
     // Make requests for different types take different amounts of time.
@@ -1716,8 +1716,23 @@ describe('StreamingEngine', () => {
     beforeEach(() => {
       setupLive();
       mediaSourceEngine = new shaka.test.FakeMediaSourceEngine(segmentData, 0);
-      createStreamingEngine();
+
+      // Retry on failure for live streams.
+      const config = shaka.util.PlayerConfiguration.createDefault().streaming;
+      config.failureCallback = () => streamingEngine.retry(0.1);
+
+      createStreamingEngine(config);
       presentationTimeInSeconds = 100;
+
+      // Ignore 404 errors in live stream tests.
+      onError.and.callFake((error) => {
+        if (error.code == shaka.util.Error.Code.BAD_HTTP_STATUS &&
+            error.data[1] == 404) {
+          // 404 error
+        } else {
+          fail(error);
+        }
+      });
     });
 
     it('outside segment availability window', async () => {
@@ -1753,9 +1768,9 @@ describe('StreamingEngine', () => {
             if (startTime >= 100) {
               // Ignore a possible call for the first Period.
               expect(Util.invokeSpy(timeline.getSegmentAvailabilityStart))
-                  .toBe(100);
+                  .not.toBeLessThan(100);
               expect(Util.invokeSpy(timeline.getSegmentAvailabilityEnd))
-                  .toBe(120);
+                  .not.toBeLessThan(120);
               playing = true;
               mediaSourceEngine.appendBuffer.and.callFake(
                   originalAppendBuffer);
@@ -1980,7 +1995,7 @@ describe('StreamingEngine', () => {
       mediaSourceEngine = new shaka.test.FakeMediaSourceEngine(segmentData);
 
       const config = shaka.util.PlayerConfiguration.createDefault().streaming;
-      config.failureCallback = () => streamingEngine.retry();
+      config.failureCallback = () => streamingEngine.retry(0.1);
       createStreamingEngine(config);
 
       presentationTimeInSeconds = 100;
@@ -2141,7 +2156,7 @@ describe('StreamingEngine', () => {
         netEngine.request.calls.reset();
 
         // Retry streaming.
-        expect(streamingEngine.retry()).toBe(true);
+        expect(streamingEngine.retry(0.1)).toBe(true);
       });
 
       // Here we go!
@@ -2171,7 +2186,7 @@ describe('StreamingEngine', () => {
 
         // Retry streaming, which should fail and return false.
         netEngine.request.calls.reset();
-        expect(streamingEngine.retry()).toBe(false);
+        expect(streamingEngine.retry(0.1)).toBe(false);
       });
 
       // Here we go!
@@ -2223,7 +2238,7 @@ describe('StreamingEngine', () => {
 
           // Retry streaming, which should fail and return false.
           netEngine.request.calls.reset();
-          expect(streamingEngine.retry()).toBe(false);
+          expect(streamingEngine.retry(0.1)).toBe(false);
         }
       });
 
