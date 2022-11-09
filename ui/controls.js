fd93f6ae1bc917061c035abcfb835855e336bb06
PAPICO <ocipap0531@gmail.com>
2022-11-10 02:15:05 +0900

    fix(ui): Fix exception on screen rotation if fullscreen is not supported (#4669)
    If fullscreen is not supported when enableFullscreenOnRotation option is enabled, this exception occurred:
    
    TypeError: i.j.requestFullscreen is not a function. (In 'i.j.requestFullscreen({navigationUI:"hide"})', 'i.j.requestFullscreen' is undefined)
    
    This fixes the exception by checking isFullScreenSupported().
    
    Co-authored-by: Joey Parrish <joeyparrish@users.noreply.github.com>

--- ui/controls.js
+++ ui/controls.js
@@ -1024,7 +1024,10 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
     if (!this.video_ ||
         this.video_.readyState == 0 ||
         this.castProxy_.isCasting() ||
-        !this.config_.enableFullscreenOnRotation) { return; }
+        !this.config_.enableFullscreenOnRotation ||
+        !this.isFullScreenSupported()) {
+      return;
+    }
 
     if (screen.orientation.type.includes('landscape') &&
         !document.fullscreenElement) {
