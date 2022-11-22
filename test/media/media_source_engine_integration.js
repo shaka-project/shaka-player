f8668510f9cc10c2b6c83374b94937c853d02283
Joey Parrish <joeyparrish@google.com>
2022-11-21 16:09:48 -0800

    Expand nested cues

--- test/media/media_source_engine_integration.js
+++ test/media/media_source_engine_integration.js
@@ -38,21 +38,51 @@ describe('MediaSourceEngine', () => {
     startTime: Util.closeTo(0.767, 0.001),
     endTime: Util.closeTo(4.972, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(top left)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(top left)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   const tsCeaCue1 = jasmine.objectContaining({
     startTime: Util.closeTo(5.305, 0.001),
     endTime: Util.closeTo(11.979, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(middle)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(middle)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   const tsCeaCue2 = jasmine.objectContaining({
     startTime: Util.closeTo(12.312, 0.001),
     endTime: Util.closeTo(19.319, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(bottom left)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(bottom left)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   // The same segments as above, but offset by 40 seconds (yes, 40), which is
@@ -61,21 +91,51 @@ describe('MediaSourceEngine', () => {
     startTime: Util.closeTo(40.767, 0.001),
     endTime: Util.closeTo(44.972, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(top left)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(top left)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   const tsCeaCue4 = jasmine.objectContaining({
     startTime: Util.closeTo(45.305, 0.001),
     endTime: Util.closeTo(51.979, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(middle)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(middle)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   const tsCeaCue5 = jasmine.objectContaining({
     startTime: Util.closeTo(52.312, 0.001),
     endTime: Util.closeTo(59.319, 0.001),
     textAlign: Cue.textAlign.CENTER,
-    payload: 'These are 608 captions\n(bottom left)',
+    nestedCues: [
+      jasmine.objectContaining({
+        payload: 'These are 608 captions',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+      jasmine.objectContaining({lineBreak: true}),
+      jasmine.objectContaining({
+        payload: '(bottom left)',
+        textAlign: Cue.textAlign.CENTER,
+      }),
+    ],
   });
 
   /**
@@ -458,7 +518,6 @@ describe('MediaSourceEngine', () => {
     await appendWithClosedCaptions(ContentType.VIDEO, 0);
 
     expect(textDisplayer.appendSpy).toHaveBeenCalledTimes(3);
-
     expect(textDisplayer.appendSpy).toHaveBeenCalledWith([tsCeaCue0]);
     expect(textDisplayer.appendSpy).toHaveBeenCalledWith([tsCeaCue1]);
     expect(textDisplayer.appendSpy).toHaveBeenCalledWith([tsCeaCue2]);
