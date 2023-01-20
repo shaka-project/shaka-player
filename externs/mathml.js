39a4482985089978dc649de575dcb601f1a8ce98
Tobias Smolka <tosmolka@microsoft.com>
2023-01-20 12:42:39 +0100

    Add externs for MathML

---
+++ externs/mathml.js
@@ -0,0 +1,17 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for MathML markup.
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @extends {Element}
+ * @see https://w3c.github.io/mathml-core/#webidl-1630693908
+ */
+function MathMLElement(){}
