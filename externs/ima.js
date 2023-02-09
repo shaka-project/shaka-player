ea1ab66f484878cc97c70aeabbf6b22562204ed5
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-02-09 08:47:31 +0100

    fix(Demo): Allow manifest type for DAI custom assets (#4977)
    Fixes https://github.com/shaka-project/shaka-player/issues/4887

--- externs/ima.js
+++ externs/ima.js
@@ -501,6 +501,15 @@ google.ima.dai.api.StreamRequest.prototype.streamActivityMonitorId;
 google.ima.dai.api.StreamRequest.prototype.format;
 
 
+/**
+ * @enum {string}
+ */
+google.ima.dai.api.StreamRequest.StreamFormat = {
+  DASH: 'dash',
+  HLS: 'hls',
+};
+
+
 /** @const */
 google.ima.dai.api.VODStreamRequest =
     class extends google.ima.dai.api.StreamRequest {};
