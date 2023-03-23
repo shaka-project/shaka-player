8bffb99c6e19ca6f968490d8977518e61fc38ffd
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-03-23 16:58:57 +0100

    fix: Fix missing originalUri in response filters (#5114)

--- test/media/streaming_engine_unit.js
+++ test/media/streaming_engine_unit.js
@@ -3429,7 +3429,12 @@ describe('StreamingEngine', () => {
       netEngine.request.and.callFake((requestType, request, context) => {
         const buffer = new ArrayBuffer(0);
         /** @type {shaka.extern.Response} */
-        const response = {uri: request.uris[0], data: buffer, headers: {}};
+        const response = {
+          uri: request.uris[0],
+          originalUri: request.uris[0],
+          data: buffer,
+          headers: {},
+        };
         return shaka.util.AbortableOperation.completed(response);
       });
 
