051ef10aa431385b88b66bd4abc257ec75112313
Joey Parrish <joeyparrish@users.noreply.github.com>
2023-04-26 17:05:32 -0700

    test: Fix PlayReady server URL syntax (#5190)
    The padding on the base64-encoded content key was missing from the URL.
    This did not show up until I tested on Tizen.

--- test/test/util/test_scheme.js
+++ test/test/util/test_scheme.js
@@ -469,7 +469,7 @@ const axinomDrmServers = {
   'com.widevine.alpha':
       'https://cwip-shaka-proxy.appspot.com/specific_key?blodJidXR9eARuql0dNLWg=GX8m9XLIZNIzizrl0RTqnA',
   'com.microsoft.playready':
-      'https://test.playready.microsoft.com/service/rightsmanager.asmx?cfg=(kid:6e5a1d26-2757-47d7-8046-eaa5d1d34b5a,contentkey:GX8m9XLIZNIzizrl0RTqnA,sl:150)',
+      'https://test.playready.microsoft.com/service/rightsmanager.asmx?cfg=(kid:6e5a1d26-2757-47d7-8046-eaa5d1d34b5a,contentkey:GX8m9XLIZNIzizrl0RTqnA==,sl:150)',
 };
 
 /** @type {TextMetadataType} */
