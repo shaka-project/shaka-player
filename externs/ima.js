2a2cf00259e0d247f07888bfdd4355f4e93e1484
Jackson Sui <Kiro705@gmail.com>
2023-06-28 15:40:08 -0400

    chore: Update IMA externs (#5368)

--- externs/ima.js
+++ externs/ima.js
@@ -516,7 +516,7 @@ google.ima.dai.api.StreamRequest.prototype.format;
 /**
  * @type {Object.<
  *   google.ima.dai.api.OmidVerificationVendor,
- *   google.ima.dai.api.OmidAccessMode>}
+ *   (google.ima.dai.api.OmidAccessMode | undefined)>}
  */
 google.ima.dai.api.StreamRequest.prototype.omidAccessModeRules;
 
