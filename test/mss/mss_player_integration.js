fb4af826727a0ede510cfbc4be7b4164bcd028ca
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-08-18 01:18:21 +0200

    feat(MSS): Fix MSS PlayReady support (#5486)
    Closes https://github.com/shaka-project/shaka-player/issues/5390

--- test/mss/mss_player_integration.js
+++ test/mss/mss_player_integration.js
@@ -25,6 +25,12 @@ describe('MSS Player', () => {
   // eslint-disable-next-line max-len
   const url = 'https://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest';
 
+  // eslint-disable-next-line max-len
+  const playreadyUrl = 'https://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest';
+
+  // eslint-disable-next-line max-len
+  const playreadyLicenseUrl = 'https://test.playready.microsoft.com/service/rightsmanager.asmx?cfg=(persist:false,sl:150)';
+
   beforeAll(async () => {
     video = shaka.test.UiUtils.createVideoElement();
     document.body.appendChild(video);
@@ -67,6 +73,46 @@ describe('MSS Player', () => {
     player.configure('abr.restrictions.maxHeight', 1);
 
     await player.load(url, /* startTime= */ null,
+        /* mimeType= */ 'application/vnd.ms-sstr+xml');
+    video.play();
+    expect(player.isLive()).toBe(false);
+
+    // Wait for the video to start playback.  If it takes longer than 10
+    // seconds, fail the test.
+    await waiter.waitForMovementOrFailOnTimeout(video, 10);
+
+    // Play for 5 seconds, but stop early if the video ends.  If it takes
+    // longer than 10 seconds, fail the test.
+    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 5, 10);
+
+    await player.unload();
+  });
+
+  it('MSS VoD PlayReady', async () => {
+    const support = await shaka.media.DrmEngine.probeSupport();
+    if (!support['com.microsoft.playready']) {
+      return;
+    }
+    // Make sure we are playing the lowest res available to avoid test flake
+    // based on network issues.  Note that disabling ABR and setting a low
+    // abr.defaultBandwidthEstimate would not be sufficient, because it
+    // would only affect the choice of track on the first period.  When we
+    // cross a period boundary, the default bandwidth estimate will no
+    // longer be in effect, and AbrManager may choose higher res tracks for
+    // the new period.  Using abr.restrictions.maxHeight will let us force
+    // AbrManager to the lowest resolution, which is its fallback when these
+    // soft restrictions cannot be met.
+    player.configure('abr.restrictions.maxHeight', 1);
+
+    player.configure({
+      drm: {
+        servers: {
+          'com.microsoft.playready': playreadyLicenseUrl,
+        },
+      },
+    });
+
+    await player.load(playreadyUrl, /* startTime= */ null,
         /* mimeType= */ 'application/vnd.ms-sstr+xml');
     video.play();
     expect(player.isLive()).toBe(false);
