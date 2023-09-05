6bf90a19d1ef13db774bafd5ea689afa7cdb536a
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-09-05 13:10:14 +0200

    chore(Demo): Allow use com.apple.fps.1_0 in the custom DRM System field

--- demo/custom.js
+++ demo/custom.js
@@ -407,9 +407,14 @@ shakaDemo.Custom = class {
       const licenseServerURL = licenseServerUrlInput.value;
       const customDRMSystem = customDrmSystemInput.value;
       if (licenseServerURL) {
+        const filterFairplay =
+            customDRMSystem && customDRMSystem.startsWith('com.apple.fps');
         // Make a license server entry for every common DRM plugin.
         assetInProgress.licenseServers.clear();
         for (const drmSystem of shakaDemo.Main.commonDrmSystems) {
+          if (filterFairplay && drmSystem.startsWith('com.apple.fps')) {
+            continue;
+          }
           assetInProgress.licenseServers.set(drmSystem, licenseServerURL);
         }
         if (customDRMSystem) {
