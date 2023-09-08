276d5796f43e8e82709e968f4376ce26190ed199
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-09-08 08:53:57 +0200

    Delete miscoded asset

--- karma.conf.js
+++ karma.conf.js
@@ -242,7 +242,6 @@ module.exports = (config) => {
       {pattern: 'test/test/assets/hls-ts-ac3/*', included: false},
       {pattern: 'test/test/assets/hls-ts-ec3/*', included: false},
       {pattern: 'test/test/assets/hls-ts-h264/*', included: false},
-      {pattern: 'test/test/assets/hls-ts-h265/*', included: false},
       {pattern: 'test/test/assets/hls-ts-mp3/*', included: false},
       {pattern: 'test/test/assets/hls-ts-muxed-aac-h264/*', included: false},
       {pattern: 'test/test/assets/hls-ts-muxed-aac-h265/*', included: false},
