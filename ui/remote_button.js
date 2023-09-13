e1d371b35c9acb821badd63799913cdbd601307b
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-09-13 13:00:27 +0200

    feat(UI): Add remote button with RemotePlayback API

---
+++ ui/remote_button.js
@@ -0,0 +1,158 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+goog.provide('shaka.ui.RemoteButton');
+
+goog.require('shaka.Player');
+goog.require('shaka.ui.Controls');
+goog.require('shaka.ui.Element');
+goog.require('shaka.ui.Enums');
+goog.require('shaka.ui.Locales');
+goog.require('shaka.ui.Localization');
+goog.require('shaka.ui.OverflowMenu');
+goog.require('shaka.ui.Utils');
+goog.require('shaka.util.Dom');
+goog.require('shaka.util.Platform');
+goog.requireType('shaka.ui.Controls');
+
+
+/**
+ * @extends {shaka.ui.Element}
+ * @final
+ * @export
+ */
+shaka.ui.RemoteButton = class extends shaka.ui.Element {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls);
+
+    /** @private {!HTMLButtonElement} */
+    this.remoteButton_ = shaka.util.Dom.createButton();
+    this.remoteButton_.classList.add('shaka-remote-button');
+    this.remoteButton_.classList.add('shaka-tooltip');
+    this.remoteButton_.ariaPressed = 'false';
+
+    /** @private {!HTMLElement} */
+    this.remoteIcon_ = shaka.util.Dom.createHTMLElement('i');
+    this.remoteIcon_.classList.add('material-icons-round');
+    let icon = shaka.ui.Enums.MaterialDesignIcons.CAST;
+    if (shaka.util.Platform.safariVersion()) {
+      icon = shaka.ui.Enums.MaterialDesignIcons.AIRPLAY;
+    }
+    this.remoteIcon_.textContent = icon;
+    this.remoteButton_.appendChild(this.remoteIcon_);
+
+    const label = shaka.util.Dom.createHTMLElement('label');
+    label.classList.add('shaka-overflow-button-label');
+    label.classList.add('shaka-overflow-menu-only');
+    this.remoteNameSpan_ = shaka.util.Dom.createHTMLElement('span');
+    label.appendChild(this.remoteNameSpan_);
+
+    this.remoteCurrentSelectionSpan_ =
+      shaka.util.Dom.createHTMLElement('span');
+    this.remoteCurrentSelectionSpan_.classList.add(
+        'shaka-current-selection-span');
+    label.appendChild(this.remoteCurrentSelectionSpan_);
+    this.remoteButton_.appendChild(label);
+    this.parent.appendChild(this.remoteButton_);
+
+    /** @private {number} */
+    this.callbackId_ = -1;
+
+    // Setup strings in the correct language
+    this.updateLocalizedStrings_();
+
+    if (!this.video.remote || this.video.disableRemotePlayback) {
+      this.remoteButton_.classList.add('shaka-hidden');
+    } else {
+      this.eventManager.listen(
+          this.localization, shaka.ui.Localization.LOCALE_UPDATED, () => {
+            this.updateLocalizedStrings_();
+          });
+
+      this.eventManager.listen(
+          this.localization, shaka.ui.Localization.LOCALE_CHANGED, () => {
+            this.updateLocalizedStrings_();
+          });
+
+      this.eventManager.listen(this.remoteButton_, 'click', () => {
+        this.video.remote.prompt();
+      });
+
+      this.eventManager.listen(this.video.remote, 'connect', () => {
+        this.updateRemoteState_();
+      });
+
+      this.eventManager.listen(this.video.remote, 'connecting', () => {
+        this.updateRemoteState_();
+      });
+
+      this.eventManager.listen(this.video.remote, 'disconnect', () => {
+        this.updateRemoteState_();
+      });
+    }
+  }
+
+  /**
+   * @private
+   */
+  async updateRemoteState_() {
+    if (this.video.remote.state == 'disconnected') {
+      const handleAvailabilityChange = (availability) => {
+        const loadMode = this.player.getLoadMode();
+        const srcMode = loadMode == shaka.Player.LoadMode.SRC_EQUALS;
+        shaka.ui.Utils.setDisplay(this.remoteButton_, srcMode && availability);
+      };
+      try {
+        const id = await this.video.remote.watchAvailability(
+            handleAvailabilityChange);
+        this.callbackId_ = id;
+      } catch (e) {
+        handleAvailabilityChange(/* availability= */ true);
+      }
+    } else if (this.callbackId_ != -1) {
+      // If remote device is connecting or connected, we should stop
+      // watching remote device availability to save power.
+      await this.video.remote.cancelWatchAvailability(this.callbackId_);
+      this.callbackId_ = -1;
+    }
+  }
+
+  /**
+   * @private
+   */
+  updateLocalizedStrings_() {
+    const LocIds = shaka.ui.Locales.Ids;
+    let text = this.localization.resolve(LocIds.CAST);
+    if (shaka.util.Platform.safariVersion()) {
+      text = this.localization.resolve(LocIds.AIRPLAY);
+    }
+    this.remoteButton_.ariaLabel = text;
+    this.remoteNameSpan_.textContent = text;
+  }
+};
+
+
+/**
+ * @implements {shaka.extern.IUIElement.Factory}
+ * @final
+ */
+shaka.ui.RemoteButton.Factory = class {
+  /** @override */
+  create(rootElement, controls) {
+    return new shaka.ui.RemoteButton(rootElement, controls);
+  }
+};
+
+shaka.ui.OverflowMenu.registerElement(
+    'remote', new shaka.ui.RemoteButton.Factory());
+
+shaka.ui.Controls.registerElement(
+    'remote', new shaka.ui.RemoteButton.Factory());
