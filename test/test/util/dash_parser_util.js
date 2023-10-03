377b5ec01b10db35a8b5f964e28455c6815d5d83
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-10-03 17:48:11 +0200

    test: Stop correctly DASH parser (#5715)

--- test/test/util/dash_parser_util.js
+++ test/test/util/dash_parser_util.js
@@ -48,11 +48,15 @@ shaka.test.Dash = class {
       onManifestUpdated: () => {},
       getBandwidthEstimate: () => 1e6,
     };
-    const manifest = await dashParser.start('dummy://foo', playerInterface);
-    const stream = manifest.variants[0].video;
-    await stream.createSegmentIndex();
+    try {
+      const manifest = await dashParser.start('dummy://foo', playerInterface);
+      const stream = manifest.variants[0].video;
+      await stream.createSegmentIndex();
 
-    shaka.test.ManifestParser.verifySegmentIndex(stream, references);
+      shaka.test.ManifestParser.verifySegmentIndex(stream, references);
+    } finally {
+      dashParser.stop();
+    }
   }
 
   /**
@@ -86,9 +90,14 @@ shaka.test.Dash = class {
       onManifestUpdated: () => {},
       getBandwidthEstimate: () => 1e6,
     };
-    const p = dashParser.start('dummy://foo', playerInterface);
-    await expectAsync(p).toBeRejectedWith(
-        shaka.test.Util.jasmineError(expectedError));
+
+    try {
+      const p = dashParser.start('dummy://foo', playerInterface);
+      await expectAsync(p).toBeRejectedWith(
+          shaka.test.Util.jasmineError(expectedError));
+    } finally {
+      dashParser.stop();
+    }
   }
 
   /**
