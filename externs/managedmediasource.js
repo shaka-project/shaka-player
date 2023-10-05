ea08b74fb86ccabdef4d64c6cc2652ffd1e073d2
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-10-05 08:53:19 +0200

    Simplify extern

--- externs/managedmediasource.js
+++ externs/managedmediasource.js
@@ -13,63 +13,9 @@
 
 /**
  * @constructor
- * @implements {EventTarget}
+ * @extends {MediaSource}
  */
 function ManagedMediaSource() {}
-
-/** @override */
-ManagedMediaSource.prototype.addEventListener =
-    function(type, listener, optPptions) {};
-
-/** @override */
-ManagedMediaSource.prototype.removeEventListener =
-    function(type, listener, optPptions) {};
-
-/** @override */
-ManagedMediaSource.prototype.dispatchEvent = function(evt) {};
-
-/** @type {Array<SourceBuffer>} */
-ManagedMediaSource.prototype.sourceBuffers;
-
-/** @type {Array<SourceBuffer>} */
-ManagedMediaSource.prototype.activeSourceBuffers;
-
-/** @type {number} */
-ManagedMediaSource.prototype.duration;
-
-/**
- * @param {string} type
- * @return {SourceBuffer}
- */
-ManagedMediaSource.prototype.addSourceBuffer = function(type) {};
-
-/**
- * @param {SourceBuffer} sourceBuffer
- * @return {undefined}
- */
-ManagedMediaSource.prototype.removeSourceBuffer = function(sourceBuffer) {};
-
-/**
- * Updates the live seekable range.
- * @param {number} start
- * @param {number} end
- */
-ManagedMediaSource.prototype.setLiveSeekableRange = function(start, end) {};
-
-/**
- * Clears the live seekable range.
- * @return {void}
- */
-ManagedMediaSource.prototype.clearLiveSeekableRange = function() {};
-
-/** @type {string} */
-ManagedMediaSource.prototype.readyState;
-
-/**
- * @param {string=} optError
- * @return {undefined}
- */
-ManagedMediaSource.prototype.endOfStream = function(optError) {};
 
 /**
  * @param {string} type
