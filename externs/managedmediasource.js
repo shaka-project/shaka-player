01da5fa8a4b32c14582bd3cb865b5d5eca591d7f
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-10-11 11:22:17 +0200

    feat: Use ManagedMediaSource when available (#5683)
    The spec can be seen at https://github.com/w3c/media-source/issues/320
    
    Closes https://github.com/shaka-project/shaka-player/issues/5271

---
+++ externs/managedmediasource.js
@@ -0,0 +1,24 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for ManagedMediaSource which were missing in the
+ * Closure compiler.
+ *
+ * @externs
+ */
+
+/**
+ * @constructor
+ * @extends {MediaSource}
+ */
+function ManagedMediaSource() {}
+
+/**
+ * @param {string} type
+ * @return {boolean}
+ */
+ManagedMediaSource.isTypeSupported = function(type) {};
