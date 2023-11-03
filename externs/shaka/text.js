d41e06233bbd2a480d45ad05f9a856441ed263b7
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-11-03 11:34:57 +0100

    fix(TTML): Display subtitles from TTML with smpte:backgroundImage

--- externs/shaka/text.js
+++ externs/shaka/text.js
@@ -36,11 +36,13 @@ shaka.extern.TextParser = class {
    * @param {shaka.extern.TextParser.TimeContext} timeContext
    *    The time information that should be used to adjust the times values
    *    for each cue.
+   * @param {?string} uri
+   *    The media uri.
    * @return {!Array.<!shaka.text.Cue>}
    *
    * @exportDoc
    */
-  parseMedia(data, timeContext) {}
+  parseMedia(data, timeContext, uri) {}
 
   /**
    * Notifies the stream if the manifest is in sequence mode or not.
