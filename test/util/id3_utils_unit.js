01ac61bd82c969481f9f53fef895549bb8e69317
Przemysław Sobala <przemyslaw.sobala@grupawp.pl>
2023-11-03 18:41:15 +0100

    feat(ID3): decode APIC frames

--- test/util/id3_utils_unit.js
+++ test/util/id3_utils_unit.js
@@ -13,6 +13,44 @@ describe('Id3Utils', () => {
     expect(Id3Utils.getID3Frames(new Uint8Array([]))).toEqual([]);
   });
 
+  it('parse an APIC frame with image data', () => {
+    const apicValue = new Uint8Array([
+      3, 105, 109, 97, 103, 101, 47, 106, 112, 101, 103, 0, 3, 83, 104, 97,
+      107, 97, 0, 1, 2, 3,
+    ]);
+    const apicFrame = Id3Generator.generateId3Frame('APIC', apicValue);
+    const apicID3 = Id3Generator.generateId3(apicFrame);
+    const expectedID3 = [
+      {
+        key: 'APIC',
+        mimeType: 'image/jpeg',
+        pictureType: 3,
+        description: 'Shaka',
+        data: BufferUtils.toArrayBuffer(new Uint8Array([1, 2, 3])),
+      },
+    ];
+    expect(Id3Utils.getID3Frames(apicID3)).toEqual(expectedID3);
+  });
+
+  it('parse an APIC frame with image URL', () => {
+    const apicValue = new Uint8Array([
+      3, 45, 45, 62, 0, 3, 83, 104, 97, 107, 97, 0, 103, 111, 111, 103, 108,
+      101, 46, 99, 111, 109,
+    ]);
+    const apicFrame = Id3Generator.generateId3Frame('APIC', apicValue);
+    const apicID3 = Id3Generator.generateId3(apicFrame);
+    const expectedID3 = [
+      {
+        key: 'APIC',
+        mimeType: '-->',
+        pictureType: 3,
+        description: 'Shaka',
+        data: 'google.com',
+      },
+    ];
+    expect(Id3Utils.getID3Frames(apicID3)).toEqual(expectedID3);
+  });
+
   it('parse a TXXX frame', () => {
     const txxxValue = new Uint8Array([3, 65, 0, 83, 104, 97, 107, 97]);
     const txxxFrame = Id3Generator.generateId3Frame('TXXX', txxxValue);
