b06b136e18e2d016e3e6eed9bb09ef6a99b2dc89
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-11-28 12:50:48 +0100

    feat(UI): Add double tap to forward/rewind in the video

---
+++ ui/hidden_rewind_button.js
@@ -0,0 +1,117 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+goog.provide('shaka.ui.HiddenRewindButton');
+
+goog.require('shaka.ui.Element');
+goog.require('shaka.util.Timer');
+goog.require('shaka.ui.Enums');
+goog.require('shaka.util.Dom');
+
+goog.requireType('shaka.ui.Controls');
+
+/**
+ * @extends {shaka.ui.Element}
+ * @final
+ * @export
+ */
+shaka.ui.HiddenRewindButton = class extends shaka.ui.Element {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls);
+
+    /** @private {?number} */
+    this.lastTouchEventTimeSet_ = null;
+
+    /** @private {?boolean} */
+    this.triggeredTouchValid_ = false;
+
+    /**
+     * This timer will be used to hide rewind button on video Container.
+     * When the timer ticks it will force button to be invisible.
+     *
+     * @private {shaka.util.Timer}
+     */
+    this.hideRewindButtonContainerTimer_ = new shaka.util.Timer(() => {
+      this.hideRewindButtonContainer_();
+    });
+
+    /** @private {!HTMLElement} */
+    this.rewindContainer_ = shaka.util.Dom.createHTMLElement('div');
+    this.rewindContainer_.classList.add(
+        'shaka-rewind-container');
+    this.parent.appendChild(this.rewindContainer_);
+
+    this.eventManager.listen(this.rewindContainer_, 'touchstart', (event) => {
+      // In case any settings menu are open this assigns the first touch
+      // to close the menu.
+      if (this.controls.anySettingsMenusAreOpen()) {
+        // prevent the default changes that browser triggers
+        event.preventDefault();
+        this.controls.hideSettingsMenus();
+      } else if (this.controls.getConfig().tapSeekDistance > 0) {
+        // prevent the default changes that browser triggers
+        event.preventDefault();
+        this.onRewindButtonClick_();
+      }
+    });
+
+    /** @private {!HTMLElement} */
+    this.rewindValue_ = shaka.util.Dom.createHTMLElement('span');
+    this.rewindValue_.textContent = '0s';
+    this.rewindContainer_.appendChild(this.rewindValue_);
+
+    /** @private {!HTMLElement} */
+    this.rewindIcon_ = shaka.util.Dom.createHTMLElement('span');
+    this.rewindIcon_.classList.add(
+        'shaka-forward-rewind-container-icon');
+    this.rewindIcon_.textContent =
+        shaka.ui.Enums.MaterialDesignIcons.REWIND;
+    this.rewindContainer_.appendChild(this.rewindIcon_);
+  }
+
+  /**
+   * @private
+   */
+  onRewindButtonClick_() {
+    const tapSeekDistance = this.controls.getConfig().tapSeekDistance;
+    // This stores the time for first touch and makes touch valid for
+    // next 1s so incase the touch event is triggered again within 1s
+    // this if condition fails and the video seeking happens.
+    if (!this.triggeredTouchValid_) {
+      this.triggeredTouchValid_ = true;
+      this.lastTouchEventTimeSet_ = Date.now();
+      this.hideRewindButtonContainerTimer_.tickAfter(1);
+    } else if (this.lastTouchEventTimeSet_+1000 > Date.now()) {
+      // stops hidding of fast-forward button incase the timmer is active
+      // because of previous touch event.
+      this.hideRewindButtonContainerTimer_.stop();
+      this.lastTouchEventTimeSet_ = Date.now();
+      const position =
+          parseInt(this.rewindValue_.textContent, 10) - tapSeekDistance;
+      this.rewindValue_.textContent = position.toString() + 's';
+      this.rewindContainer_.style.opacity = '1';
+      this.hideRewindButtonContainerTimer_.tickAfter(1);
+    }
+  }
+
+  /**
+   * @private
+   */
+  hideRewindButtonContainer_() {
+    // Prevent adding seek value if its a single tap.
+    if (parseInt(this.rewindValue_.textContent, 10) != 0) {
+      this.video.currentTime = this.controls.getDisplayTime() + parseInt(
+          this.rewindValue_.textContent, 10);
+    }
+    this.rewindContainer_.style.opacity = '0';
+    this.triggeredTouchValid_ = false;
+    this.rewindValue_.textContent = '0s';
+  }
+};
