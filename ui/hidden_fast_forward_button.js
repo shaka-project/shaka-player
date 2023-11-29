918c30b25ae7d193bef82de95d6e0e807426cea9
Álvaro Velad Galván <ladvan91@hotmail.com>
2023-11-29 10:22:04 +0100

    feat(UI): Add double tap to forward/rewind in the video (#5943)
    Closes https://github.com/shaka-project/shaka-player/issues/3357
    Closes https://github.com/shaka-project/shaka-player/issues/3303
    
    Thanks to @surajkumar-sk, his
    https://github.com/shaka-project/shaka-player/pull/3373 has been the
    inspiration for this.

---
+++ ui/hidden_fast_forward_button.js
@@ -0,0 +1,32 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+goog.provide('shaka.ui.HiddenFastForwardButton');
+
+goog.require('shaka.ui.Enums');
+goog.require('shaka.ui.HiddenSeekButton');
+
+goog.requireType('shaka.ui.Controls');
+
+/**
+ * @extends {shaka.ui.HiddenSeekButton}
+ * @final
+ * @export
+ */
+shaka.ui.HiddenFastForwardButton = class extends shaka.ui.HiddenSeekButton {
+  /**
+   * @param {!HTMLElement} parent
+   * @param {!shaka.ui.Controls} controls
+   */
+  constructor(parent, controls) {
+    super(parent, controls);
+
+    this.seekContainer.classList.add('shaka-fast-foward-container');
+    this.seekIcon.textContent =
+        shaka.ui.Enums.MaterialDesignIcons.FAST_FORWARD;
+    this.isRewind = false;
+  }
+};
