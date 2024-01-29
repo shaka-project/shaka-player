f59ac9bcbef997f789d6f5c28b34d0f19de07452
Gary Katsevman <git@gkatsev.com>
2024-01-29 15:58:52 -0500

    parse target latency

--- externs/shaka/manifest.js
+++ externs/shaka/manifest.js
@@ -151,6 +151,7 @@ shaka.extern.InitDataOverride;
 
 /**
  * @typedef {{
+ *   targetLatency:?number,
  *   maxLatency: ?number,
  *   maxPlaybackRate: ?number,
  *   minLatency: ?number,
@@ -164,6 +165,8 @@ shaka.extern.InitDataOverride;
  * minPlaybackRate to increase  latency.
  * More information {@link https://dashif.org/docs/CR-Low-Latency-Live-r8.pdf here}.
  *
+ * @property {?number} targetLatency
+ *  The target latency to aim for.
  * @property {?number} maxLatency
  *  Maximum latency in seconds.
  * @property {?number} maxPlaybackRate
