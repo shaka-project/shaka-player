0992c5d28d6958d43176f3372e0d77fe56f83746
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-01-29 16:14:11 +0100

    feat: Detect maximum HW resolution automatically on some platforms

---
+++ externs/tizen.js
@@ -0,0 +1,41 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for Tizen
+ * @externs
+ */
+
+
+/** @const */
+var webapis = {};
+
+
+/** @const */
+webapis.systeminfo = {};
+
+
+/**
+ * @return {{width: number, height: number}}
+ */
+webapis.systeminfo.getMaxVideoResolution = function() {};
+
+
+/** @const */
+webapis.productinfo = {};
+
+
+/**
+ * @return {boolean}
+ */
+webapis.productinfo.is8KPanelSupported = function() {};
+
+
+/**
+ * @return {boolean}
+ */
+webapis.productinfo.isUdPanelSupported = function() {};
+
---
+++ externs/webos.js
@@ -0,0 +1,19 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for WebOS
+ *
+ * @externs
+ */
+
+
+/** @const */
+var PalmSystem = {};
+
+
+/** @type {string} */
+PalmSystem.deviceInfo;
