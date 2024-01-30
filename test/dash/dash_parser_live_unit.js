a3320707b9344a84242bcdc413f1bbf23bfa49e9
Iragne <adelskott@gmail.com>
2024-01-30 15:57:02 +0100

    fix(DASH): Update dash manifest when minimumUpdatePeriod = 0 (#6187)
    Fixes https://github.com/shaka-project/shaka-player/issues/6185

--- test/dash/dash_parser_live_unit.js
+++ test/dash/dash_parser_live_unit.js
@@ -642,6 +642,25 @@ describe('DashParser Live', () => {
     expect(updateTick).toHaveBeenCalledTimes(1);
   });
 
+  it('still updates when @minimumUpdatePeriod is zero', async () => {
+    const lines = [
+      '<SegmentTemplate startNumber="1" media="s$Number$.mp4" duration="2" />',
+    ];
+    // updateTime parameter sets @minimumUpdatePeriod in the manifest.
+    const manifestText = makeSimpleLiveManifestText(lines, /* updateTime= */ 0);
+
+    /** @type {!jasmine.Spy} */
+    const tickAfter = updateTickSpy();
+    Date.now = () => 0;
+
+    fakeNetEngine.setResponseText('dummy://foo', manifestText);
+    await parser.start('dummy://foo', playerInterface);
+
+    expect(tickAfter).toHaveBeenCalledTimes(1);
+    const delay = tickAfter.calls.mostRecent().args[0];
+    expect(delay).toBe(0);
+  });
+
   it('uses @minimumUpdatePeriod', async () => {
     const lines = [
       '<SegmentTemplate startNumber="1" media="s$Number$.mp4" duration="2" />',
