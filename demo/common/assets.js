9ddf231baba5609ad885841c00026d60bc02db3b
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-01-31 09:43:13 +0100

    fix(Demo): Fix MSS assets

--- demo/common/assets.js
+++ demo/common/assets.js
@@ -1428,16 +1428,19 @@ shakaAssets.testAssets = [
       /* source= */ shakaAssets.Source.MICROSOFT)
       .addFeature(shakaAssets.Feature.MSS)
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
-      .addFeature(shakaAssets.Feature.MP4),
+      .addFeature(shakaAssets.Feature.MP4)
+      .setMimeType('application/vnd.ms-sstr+xml'),
   new ShakaDemoAssetInfo(
       /* name= */ 'Super Speedway Trailer (MSS - PlayReady)',
       /* iconUri= */ 'https://reference.dashif.org/dash.js/latest/samples/lib/img/mss-1.jpg',
       /* manifestUri= */ 'https://test.playready.microsoft.com/smoothstreaming/SSWSS720H264PR/SuperSpeedway_720.ism/Manifest',
       /* source= */ shakaAssets.Source.MICROSOFT)
+      .addKeySystem(shakaAssets.KeySystem.PLAYREADY)
       .addFeature(shakaAssets.Feature.MSS)
       .addFeature(shakaAssets.Feature.HIGH_DEFINITION)
       .addFeature(shakaAssets.Feature.MP4)
-      .addLicenseServer('com.microsoft.playready', 'https://test.playready.microsoft.com/service/rightsmanager.asmx?cfg=(persist:false,sl:150)'),
+      .addLicenseServer('com.microsoft.playready', 'https://test.playready.microsoft.com/service/rightsmanager.asmx?cfg=(persist:false,sl:150)')
+      .setMimeType('application/vnd.ms-sstr+xml'),
   // }}}
 
   // MPEG-5 LCEVC assets {{{
