adfadeeb3091d89ff17b089ebf19ebf46224b1fd
Theodore Abshire <theodab@google.com>
2024-02-02 00:33:10 -0800

    feat: Add unloadAndSavePreload
    This adds a supplementary new method, unloadAndSavePreload. This method unloads the current
    asset but creates a PreloadManager object containing the loaded manifest, so the asset can be loaded
    again more quickly. This can be used for situations where an asset must be unloaded only temporarily;
    say, you wish to detach the player from the current media element and re-attach it to another, then begin
    playing the same asset.

--- test/player_integration.js
+++ test/player_integration.js
@@ -1382,4 +1382,13 @@ describe('Player', () => {
     await video.play();
     await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 1, 10);
   });
+
+  it('unloadAndSavePreload', async () => {
+    await player.load('test:sintel_compiled');
+    await video.play();
+    const preloadManager = await player.unloadAndSavePreload();
+    await player.load(preloadManager);
+    await video.play();
+    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 1, 10);
+  });
 });
