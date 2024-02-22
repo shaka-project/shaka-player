4a6bc1c382d500a4fd07748a5b44fa93e1a647cf
Joey Parrish <joeyparrish@users.noreply.github.com>
2024-02-22 10:27:35 -0800

    test: Fix several test failures (#6281)
    - Fix bogus assertion failure in tests on Safari (#6272)
    - This test should not be seeking so close to the duration (10ms) as to
    cause flakiness on certain browsers. Adjusting the seek time fixes the
    assertion without compromising the purpose of the test.
     - Fix uncaught rejections in load graph tests (#6274)
    - These tests had calls that were interrupted by the teardown process
    that occurs after the test body. Waiting for the operations before
    ending the test fixes the issue.
     - Fix interrupted load in test on Tizen (#6274)
    - This test triggers a deliberate DRM error without waiting for load().
    On most platforms in the lab, the load() operation completed before the
    error. Not so on Tizen. This exposed a general problem in which we
    should be catching errors in this test. Only timing saved us on most
    platforms.
    
    Closes #6272
    Closes #6274
    
    Backported to v4.3.x

--- test/player_load_graph_integration.js
+++ test/player_load_graph_integration.js
@@ -464,11 +464,11 @@ describe('Player Load Graph', () => {
 
       // Make the two requests one-after-another so that we don't have any idle
       // time between them.
-      const attachRequest = player.attach(video);
-      const loadRequest = player.load('test:sintel');
+      const attach = player.attach(video);
+      const load = player.load('test:sintel');
 
-      await attachRequest;
-      await expectAsync(loadRequest).toBeRejected();
+      await attach;
+      await expectAsync(load).toBeRejected();
 
       // Wait a couple interrupter cycles to allow the player to enter idle
       // state.
@@ -506,10 +506,12 @@ describe('Player Load Graph', () => {
       // Normally the player would initialize media source after attaching to
       // the media element, however since we don't support media source, it
       // should stop at the attach state.
-      player.attach(video, /* initMediaSource= */ true);
+      const attach = player.attach(video, /* initMediaSource= */ true);
 
       const event = await spyIsCalled(stateIdleSpy);
       expect(event.state).toBe('attach');
+
+      await attach;
     });
 
     it('loading ignores media source path', async () => {
@@ -518,10 +520,12 @@ describe('Player Load Graph', () => {
       // Normally the player would load content like this with the media source
       // path, but since we don't have media source support, it should use the
       // src= path.
-      player.load(SMALL_MP4_CONTENT_URI);
+      const load = player.load(SMALL_MP4_CONTENT_URI);
 
       const event = await spyIsCalled(stateIdleSpy);
       expect(event.state).toBe('src-equals');
+
+      await load;
     });
 
     it('unloading ignores init media source flag', async () => {
@@ -531,10 +535,12 @@ describe('Player Load Graph', () => {
       // Normally the player would try to go to the media source state because
       // we are saying to initialize media source after unloading, but since we
       // don't have media source, it should stop at the attach state.
-      player.unload(/* initMediaSource= */ true);
+      const unload = player.unload(/* initMediaSource= */ true);
 
       const event = await spyIsCalled(stateIdleSpy);
       expect(event.state).toBe('attach');
+
+      await unload;
     });
   });
 
