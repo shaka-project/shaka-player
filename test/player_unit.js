ef1f06ca84cd25e6a732bb7392d30cc6b58e46fd
Iragne <adelskott@gmail.com>
2024-05-06 07:52:53 +0200

    fix: reBufferingGoal is not respected  (#6433)
    Fixes https://github.com/shaka-project/shaka-player/issues/6355

--- test/player_unit.js
+++ test/player_unit.js
@@ -2683,6 +2683,7 @@ describe('Player', () => {
     });
 
     it('tracks info about current stream', () => {
+      forceBufferingTo(false);
       let stats = player.getStats();
       // Should have chosen the first of each type of stream.
       expect(stats.width).toBe(100);
