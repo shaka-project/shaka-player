03ea8ed6d8398b9eefc1376d23c874b5c4ede002
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-05-16 14:52:01 +0200

    Fix test execution in Tizen

--- test/ads_integration.js
+++ test/ads_integration.js
@@ -79,7 +79,15 @@ describe('Ads', () => {
   });
 
   describe('supports IMA SDK with vast', () => {
-    const imaTest = async () => {
+    it('with support for multiple media elements', async () => {
+      if (shaka.util.Platform.isSmartTV()) {
+        pending('Platform without support for multiple media elements.');
+      }
+      player.configure('ads.customPlayheadTracker', false);
+      player.configure('ads.skipPlayDetection', false);
+      player.configure('ads.supportsMultipleMediaElements', true);
+
+
       adManager.initClientSide(
           adContainer, video, /** adsRenderingSettings= **/ null);
 
@@ -103,7 +111,7 @@ describe('Ads', () => {
           'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
       adManager.requestClientSideAds(adRequest);
 
-      // The ad lasts 10 seconds. If it takes longer than 300 seconds, fail the
+      // The ad lasts 10 seconds. If it takes longer than 30 seconds, fail the
       // test.
       await waiter.timeoutAfter(30)
           .waitForEvent(adManager, shaka.ads.AdManager.AD_STOPPED);
@@ -113,23 +121,47 @@ describe('Ads', () => {
       await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 10, 30);
 
       await player.unload();
-    };
-
-    it('with support for multiple media elements', async () => {
-      if (shaka.util.Platform.isSmartTV()) {
-        pending('Platform without support for multiple media elements.');
-      }
-      player.configure('ads.customPlayheadTracker', false);
-      player.configure('ads.skipPlayDetection', false);
-      player.configure('ads.supportsMultipleMediaElements', true);
-      await imaTest();
     });
 
     it('without support for multiple media elements', async () => {
       player.configure('ads.customPlayheadTracker', true);
       player.configure('ads.skipPlayDetection', true);
       player.configure('ads.supportsMultipleMediaElements', false);
-      await imaTest();
+
+
+      adManager.initClientSide(
+          adContainer, video, /** adsRenderingSettings= **/ null);
+
+      await player.load('/base/test/test/assets/dash-aes-128/dash.mpd');
+      await video.play();
+      expect(player.isLive()).toBe(false);
+
+      // Wait for the video to start playback.  If it takes longer than 10
+      // seconds, fail the test.
+      await waiter.waitForMovementOrFailOnTimeout(video, 10);
+
+      // Play for 5 seconds, but stop early if the video ends.  If it takes
+      // longer than 20 seconds, fail the test.
+      await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 5, 20);
+
+      const adRequest = new google.ima.AdsRequest();
+      adRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+          'sz=640x480&iu=/124319096/external/single_ad_samples&' +
+          'ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&' +
+          'unviewed_position_start=1&' +
+          'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
+      adManager.requestClientSideAds(adRequest);
+
+      // The ad lasts 10 seconds. If it takes longer than 30 seconds, fail the
+      // test.
+      await waiter.timeoutAfter(30)
+          .waitForEvent(adManager, shaka.ads.AdManager.AD_STOPPED);
+
+      // Play for 10 seconds, but stop early if the video ends.  If it takes
+      // longer than 30 seconds, fail the test.
+      await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 10, 30);
+
+      await player.unload();
     });
   });
 });
