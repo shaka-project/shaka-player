7551b3d3e0ba82a0a44d3774d24ac798217b7938
Wojciech Tyczyński <wojciech.tyczynski@sky.uk>
2024-06-04 08:40:07 +0200

    Add windows check in fakeEncryption()

--- test/media/content_workarounds_unit.js
+++ test/media/content_workarounds_unit.js
@@ -45,6 +45,7 @@ describe('ContentWorkarounds', () => {
 
   it('faked encryption on Edge returns two init segments', () => {
     spyOn(shaka.util.Platform, 'isEdge').and.returnValue(true);
+    spyOn(shaka.util.Platform, 'isWindows').and.returnValue(true);
 
     const unencrypted = new Uint8Array([
       0x00, 0x00, 0x00, 0x20, // size
