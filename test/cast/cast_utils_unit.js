b500799b97abf41299b6cca81ef58412348f82f6
theodab <theodab@google.com>
2024-06-05 03:10:00 -0700

    feat(preload): Add new method destroyAllPreloads (#6756)

--- test/cast/cast_utils_unit.js
+++ test/cast/cast_utils_unit.js
@@ -31,6 +31,7 @@ describe('CastUtils', () => {
       'detachAndSavePreload',
       'unloadAndSavePreload',
       'preload',
+      'destroyAllPreloads',
       'getNonDefaultConfiguration',
 
       // Test helper methods (not @export'd)
