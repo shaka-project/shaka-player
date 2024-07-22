5717309c1ad05efdf207560e3c1e642d17eb1ba3
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-07-22 14:09:50 +0200

    test: Reduce test execution times (#7083)
    Integration testing times are reduced to the minimum necessary to test
    the functionality instead of a generic one.

--- test/hls/hls_parser_integration.js
+++ test/hls/hls_parser_integration.js
@@ -80,9 +80,9 @@ describe('HlsParser', () => {
     // seconds, fail the test.
     await waiter.waitForMovementOrFailOnTimeout(video, 10);
 
-    // Play for 10 seconds, but stop early if the video ends.  If it takes
+    // Play for 8 seconds, but stop early if the video ends.  If it takes
     // longer than 30 seconds, fail the test.
-    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 10, 30);
+    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 8, 30);
 
     await player.unload();
 
@@ -103,9 +103,9 @@ describe('HlsParser', () => {
     // seconds, fail the test.
     await waiter.waitForMovementOrFailOnTimeout(video, 10);
 
-    // Play for 10 seconds, but stop early if the video ends.  If it takes
+    // Play for 8 seconds, but stop early if the video ends.  If it takes
     // longer than 30 seconds, fail the test.
-    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 10, 30);
+    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 8, 30);
 
     await player.unload();
   });
