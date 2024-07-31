d36ff6553c174682d8d12b91a54669b6439ed768
Wojciech Tyczyński <tykus160@gmail.com>
2024-07-31 14:34:13 +0200

    feat(net): Add minimum bytes for progress events (#7117)
    By limiting progress events to only ones with minimal chunk size, we might end up with feeding better default ABR implementation, which should result in more accurate adaptation in case of network throttle.

--- externs/shaka/net.js
+++ externs/shaka/net.js
@@ -169,7 +169,8 @@ shaka.extern.Response;
  *                     shaka.extern.Request,
  *                     shaka.net.NetworkingEngine.RequestType,
  *                     shaka.extern.ProgressUpdated,
- *                     shaka.extern.HeadersReceived):
+ *                     shaka.extern.HeadersReceived,
+ *                     shaka.extern.SchemePluginConfig):
  *     !shaka.extern.IAbortableOperation.<shaka.extern.Response>}
  * @description
  * Defines a plugin that handles a specific scheme.
@@ -183,6 +184,23 @@ shaka.extern.Response;
  * @exportDoc
  */
 shaka.extern.SchemePlugin;
+
+
+/**
+ * @typedef {{
+*   minBytesForProgressEvents: (number|undefined)
+* }}
+*
+* @description
+*   Defines configuration object to use by SchemePlugins.
+*
+* @property {(number|undefined)} minBytesForProgressEvents
+*   Defines minimum number of bytes that should be use to emit progress event,
+*   if possible.
+*
+* @exportDoc
+*/
+shaka.extern.SchemePluginConfig;
 
 
 /**
