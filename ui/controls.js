52b48be55dad8e89dad4d2d6181521209d04df36
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-09-16 10:34:19 +0200

    fix(UI): Remove spinner and client side ad container elements when calling destroy (#7320)
    Fixes https://github.com/shaka-project/shaka-player/issues/7311
    
    Original author: @absidue

--- ui/controls.js
+++ ui/controls.js
@@ -259,6 +259,16 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
       this.castProxy_ = null;
     }
 
+    if (this.spinnerContainer_) {
+      this.videoContainer_.removeChild(this.spinnerContainer_);
+      this.spinnerContainer_ = null;
+    }
+
+    if (this.clientAdContainer_) {
+      this.videoContainer_.removeChild(this.clientAdContainer_);
+      this.clientAdContainer_ = null;
+    }
+
     if (this.localPlayer_) {
       await this.localPlayer_.destroy();
       this.localPlayer_ = null;
@@ -518,6 +528,8 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
    * @export
    */
   getClientSideAdContainer() {
+    goog.asserts.assert(
+        this.clientAdContainer_, 'No client ad container after destruction!');
     return this.clientAdContainer_;
   }
 
@@ -976,7 +988,7 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
 
   /** @private */
   addBufferingSpinner_() {
-    /** @private {!HTMLElement} */
+    /** @private {HTMLElement} */
     this.spinnerContainer_ = shaka.util.Dom.createHTMLElement('div');
     this.spinnerContainer_.classList.add('shaka-spinner-container');
     this.videoContainer_.appendChild(this.spinnerContainer_);
@@ -1133,7 +1145,7 @@ shaka.ui.Controls = class extends shaka.util.FakeEventTarget {
    * @private
    */
   addClientAdContainer_() {
-    /** @private {!HTMLElement} */
+    /** @private {HTMLElement} */
     this.clientAdContainer_ = shaka.util.Dom.createHTMLElement('div');
     this.clientAdContainer_.classList.add('shaka-client-side-ad-container');
     shaka.ui.Utils.setDisplay(this.clientAdContainer_, false);
