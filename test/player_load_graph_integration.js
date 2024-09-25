b9ba66f211cc2c8ec372f441d0690c81406b65b9
theodab <theodab@google.com>
2024-09-24 23:27:13 -0700

    fix(preload): Set manifest before initializing DRM (#7359)
    Previously, there were situations where, when handling `trackschanged` events, the manifest would not yet have been
    copied from the preload manager to the player. This would prevent developers from properly handling those events.
    
    This PR changes the order of operations slightly, such that the manifest is copied over earlier.

--- test/player_load_graph_integration.js
+++ test/player_load_graph_integration.js
@@ -150,16 +150,16 @@ describe('Player Load Graph', () => {
       'unload',
       'manifest-parser',
       'manifest',
-      'media-source',
       'drm-engine',
+      'media-source',
       'load',
 
       // Load 3
       'unload',
       'manifest-parser',
       'manifest',
-      'media-source',
       'drm-engine',
+      'media-source',
       'load',
     ]);
   });
