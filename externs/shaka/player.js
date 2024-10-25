7b38ca8b4938bf4e75828549e1844645e89d357c
Iragne <adelskott@gmail.com>
2024-10-25 17:55:09 +0200

    feat(HLS): Add the update period for HLS manifest (#7498)
    Create the update period for hls parse
    
    Close https://github.com/shaka-project/shaka-player/issues/7505

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -1179,10 +1179,10 @@ shaka.extern.xml.Node;
  *   <br>
  *   Defaults to <code>false</code>.
  * @property {number} updatePeriod
- *   Override the minimumUpdatePeriod of the manifest. The value is in second
- *   if the value is greater than the minimumUpdatePeriod, it will update the
- *   manifest less frequently. if you update the value during for a dynamic
- *   manifest, it will directly trigger a new download of the manifest
+ *   Override the minimumUpdatePeriod of the manifest. The value is in seconds.
+ *   If the value is greater than the minimumUpdatePeriod, it will update the
+ *   manifest less frequently. If you update the value during for a dynamic
+ *   manifest, it will directly trigger a new download of the manifest.
  *   <br>
  *   Defaults to <code>-1</code>.
  * @property {boolean} enableFastSwitching
@@ -1208,7 +1208,8 @@ shaka.extern.DashManifestConfiguration;
  *   ignoreManifestTimestampsInSegmentsMode: boolean,
  *   disableCodecGuessing: boolean,
  *   disableClosedCaptionsDetection: boolean,
- *   allowLowLatencyByteRangeOptimization: boolean
+ *   allowLowLatencyByteRangeOptimization: boolean,
+ *   updatePeriod: number
  * }}
  *
  * @property {boolean} ignoreTextStreamFailures
@@ -1294,6 +1295,14 @@ shaka.extern.DashManifestConfiguration;
  *   https://www.akamai.com/blog/performance/-using-ll-hls-with-byte-range-addressing-to-achieve-interoperabi
  *   <br>
  *   Defaults to <code>true</code>.
+ * @property {number} updatePeriod
+ *   Override the update period of the playlist. The value is in seconds.
+ *   If the value is less than 0, the period will be determined based on the
+ *   segment length.  If the value is greater than 0, it will update the target
+ *   duration.  If you update the value during the live, it will directly
+ *   trigger a new download of the manifest.
+ *   <br>
+ *   Defaults to <code>-1</code>.
  * @exportDoc
  */
 shaka.extern.HlsManifestConfiguration;
