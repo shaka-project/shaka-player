abd95643f655978842a4034c477c2720973a81b4
Iragne <adelskott@gmail.com>
2024-11-04 09:08:02 +0100

    Adding the extern

---
+++ externs/comcast.js
@@ -0,0 +1,19 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2016 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for Comcast
+ *
+ * @externs
+ */
+
+
+/** @const */
+var ServiceManager = {};
+
+
+/** @type {string} */
+ServiceManager.getServiceForJavaScript;
