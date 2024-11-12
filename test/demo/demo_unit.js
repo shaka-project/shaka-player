2a3a87d272d0f4d23c2722d41dd80414593f9ee9
Joey Parrish <joeyparrish@google.com>
2024-11-12 13:31:22 -0800

    Not adding ignoreHardwareResolution to the demo UI

--- test/demo/demo_unit.js
+++ test/demo/demo_unit.js
@@ -79,6 +79,7 @@ describe('Demo', () => {
     function checkConfig(checkValueNameFn) {
       const configPrimitives = new Set(['number', 'string', 'boolean']);
       const exceptions = new Set()
+          .add('ignoreHardwareResolution')
           .add('playRangeStart')
           .add('playRangeEnd')
           .add('manifest.dash.keySystemsByURI')
