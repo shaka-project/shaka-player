8e0a201bbb442d7edbea8c0e5cb5c607a0fcbb45
Álvaro Velad Galván <ladvan91@hotmail.com>
2024-11-26 17:47:41 +0100

    Add vast and vmap

--- test/ads/interstitial_ad_manager_unit.js
+++ test/ads/interstitial_ad_manager_unit.js
@@ -7,6 +7,9 @@
 describe('Interstitial Ad manager', () => {
   const TXml = shaka.util.TXml;
 
+  /** @type {!shaka.test.FakeNetworkingEngine} */
+  let networkingEngine;
+
   /** @type {!HTMLElement} */
   let adContainer;
   /** @type {!shaka.Player} */
@@ -19,9 +22,16 @@ describe('Interstitial Ad manager', () => {
   let interstitialAdManager;
 
   beforeEach(() => {
+    function dependencyInjector(player) {
+      // Create a networking engine that always returns an empty buffer.
+      networkingEngine = new shaka.test.FakeNetworkingEngine();
+      networkingEngine.setDefaultValue(new ArrayBuffer(0));
+      player.createNetworkingEngine = () => networkingEngine;
+    }
+
     adContainer =
       /** @type {!HTMLElement} */ (document.createElement('div'));
-    player = new shaka.Player();
+    player = new shaka.Player(null, null, dependencyInjector);
     mockVideo = new shaka.test.FakeVideo();
     onEventSpy = jasmine.createSpy('onEvent');
     interstitialAdManager = new shaka.ads.InterstitialAdManager(
@@ -452,6 +462,111 @@ describe('Interstitial Ad manager', () => {
         cuepoints: [
           {
             start: 10,
+            end: null,
+          },
+        ],
+      };
+      expect(onEventSpy).toHaveBeenCalledWith(
+          jasmine.objectContaining(eventValue1));
+    });
+  });
+
+  describe('VAST', () => {
+    it('basic interstitial support', async () => {
+      const vast = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<VAST version="3.0">',
+        '<Ad id="5925573263">',
+        '<InLine>',
+        '<Creatives>',
+        '<Creative id="138381721867" sequence="1">',
+        '<Linear>',
+        '<Duration>00:00:10</Duration>',
+        '<MediaFiles>',
+        '<MediaFile bitrate="140" delivery="progressive" ',
+        'height="360" type="video/mp4" width="640">',
+        '<![CDATA[test.mp4]]>',
+        '</MediaFile>',
+        '</MediaFiles>',
+        '</Linear>',
+        '</Creative>',
+        '</Creatives>',
+        '</InLine>',
+        '</Ad>',
+        '</VAST>',
+      ].join('');
+
+      networkingEngine.setResponseText('test:/vast', vast);
+
+      await interstitialAdManager.addAdUrlInterstitial('test:/vast');
+
+      expect(onEventSpy).toHaveBeenCalledTimes(1);
+      const eventValue1 = {
+        type: 'ad-cue-points-changed',
+        cuepoints: [
+          {
+            start: 0,
+            end: null,
+          },
+        ],
+      };
+      expect(onEventSpy).toHaveBeenCalledWith(
+          jasmine.objectContaining(eventValue1));
+    });
+  });
+
+  describe('VMAP', () => {
+    it('basic interstitial support', async () => {
+      const vmap = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<vmap:VMAP xmlns:vmap="http://www.iab.net/videosuite/vmap"',
+        ' version="1.0">',
+        '<vmap:AdBreak timeOffset="00:00:15.000" breakType="linear"',
+        ' breakId="midroll-1">',
+        '<vmap:AdSource id="midroll-1-ad-1" allowMultipleAds="false"',
+        ' followRedirects="true">',
+        '<vmap:AdTagURI templateType="vast3"><![CDATA[test:/vast]]>',
+        '</vmap:AdTagURI>',
+        '</vmap:AdSource>',
+        '</vmap:AdBreak>',
+        '</vmap:VMAP>',
+      ].join('');
+
+      const vast = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<VAST version="3.0">',
+        '<Ad id="5925573263">',
+        '<InLine>',
+        '<Creatives>',
+        '<Creative id="138381721867" sequence="1">',
+        '<Linear>',
+        '<Duration>00:00:10</Duration>',
+        '<MediaFiles>',
+        '<MediaFile bitrate="140" delivery="progressive" ',
+        'height="360" type="video/mp4" width="640">',
+        '<![CDATA[test.mp4]]>',
+        '</MediaFile>',
+        '</MediaFiles>',
+        '</Linear>',
+        '</Creative>',
+        '</Creatives>',
+        '</InLine>',
+        '</Ad>',
+        '</VAST>',
+      ].join('');
+
+      networkingEngine
+          .setResponseText('test:/vmap', vmap)
+          .setResponseText('test:/vast', vast);
+
+      await interstitialAdManager.addAdUrlInterstitial('test:/vmap');
+
+      expect(onEventSpy).toHaveBeenCalledTimes(1);
+      const eventValue1 = {
+        type: 'ad-cue-points-changed',
+        cuepoints: [
+          {
+            start: 15,
             end: null,
           },
         ],
