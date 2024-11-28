85aefa1850bba80408dfe44591a27798ad5772e0
Daniel Marino <1237997+CHaNGeTe@users.noreply.github.com>
2024-11-28 15:23:30 +0100

    fix: Functional test lint + refactor to expectAsync

--- test/util/functional_unit.js
+++ test/util/functional_unit.js
@@ -5,32 +5,30 @@
  */
 
 describe('Functional', () => {
-    const Functional = shaka.util.Functional;
-    describe('promiseWithTimeout', () => {
-        it('resolves if asyncProcess resolves within the timeout', async () => {
-            const asyncProcess = new Promise((resolve) => setTimeout(() => resolve('success'), 100));
-            const result = await Functional.promiseWithTimeout(1, asyncProcess);
-            expect(result).toBe('success');
-        });
-
-        it('rejects if asyncProcess rejects', async () => {
-            const asyncProcess = new Promise((_, reject) => setTimeout(() => reject('error'), 100));
-            try {
-                await Functional.promiseWithTimeout(1, asyncProcess);
-                fail('Expected promise to be rejected');
-            } catch (error) {
-                expect(error).toBe('error');
-            }
-        });
-
-        it('rejects if asyncProcess takes longer than the timeout', async () => {
-            const asyncProcess = new Promise((resolve) => setTimeout(() => resolve('success'), 2000));
-            try {
-                await Functional.promiseWithTimeout(1, asyncProcess);
-                fail('Expected promise to be rejected');
-            } catch (error) {
-                expect(error).toBeUndefined();
-            }
-        });
+  const Functional = shaka.util.Functional;
+  describe('promiseWithTimeout', () => {
+    it('resolves if asyncProcess resolves within the timeout', async () => {
+      const asyncProcess = new Promise((resolve) =>
+        setTimeout(() => resolve('success'), 100),
+      );
+      const result = await Functional.promiseWithTimeout(1, asyncProcess);
+      expect(result).toBe('success');
     });
+
+    it('rejects if asyncProcess rejects', async () => {
+      const asyncProcess = new Promise((_, reject) =>
+        setTimeout(() => reject('error'), 100),
+      );
+      const promise = Functional.promiseWithTimeout(1, asyncProcess);
+      await expectAsync(promise).toBeRejectedWith('error');
+    });
+
+    it('rejects if asyncProcess takes longer than the timeout', async () => {
+      const asyncProcess = new Promise((resolve) =>
+        setTimeout(() => resolve('success'), 2000),
+      );
+      const promise = Functional.promiseWithTimeout(1, asyncProcess);
+      await expectAsync(promise).toBeRejected();
+    });
+  });
 });
