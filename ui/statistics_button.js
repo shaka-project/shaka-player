a6fb9679e3ef0ee199f8003c21e3b840d7fd8e48
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-01-24 11:18:33 +0100

    chore: Stop using "Object" in UI annotations (#7936)
    Related to https://github.com/shaka-project/shaka-player/issues/1672

--- ui/statistics_button.js
+++ ui/statistics_button.js
@@ -80,8 +80,8 @@ shaka.ui.StatisticsButton = class extends shaka.ui.Element {
     /** @private {!Object<string, number>} */
     this.currentStats_ = this.player.getStats();
 
-    /** @private {!Object<string, HTMLElement>} */
-    this.displayedElements_ = {};
+    /** @private {!Map<string, HTMLElement>} */
+    this.displayedElements_ = new Map();
 
 
     const parsePx = (name) => {
@@ -136,33 +136,32 @@ shaka.ui.StatisticsButton = class extends shaka.ui.Element {
       }
     };
 
-    /** @private {!Object<string, function(string): string>} */
-    this.parseFrom_ = {
-      'width': parsePx,
-      'height': parsePx,
-      'completionPercent': parsePercent,
-      'bufferingTime': parseSeconds,
-      'drmTimeSeconds': parseSeconds,
-      'licenseTime': parseSeconds,
-      'liveLatency': parseSeconds,
-      'loadLatency': parseSeconds,
-      'manifestTimeSeconds': parseSeconds,
-      'estimatedBandwidth': parseBits,
-      'streamBandwidth': parseBits,
-      'maxSegmentDuration': parseTime,
-      'pauseTime': parseTime,
-      'playTime': parseTime,
-      'corruptedFrames': parseFrames,
-      'decodedFrames': parseFrames,
-      'droppedFrames': parseFrames,
-      'stallsDetected': parseStalls,
-      'gapsJumped': parseGaps,
-      'manifestSizeBytes': parseBytes,
-      'bytesDownloaded': parseBytes,
-      'nonFatalErrorCount': parseErrors,
-      'manifestPeriodCount': parsePeriods,
-      'manifestGapCount': parseGaps,
-    };
+    /** @private {!Map<string, function(string): string>} */
+    this.parseFrom_ = new Map()
+        .set('width', parsePx)
+        .set('height', parsePx)
+        .set('completionPercent', parsePercent)
+        .set('bufferingTime', parseSeconds)
+        .set('drmTimeSeconds', parseSeconds)
+        .set('licenseTime', parseSeconds)
+        .set('liveLatency', parseSeconds)
+        .set('loadLatency', parseSeconds)
+        .set('manifestTimeSeconds', parseSeconds)
+        .set('estimatedBandwidth', parseBits)
+        .set('streamBandwidth', parseBits)
+        .set('maxSegmentDuration', parseTime)
+        .set('pauseTime', parseTime)
+        .set('playTime', parseTime)
+        .set('corruptedFrames', parseFrames)
+        .set('decodedFrames', parseFrames)
+        .set('droppedFrames', parseFrames)
+        .set('stallsDetected', parseStalls)
+        .set('gapsJumped', parseGaps)
+        .set('manifestSizeBytes', parseBytes)
+        .set('bytesDownloaded', parseBytes)
+        .set('nonFatalErrorCount', parseErrors)
+        .set('manifestPeriodCount', parsePeriods)
+        .set('manifestGapCount', parseGaps);
 
     /** @private {shaka.util.Timer} */
     this.timer_ = new shaka.util.Timer(() => {
@@ -233,10 +232,10 @@ shaka.ui.StatisticsButton = class extends shaka.ui.Element {
     section.appendChild(label);
 
     const value = shaka.util.Dom.createHTMLElement('span');
-    value.textContent = this.parseFrom_[name](name);
+    value.textContent = this.parseFrom_.get(name)(name);
     section.appendChild(value);
 
-    this.displayedElements_[name] = value;
+    this.displayedElements_.set(name, value);
 
     return section;
   }
@@ -271,8 +270,8 @@ shaka.ui.StatisticsButton = class extends shaka.ui.Element {
     this.currentStats_ = this.player.getStats();
 
     for (const name of this.statisticsList_) {
-      const element = this.displayedElements_[name];
-      element.textContent = this.parseFrom_[name](name);
+      const element = this.displayedElements_.get(name);
+      element.textContent = this.parseFrom_.get(name)(name);
       if (element && element.parentElement) {
         shaka.ui.Utils.setDisplay(element.parentElement,
             !isNaN(this.currentStats_[name]));
