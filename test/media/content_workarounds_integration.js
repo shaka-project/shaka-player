de1f597f49a2c3838577f28f2c58dc198592c611
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-01-28 16:38:38 +0100

    More changes

--- test/media/content_workarounds_integration.js
+++ test/media/content_workarounds_integration.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-describe('DashParser', () => {
+describe('ContentWorkarounds', () => {
   const Util = shaka.test.Util;
 
   /** @type {!jasmine.Spy} */
@@ -21,14 +21,6 @@ describe('DashParser', () => {
 
   /** @type {!shaka.test.Waiter} */
   let waiter;
-
-  function checkClearKeySupport() {
-    const clearKeySupport = shakaSupport.drm['org.w3.clearkey'];
-    if (!clearKeySupport) {
-      return false;
-    }
-    return clearKeySupport.encryptionSchemes.includes('cenc');
-  }
 
   beforeAll(async () => {
     video = shaka.test.UiUtils.createVideoElement();
@@ -64,8 +56,13 @@ describe('DashParser', () => {
     document.body.removeChild(video);
   });
 
-  it('supports AES-128 streaming', async () => {
-    await player.load('/base/test/test/assets/dash-aes-128/dash.mpd');
+  // Check that fakeEC3 workaround is applied on the platforms where it is
+  // needed.
+  it('supports AC-3 if platform supports it', async () => {
+    if (!await Util.isTypeSupported('audio/mp4; codecs="ac-3"')) {
+      pending('Codec AC-3 is not supported by the platform.');
+    }
+    await player.load('/base/test/test/assets/dash-audio-ac3/dash.mpd');
     await video.play();
     expect(player.isLive()).toBe(false);
 
@@ -76,34 +73,6 @@ describe('DashParser', () => {
     // Play for 5 seconds, but stop early if the video ends.  If it takes
     // longer than 30 seconds, fail the test.
     await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 5, 30);
-
-    await player.unload();
-  });
-
-  it('supports ClearKey with raw single key', async () => {
-    if (!checkClearKeySupport()) {
-      pending('ClearKey is not supported');
-    }
-
-    player.configure({
-      drm: {
-        clearKeys: {
-          // cspell: disable-next-line
-          'nrQFDeRLSAKTLifXUIPiZg': 'FmY0xnWCPCNaSpRG-tUuTQ',
-        },
-      },
-    });
-    await player.load('/base/test/test/assets/dash-clearkey/dash.mpd');
-    await video.play();
-    expect(player.isLive()).toBe(false);
-
-    // Wait for the video to start playback.  If it takes longer than 10
-    // seconds, fail the test.
-    await waiter.waitForMovementOrFailOnTimeout(video, 10);
-
-    // Play for 8 seconds, but stop early if the video ends.  If it takes
-    // longer than 30 seconds, fail the test.
-    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 8, 30);
 
     await player.unload();
   });
