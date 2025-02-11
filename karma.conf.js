fc1b96a4cef8bb53169fa722232e5c7da963c4cd
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-02-11 19:10:38 +0100

    fix(HLS): Fix switching between muxed MP4 and muxed TS (#8061)

--- karma.conf.js
+++ karma.conf.js
@@ -267,6 +267,7 @@ module.exports = (config) => {
       {pattern: 'test/test/assets/dv-p10-av1/*', included: false},
       {pattern: 'test/test/assets/hls-aes-256/*', included: false},
       {pattern: 'test/test/assets/hls-interstitial/*', included: false},
+      {pattern: 'test/test/assets/hls-muxed-mp4-ts/*', included: false},
       {pattern: 'test/test/assets/hls-raw-aac/*', included: false},
       {pattern: 'test/test/assets/hls-raw-ac3/*', included: false},
       {pattern: 'test/test/assets/hls-raw-ec3/*', included: false},
