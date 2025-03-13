6f6e6df2179b958d9742cdc5285a69dbdc2ac324
Wojciech Tyczyński <wojciech.tyczynski@sky.uk>
2025-03-13 16:08:33 +0100

    satisfy compiler

--- test/util/lazy_unit.js
+++ test/util/lazy_unit.js
@@ -7,7 +7,7 @@
 describe('Lazy', () => {
   it('returns the same object each time', () => {
     const generator = jasmine.createSpy('generator').and.returnValue({});
-    const lazy = new shaka.util.Lazy(generator);
+    const lazy = new shaka.util.Lazy(shaka.test.Util.spyFunc(generator));
     const value = lazy.value();
     const value2 = lazy.value();
     expect(value).toBe(value2);
@@ -16,7 +16,7 @@ describe('Lazy', () => {
 
   it('works correctly for primitive value', () => {
     const generator = jasmine.createSpy('generator').and.returnValue(7);
-    const lazy = new shaka.util.Lazy(generator);
+    const lazy = new shaka.util.Lazy(shaka.test.Util.spyFunc(generator));
     const value = lazy.value();
     const value2 = lazy.value();
     expect(value).toBe(value2);
@@ -25,7 +25,7 @@ describe('Lazy', () => {
 
   it('works correctly for null', () => {
     const generator = jasmine.createSpy('generator').and.returnValue(null);
-    const lazy = new shaka.util.Lazy(generator);
+    const lazy = new shaka.util.Lazy(shaka.test.Util.spyFunc(generator));
     const value = lazy.value();
     expect(value).toBe(null);
     expect(generator).toHaveBeenCalledTimes(1);
