9c6006dab5d97ae380a6c09571b77903908ca33f
Darrin DuFord <darrinduford@gmail.com>
2025-03-21 08:43:11 -0400

    Updated userAgent strings

--- test/util/platform_unit.js
+++ test/util/platform_unit.js
@@ -31,9 +31,9 @@ describe('Platform', () => {
   // cspell: disable-next-line
   const vizio = 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 CrKey/1.0.999999 VIZIO SmartCast(Conjure/MTKF-5.1.516.1 FW/0.6.11.1-2 Model/V50C6-J09)';
   // cspell: disable-next-line
-  const chromecastBuiltinOrOlder = 'Mozilla/5.0 (X11; Linuz armv71) AppleWbKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000';
+  const chromecastBuiltinOrOlder = 'Mozilla/5.0 (X11; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000 DeviceType/Chromecast';
   // cspell: disable-next-line
-  const chromecastFuchsia = 'Mozilla/5.0 (Fuchsia) AppleWbKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.225 Safari/537.36 CrKey/1.56.500000';
+  const chromecastFuchsia = 'Mozilla/5.0 (Fuchsia) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 CrKey/1.56.500000';
   // cspell: disable-next-line
   const chromecastAndroid = 'Mozilla/5.0 (Linux; Android 12; Build/STTL.240206.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.0 Safari/537.36 CrKey/1.56.500000 DeviceType/AndroidTV';
   /* eslint-enable @stylistic/max-len */
