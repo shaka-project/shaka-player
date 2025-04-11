89584bf73717730105d8535a451b8a43f676a4b6
Wojciech Tyczyński <tykus160@gmail.com>
2025-04-11 12:24:36 +0200

    fix: Fix NTP conversion on Tizen 3 (#8444)

--- test/util/time_utils_unit.js
+++ test/util/time_utils_unit.js
@@ -8,7 +8,8 @@ describe('TimeUtils', () => {
   const TimeUtils = shaka.util.TimeUtils;
 
   it('should convert NTP timestamp to UTC time', () => {
-    const ntpStart = Date.UTC(1900);
+    // NTP time starts at 1900-01-01T00:00:00Z
+    const ntpStart = -2208988800000;
     expect(TimeUtils.convertNtp(0)).toBe(ntpStart);
     expect(TimeUtils.convertNtp(1000)).toBe(ntpStart + 1000);
   });
