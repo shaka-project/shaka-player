00f05407f00d386fb6820c4367901b4e58c59d79
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-04-25 15:14:06 +0200

    fix: Fix Firefox PlayReady support (#8497)
    See: https://bugzilla.mozilla.org/show_bug.cgi?id=1806566 and
    https://bugzilla.mozilla.org/show_bug.cgi?id=1960673
    This also adds mapping for other browsers on Windows, since Chromium is
    working on the same functionality
    (https://issues.chromium.org/issues/378869813).

--- test/cast/cast_receiver_integration.js
+++ test/cast/cast_receiver_integration.js
@@ -204,8 +204,8 @@ filterDescribe('CastReceiver', castReceiverIntegrationSupport, () => {
       for (const message of messages) {
         // Check that the update message is of a reasonable size. From previous
         // testing we found that the socket would silently reject data that got
-        // too big. 7KB is safely below the limit.
-        expect(message.length).toBeLessThan(7000);
+        // too big. 7.1KB is safely below the limit.
+        expect(message.length).toBeLessThan(7100);
       }
     });
 
@@ -270,8 +270,8 @@ filterDescribe('CastReceiver', castReceiverIntegrationSupport, () => {
       for (const message of messages) {
         // Check that the update message is of a reasonable size. From previous
         // testing we found that the socket would silently reject data that got
-        // too big. 7KB is safely below the limit.
-        expect(message.length).toBeLessThan(7000);
+        // too big. 7.1KB is safely below the limit.
+        expect(message.length).toBeLessThan(7100);
       }
     });
 
