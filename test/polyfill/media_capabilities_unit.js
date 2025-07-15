9ef8b8dab17e303fc24e09549904f33a7575d255
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-07-15 12:57:45 +0200

    feat: Use MCap to clear if available for MSE when using MCap polyfill
    Most MCap implementations fail in the DRM and when using src=, but we can use it in all other cases. This PR attempts to address those cases.

--- test/polyfill/media_capabilities_unit.js
+++ test/polyfill/media_capabilities_unit.js
@@ -202,9 +202,10 @@ describe('MediaCapabilities', () => {
           await navigator.mediaCapabilities.decodingInfo(mockDecodingConfig);
 
           expect(mockCanDisplayType).not.toHaveBeenCalled();
+          // 1 (for MCap clear check) +
           // 1 (for video config check) +
           // 1 (for audio config check).
-          expect(isChromecastSpy).toHaveBeenCalledTimes(2);
+          expect(isChromecastSpy).toHaveBeenCalledTimes(3);
           // 1 (fallback in canCastDisplayType()) +
           // 1 (mockDecodingConfig.audio).
           expect(supportMap.has(mockDecodingConfig.video.contentType))
@@ -225,9 +226,10 @@ describe('MediaCapabilities', () => {
           await navigator.mediaCapabilities.decodingInfo(mockDecodingConfig);
 
           expect(mockCanDisplayType).not.toHaveBeenCalled();
+          // 1 (for MCap clear check) +
           // 1 (for video config check) +
           // 1 (for audio config check).
-          expect(isChromecastSpy).toHaveBeenCalledTimes(2);
+          expect(isChromecastSpy).toHaveBeenCalledTimes(3);
           // 1 (fallback in canCastDisplayType()) +
           // 1 (mockDecodingConfig.audio).
           expect(supportMap.has(mockDecodingConfig.video.contentType))
@@ -272,9 +274,10 @@ describe('MediaCapabilities', () => {
       shaka.polyfill.MediaCapabilities.install();
       await navigator.mediaCapabilities.decodingInfo(mockDecodingConfig);
 
+      // 1 (for MCap clear check) +
       // 1 (for video config check) +
       // 1 (for audio config check).
-      expect(isChromecastSpy).toHaveBeenCalledTimes(2);
+      expect(isChromecastSpy).toHaveBeenCalledTimes(3);
       // 1 (mockDecodingConfig.audio).
       expect(supportMap.has(chromecastType)).toBe(true);
       // Called once in canCastDisplayType.
