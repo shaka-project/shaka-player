shaka-project/shaka-player

commit f6df1f0b96cec7e0ff864c8a93e7ba13d66b80d2

fix: Fix Firefox PlayReady support (#8497)
See: https://bugzilla.mozilla.org/show_bug.cgi?id=1806566 and
https://bugzilla.mozilla.org/show_bug.cgi?id=1960673
This also adds mapping for other browsers on Windows, since Chromium is
working on the same functionality
(https://issues.chromium.org/issues/378869813).

lib/drm/drm_engine.js
@@ -603,33 +603,30 @@ shaka.drm.DrmEngine = class {
           (initDataOverride) => initDataOverride.initData.length > 0,
       ) || null) : null;
 
+    const keySystem = this.currentDrmInfo_.keySystem;
+    const needWaitForEncryptedEvent =
+        keySystem.startsWith('com.microsoft.playready.recommendation') ||
+        keySystem == 'com.apple.fps';
+
     /**
      * We can attach media keys before the playback actually begins when:
      *  - If we are not using FairPlay Modern EME
      *  - Some initData already has been generated (through the manifest)
      *  - In case of an offline session
      */
-    if (this.manifestInitData_ ||
-        this.currentDrmInfo_.keySystem !== 'com.apple.fps' ||
-        this.storedPersistentSessions_.size) {
+    if (!needWaitForEncryptedEvent &&
+        (this.manifestInitData_ || this.storedPersistentSessions_.size ||
+        this.config_.parseInbandPsshEnabled)) {
       await this.attachMediaKeys_();
+    } else {
+      this.eventManager_.listen(
+          this.video_, 'encrypted', (e) => this.onEncryptedEvent_(e));
     }
 
     this.createOrLoad().catch(() => {
       // Silence errors
       // createOrLoad will run async, errors are triggered through onError_
     });
-
-    // Explicit init data for any one stream or an offline session is
-    // sufficient to suppress 'encrypted' events for all streams.
-    // Also suppress 'encrypted' events when parsing in-band pssh
-    // from media segments because that serves the same purpose as the
-    // 'encrypted' events.
-    if (!this.manifestInitData_ && !this.storedPersistentSessions_.size &&
-        !this.config_.parseInbandPsshEnabled) {
-      this.eventManager_.listen(
-          this.video_, 'encrypted', (e) => this.onEncryptedEvent_(e));
-    }
   }
 
   /**

lib/util/player_configuration.js
@@ -108,6 +108,15 @@ shaka.util.PlayerConfiguration = class {
       drm.preferredKeySystems.push('com.microsoft.playready');
     }
 
+    // Other browsers different than Edge only supports HW PlayReady with the
+    // recommendation keysystem on Windows, so we do a direct mapping here.
+    if (shaka.util.Platform.isWindows() && !shaka.util.Platform.isEdge()) {
+      drm.keySystemsMapping = {
+        'com.microsoft.playready':
+            'com.microsoft.playready.recommendation',
+      };
+    }
+
     let codecSwitchingStrategy = shaka.config.CodecSwitchingStrategy.RELOAD;
     let multiTypeVariantsAllowed = false;
     if (shaka.media.Capabilities.isChangeTypeSupported() &&

test/cast/cast_receiver_integration.js
@@ -204,8 +204,8 @@ filterDescribe('CastReceiver', castReceiverIntegrationSupport, () => {
       for (const message of messages) {
         // Check that the update message is of a reasonable size. From previous
         // testing we found that the socket would silently reject data that got
-        // too big. 7KB is safely below the limit.
-        expect(message.length).toBeLessThan(7000);
+        // too big. 7.1KB is safely below the limit.
+        expect(message.length).toBeLessThan(7100);
       }
     });
 
@@ -269,8 +269,8 @@ filterDescribe('CastReceiver', castReceiverIntegrationSupport, () => {
       for (const message of messages) {
         // Check that the update message is of a reasonable size. From previous
         // testing we found that the socket would silently reject data that got
-        // too big. 7KB is safely below the limit.
-        expect(message.length).toBeLessThan(7000);
+        // too big. 7.1KB is safely below the limit.
+        expect(message.length).toBeLessThan(7100);
       }
     });
 

commit 4e1e9e66ec63fbaa4706e0f237c97d195eb1c604

fix: Attach mediaKeys when the device does not force us to wait for the encrypted event (#9059)
Fixes https://github.com/shaka-project/shaka-player/issues/9032

Regression introduced in
https://github.com/shaka-project/shaka-player/pull/8497

lib/drm/drm_engine.js
@@ -608,18 +608,28 @@ shaka.drm.DrmEngine = class {
      *  - In case of an offline session
      */
     if (!needWaitForEncryptedEvent &&
-        (this.manifestInitData_ || this.storedPersistentSessions_.size ||
-        this.config_.parseInbandPsshEnabled)) {
+        (this.manifestInitData_ ||
+        this.currentDrmInfo_.keySystem !== 'com.apple.fps' ||
+        this.storedPersistentSessions_.size)) {
       await this.attachMediaKeys_();
-    } else {
-      this.eventManager_.listen(
-          this.video_, 'encrypted', (e) => this.onEncryptedEvent_(e));
     }
 
     this.createOrLoad().catch(() => {
       // Silence errors
       // createOrLoad will run async, errors are triggered through onError_
     });
+
+    // Explicit init data for any one stream or an offline session is
+    // sufficient to suppress 'encrypted' events for all streams.
+    // Also suppress 'encrypted' events when parsing in-band pssh
+    // from media segments because that serves the same purpose as the
+    // 'encrypted' events.
+    if (needWaitForEncryptedEvent ||
+        (!this.manifestInitData_ && !this.storedPersistentSessions_.size &&
+        !this.config_.parseInbandPsshEnabled)) {
+      this.eventManager_.listen(
+          this.video_, 'encrypted', (e) => this.onEncryptedEvent_(e));
+    }
   }
 
   /**