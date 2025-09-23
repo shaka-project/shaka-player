89301bd41afcc21ff7be79f056695f2ec03affc7
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-09-23 11:23:47 +0200

    Update docs

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -1880,6 +1880,7 @@ shaka.extern.LiveSyncConfiguration;
  *   <br>
  *   This feature creates a text track that works like any other, but only
  *   renders when a <code>videoContainer</code> is provided to the player.
+ *   You can recognize this track by its language which is 'speech-to-text'.
  *   <br>
  *   This functionality might work with SW DRM, but it will never work with
  *   HW DRM.
@@ -1900,6 +1901,8 @@ shaka.extern.LiveSyncConfiguration;
  *   When set to true, indicates a requirement that the speech recognition
  *   process MUST be performed locally on the user’s device. If set to false,
  *   the user agent can choose between local and remote processing.
+ *   Note: remote processing is done by the browser and we have no control
+ *   over what 3rd parties are involved.
  *   <br>
  *   Defaults to <code>false</code>.
  *
