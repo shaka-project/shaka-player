bf42b2ef18926c28ee1a864473445588d7ddc97d
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-09-24 12:28:08 +0200

    Add more tests

--- test/text/speech_to_text_unit.js
+++ test/text/speech_to_text_unit.js
@@ -8,21 +8,60 @@ describe('SpeechToText', () => {
   /** @type {!shaka.Player} */
   let player;
 
+  /** @type {shaka.extern.SpeechToTextConfiguration} */
+  let config;
+
+  /** @type {shaka.text.SpeechToText} */
+  let speechToText;
+
+  const originalSpeechRecognition = window.SpeechRecognition;
+
   beforeEach(() => {
     shaka.text.SpeechToText.isMediaStreamTrackSupported.reset();
 
     player = new shaka.Player();
+
+    const defaultConfig = shaka.util.PlayerConfiguration.createDefault();
+
+    config = defaultConfig.streaming.speechToText;
   });
 
   afterEach(async () => {
+    window.SpeechRecognition = originalSpeechRecognition;
+    if (speechToText) {
+      speechToText.release();
+    }
     await player.destroy();
   });
 
-  it('isSupported return false when no SpeechRecognition support', () => {
-    const orig = window.SpeechRecognition;
-    delete window.SpeechRecognition;
-    const speechToText = new shaka.text.SpeechToText(player);
-    expect(speechToText.isSupported()).toBe(false);
-    window.SpeechRecognition = orig;
+  describe('when no SpeechRecognition support', () => {
+    beforeEach(() => {
+      delete window.SpeechRecognition;
+    });
+
+    it('isSupported returns false', () => {
+      speechToText = new shaka.text.SpeechToText(player);
+      expect(speechToText.isSupported()).toBe(false);
+    });
+
+    it('isEnabled returns false', () => {
+      speechToText = new shaka.text.SpeechToText(player);
+      expect(speechToText.isEnabled()).toBe(false);
+    });
+
+    it('getTextTracks returns empty', () => {
+      speechToText = new shaka.text.SpeechToText(player);
+      expect(speechToText.getTextTracks()).toEqual([]);
+    });
+
+    it('disable do nothing', () => {
+      speechToText = new shaka.text.SpeechToText(player);
+      speechToText.disable();
+    });
+
+    it('configure works', () => {
+      speechToText = new shaka.text.SpeechToText(player);
+      speechToText.configure(config);
+    });
   });
 });
