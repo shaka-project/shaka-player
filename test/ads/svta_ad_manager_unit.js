595be61cc084834519f146e3fab707f463541ef9
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-10-08 10:18:33 +0200

    Add more tests

--- test/ads/svta_ad_manager_unit.js
+++ test/ads/svta_ad_manager_unit.js
@@ -115,6 +115,34 @@ describe('SVTA Ad manager', () => {
           jasmine.objectContaining(eventValue1));
     });
 
+    it('basic support with alt type', async () => {
+      const metadata = {
+        type: 'urn:svta:advertising-wg:ad-id-signaling',
+        startTime: 0,
+        endTime: 5,
+        values: [
+          {
+            key: 'X-AD-CREATIVE-SIGNALING',
+            data: signalingSimple,
+          },
+        ],
+      };
+      await svtaAdManager.addMetadata(metadata);
+
+      expect(onEventSpy).toHaveBeenCalledTimes(1);
+      const eventValue1 = {
+        type: 'ad-cue-points-changed',
+        cuepoints: [
+          {
+            start: 0,
+            end: 5,
+          },
+        ],
+      };
+      expect(onEventSpy).toHaveBeenCalledWith(
+          jasmine.objectContaining(eventValue1));
+    });
+
     it('supports multiple', async () => {
       const metadata = {
         type: 'urn:svta:advertising-wg:ad-creative-signaling',
@@ -208,8 +236,7 @@ describe('SVTA Ad manager', () => {
           jasmine.objectContaining(eventValue1));
     });
 
-    it('ignore invalid', async () => {
-      // It is not valid because it does not have an interstitial URL
+    it('ignore invalid data', async () => {
       const metadata = {
         type: 'urn:svta:advertising-wg:ad-creative-signaling',
         startTime: 0,
@@ -218,6 +245,43 @@ describe('SVTA Ad manager', () => {
           {
             key: 'X-AD-CREATIVE-SIGNALING',
             data: '',
+          },
+        ],
+      };
+      await svtaAdManager.addMetadata(metadata);
+
+      expect(onEventSpy).not.toHaveBeenCalled();
+    });
+
+    it('ignore invalid type', async () => {
+      const metadata = {
+        type: 'urn:svta',
+        startTime: 0,
+        endTime: 5,
+        values: [
+          {
+            key: 'X-AD-CREATIVE-SIGNALING',
+            data: signalingSimple,
+          },
+        ],
+      };
+      await svtaAdManager.addMetadata(metadata);
+
+      expect(onEventSpy).not.toHaveBeenCalled();
+    });
+
+    it('ignore if not asset loaded', async () => {
+      player.dispatchEvent(new shaka.util.FakeEvent(
+          shaka.util.FakeEvent.EventName.Unloading));
+
+      const metadata = {
+        type: 'urn:svta:advertising-wg:ad-creative-signaling',
+        startTime: 0,
+        endTime: 5,
+        values: [
+          {
+            key: 'X-AD-CREATIVE-SIGNALING',
+            data: signalingSimple,
           },
         ],
       };
@@ -245,6 +309,40 @@ describe('SVTA Ad manager', () => {
         endTime: 5,
         id: 'TEST',
         schemeIdUri: 'urn:svta:advertising-wg:ad-creative-signaling',
+        eventNode,
+        value: '',
+        timescale: 1,
+      };
+      await svtaAdManager.addRegion(region);
+
+      expect(onEventSpy).toHaveBeenCalledTimes(1);
+      const eventValue1 = {
+        type: 'ad-cue-points-changed',
+        cuepoints: [
+          {
+            start: 0,
+            end: 5,
+          },
+        ],
+      };
+      expect(onEventSpy).toHaveBeenCalledWith(
+          jasmine.objectContaining(eventValue1));
+    });
+
+    it('basic support with alt schemeIdUri', async () => {
+      const eventString = [
+        '<Event duration="5" id="TEST" presentationTime="0">',
+        signalingSimple,
+        '</Event>',
+      ].join('');
+      const eventNode = TXml.parseXmlString(eventString);
+      goog.asserts.assert(eventNode, 'Should have a event node!');
+      /** @type {shaka.extern.TimelineRegionInfo} */
+      const region = {
+        startTime: 0,
+        endTime: 5,
+        id: 'TEST',
+        schemeIdUri: 'urn:svta:advertising-wg:ad-id-signaling',
         eventNode,
         value: '',
         timescale: 1,
@@ -338,10 +436,58 @@ describe('SVTA Ad manager', () => {
           jasmine.objectContaining(eventValue1));
     });
 
-    it('ignore invalid', async () => {
-      // It is not valid because it does not have an interstitial URL
+    it('ignore invalid schemeIdUri', async () => {
       const eventString = [
         '<Event duration="5" id="TEST" presentationTime="0">',
+        signalingSimple,
+        '</Event>',
+      ].join('');
+      const eventNode = TXml.parseXmlString(eventString);
+      goog.asserts.assert(eventNode, 'Should have a event node!');
+      /** @type {shaka.extern.TimelineRegionInfo} */
+      const region = {
+        startTime: 0,
+        endTime: 5,
+        id: 'TEST',
+        schemeIdUri: 'urn:svta',
+        eventNode,
+        value: '',
+        timescale: 1,
+      };
+      await svtaAdManager.addRegion(region);
+
+      expect(onEventSpy).not.toHaveBeenCalled();
+    });
+
+    it('ignore invalid data', async () => {
+      const eventString = [
+        '<Event duration="5" id="TEST" presentationTime="0">',
+        '</Event>',
+      ].join('');
+      const eventNode = TXml.parseXmlString(eventString);
+      goog.asserts.assert(eventNode, 'Should have a event node!');
+      /** @type {shaka.extern.TimelineRegionInfo} */
+      const region = {
+        startTime: 0,
+        endTime: 5,
+        id: 'TEST',
+        schemeIdUri: 'urn:svta:advertising-wg:ad-creative-signaling',
+        eventNode,
+        value: '',
+        timescale: 1,
+      };
+      await svtaAdManager.addRegion(region);
+
+      expect(onEventSpy).not.toHaveBeenCalled();
+    });
+
+    it('ignore if not asset loaded', async () => {
+      player.dispatchEvent(new shaka.util.FakeEvent(
+          shaka.util.FakeEvent.EventName.Unloading));
+
+      const eventString = [
+        '<Event duration="5" id="TEST" presentationTime="0">',
+        signalingSimple,
         '</Event>',
       ].join('');
       const eventNode = TXml.parseXmlString(eventString);
