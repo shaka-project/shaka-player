c2193a80ccb301a2a676b5eb83ef6a9661076a52
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-11-19 11:39:14 +0100

    Add tests

--- test/drm/playready_unit.js
+++ test/drm/playready_unit.js
@@ -43,4 +43,29 @@ describe('PlayReady', () => {
     const actual = shaka.drm.PlayReady.getLicenseUrl(input);
     expect(actual).toBe('www.example.com');
   });
+
+  it('getLicenseUrlFromPssh', () => {
+    // cspell:disable
+    const psshString =
+        'AAACvnBzc2gAAAAAmgTweZhAQoarkuZb4IhflQAAAp6eAgAAAQABAJQCPABXAFIATQBI' +
+        'AEUAQQBEAEUAUgAgAHgAbQBsAG4AcwA9ACIAaAB0AHQAcAA6AC8ALwBzAGMAaABlAG0A' +
+        'YQBzAC4AbQBpAGMAcgBvAHMAbwBmAHQALgBjAG8AbQAvAEQAUgBNAC8AMgAwADAANwAv' +
+        'ADAAMwAvAFAAbABhAHkAUgBlAGEAZAB5AEgAZQBhAGQAZQByACIAIAB2AGUAcgBzAGkA' +
+        'bwBuAD0AIgA0AC4AMwAuADAALgAwACIAPgA8AEQAQQBUAEEAPgA8AFAAUgBPAFQARQBD' +
+        'AFQASQBOAEYATwA+ADwASwBJAEQAUwA+ADwASwBJAEQAIABBAEwARwBJAEQAPQAiAEEA' +
+        'RQBTAEMAQgBDACIAIABWAEEATABVAEUAPQAiAFoAMABVAGoAQQBhAHUASgA3ADgAMABC' +
+        'AEkAMABWAG4AaQBhAHYATgA3AHcAPQA9ACIAPgA8AC8ASwBJAEQAPgA8AC8ASwBJAEQA' +
+        'UwA+ADwALwBQAFIATwBUAEUAQwBUAEkATgBGAE8APgA8AEwAQQBfAFUAUgBMAD4AaAB0' +
+        'AHQAcABzADoALwAvAHAAbABhAHkAcgBlAGEAZAB5AC4AZQB6AGQAcgBtAC4AYwBvAG0A' +
+        'LwBjAGUAbgBjAHkALwBwAHIAZQBhAHUAdABoAC4AYQBzAHAAeAA/AHAAWAA9ADIANwA0' +
+        'AEYARgA0ADwALwBMAEEAXwBVAFIATAA+ADwARABTAF8ASQBEAD4AVgBsAFIANwBJAGQA' +
+        'cwBJAEoARQB1AFIAZAAwADYATABhAHEAcwAyAGoAdwA9AD0APAAvAEQAUwBfAEkARAA+' +
+        'ADwALwBEAEEAVABBAD4APAAvAFcAUgBNAEgARQBBAEQARQBSAD4A';
+    // cspell:enable
+
+    const data = shaka.util.Uint8ArrayUtils.fromBase64(psshString);
+    const actual = shaka.drm.PlayReady.getLicenseUrlFromPssh(data);
+    const expected = 'https://playready.ezdrm.com/cency/preauth.aspx?pX=274FF4';
+    expect(actual).toBe(expected);
+  });
 });
