8cf1a6f64faa8a1c1d2b78bfacce1b6e9faf41c9
Álvaro Velad Galván <ladvan91@hotmail.com>
2025-12-02 09:26:46 +0100

    Fix UI test

--- test/ui/ui_integration.js
+++ test/ui/ui_integration.js
@@ -105,7 +105,7 @@ describe('UI', () => {
     eventManager.listen(controls, 'error', Util.spyFunc(onErrorSpy));
 
     // These tests expect a default text track to be selected.
-    player.configure('preferredTextLanguage', 'en');
+    player.configure('preferredTextLanguage', 'zh');
 
     await player.load('test:sintel_multi_lingual_multi_res_compiled');
     // For this event, we ignore a timeout, since we sometimes miss this event
