shaka-project/shaka-player

commit 876511bad63cb31b6a23d24c472e9337a5cc28d2

chore: Use only raw fields in gh api calls (#9482)
Workflows using gh api should always use -f (raw field) instead of -F
(field including special characters) because a crafted message could be
used to read files from the host, which could lead to things like leaked
keys or other private information.

There is no known exploit, because these messages were not yet
controllable by an attacker as far as we know, but better safe than
sorry.

Discovered during a careful review of #9422, which adds new usage of gh
api.

.github/workflows/shaka-bot-commands/lib.sh
@@ -30,13 +30,13 @@ function check_required_variable() {
 # Leaving a comment requires a token with "repo" scope.
 function reply() {
   echo "@$COMMENTER: $@" | \
-      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -F -
+      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -f -
 }
 
 # Leaving a comment requires a token with "repo" scope.
 function reply_from_pipe() {
   (echo -n "@$COMMENTER: "; cat /dev/stdin) | \
-      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -F -
+      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -f -
 }
 
 # Checking permissions requires a token with "repo" and "org:read" scopes, and

.github/workflows/report-incremental-coverage.yaml
@@ -52,11 +52,11 @@ jobs:
             gh api \
               --method POST \
               /repos/${{ github.repository }}/issues/$PR_NUMBER/comments \
-              -F "body=$MESSAGE"
+              -f "body=$MESSAGE"
           else
             # Update an old comment
             gh api \
               --method PATCH \
               /repos/${{ github.repository }}/issues/comments/$(cat old-comment-id) \
-              -F "body=$MESSAGE"
+              -f "body=$MESSAGE"
           fi

commit 5d318811f4c736fd2a2dab0152ad3f1840f8bbaf

ci: Fix shaka-bot issue comments
Leaving issue comments was broken in #9482, which changed gh api's -F to
-f for security reasons.  However, gh issue comment doesn't have -f,
only -F, and this is distinct from gh api's problematic -F.

This reverts back to -F for gh issue comment.

.github/workflows/shaka-bot-commands/lib.sh
@@ -30,13 +30,13 @@ function check_required_variable() {
 # Leaving a comment requires a token with "repo" scope.
 function reply() {
   echo "@$COMMENTER: $@" | \
-      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -f -
+      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -F -
 }
 
 # Leaving a comment requires a token with "repo" scope.
 function reply_from_pipe() {
   (echo -n "@$COMMENTER: "; cat /dev/stdin) | \
-      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -f -
+      gh issue comment "$PR_NUMBER" -R "$THIS_REPO" -F -
 }
 
 # Checking permissions requires a token with "repo" and "org:read" scopes, and