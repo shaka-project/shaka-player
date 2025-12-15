da7bb4d27656c7056aa8ce659de50cc50a5affb6
Soumalya Bhattacharya <59192460+opticSquid@users.noreply.github.com>
2025-12-15 15:11:32 +0530

    fix(UI): Make Overflow/Settings Menu position dynamic (#9395)
    Close #9287
    
    ---------
    
    Co-authored-by: Álvaro Velad Galván <ladvan91@hotmail.com>

--- ui/overflow_menu.js
+++ ui/overflow_menu.js
@@ -230,6 +230,7 @@ shaka.ui.OverflowMenu = class extends shaka.ui.Element {
    * @private
    */
   adjustCustomStyle_() {
+    // Compute max height
     const rectMenu = this.overflowMenu_.getBoundingClientRect();
     const styleMenu = window.getComputedStyle(this.overflowMenu_);
     const paddingTop = parseFloat(styleMenu.paddingTop);
@@ -239,6 +240,27 @@ shaka.ui.OverflowMenu = class extends shaka.ui.Element {
         rectMenu.bottom - rectContainer.top - paddingTop - paddingBottom;
 
     this.overflowMenu_.style.maxHeight = heightIntersection + 'px';
+
+    // Compute horizontal position
+    const bottomControlsPos = this.controlsContainer_.getBoundingClientRect();
+    const overflowMenuButtonPos =
+        this.overflowMenuButton_.getBoundingClientRect();
+    const leftGap = overflowMenuButtonPos.left - bottomControlsPos.left;
+    const rightGap = bottomControlsPos.right - overflowMenuButtonPos.right;
+    const EDGE_PADDING = 15;
+    const MIN_GAP = 60;
+    // Overflow menu button is either placed to the left or center
+    if (leftGap < rightGap) {
+      const left = leftGap < MIN_GAP ?
+          EDGE_PADDING : Math.max(leftGap, EDGE_PADDING);
+      this.overflowMenu_.style.left = left + 'px';
+      this.overflowMenu_.style.right = 'auto';
+    } else {
+      const right = rightGap < MIN_GAP ?
+          EDGE_PADDING : Math.max(rightGap, EDGE_PADDING);
+      this.overflowMenu_.style.right = right + 'px';
+      this.overflowMenu_.style.left = 'auto';
+    }
   }
 };
 
