dea6127a6f6373bde519ec664441b486f4326c81
KimKyuHoi <k546kh@gmail.com>
2025-12-31 12:55:10 +0900

    test: update tests for array-based language config

--- test/hls/hls_parser_integration.js
+++ test/hls/hls_parser_integration.js
@@ -105,7 +105,7 @@ describe('HlsParser', () => {
   });
 
   it('supports text discontinuity', async () => {
-    player.configure('preferredTextLanguage', 'en');
+    player.configure('preferredTextLanguages', ['en']);
     await player.load('/base/test/test/assets/hls-text-offset/index.m3u8');
     await video.play();
 
@@ -125,7 +125,7 @@ describe('HlsParser', () => {
   });
 
   it('supports text without discontinuity', async () => {
-    player.configure('preferredTextLanguage', 'de');
+    player.configure('preferredTextLanguages', ['de']);
     // eslint-disable-next-line @stylistic/max-len
     await player.load('/base/test/test/assets/hls-text-no-discontinuity/index.m3u8');
     await video.play();
