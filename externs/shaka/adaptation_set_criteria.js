6aa32e704275589ffa16d6d6caeba885ad009bdf
KimKyuHoi <k546kh@gmail.com>
2026-02-17 10:06:50 +0900

    fix: Keep deprecated fields in AdaptationSetCriteria.Configuration for backward compatibility

--- externs/shaka/adaptation_set_criteria.js
+++ externs/shaka/adaptation_set_criteria.js
@@ -64,6 +64,17 @@ shaka.extern.AdaptationSetCriteria.Factory;
  * @typedef {{
  *   preferredAudio: !Array<!shaka.extern.AudioPreference>,
  *   preferredVideo: !Array<!shaka.extern.VideoPreference>,
+ *   language: string,
+ *   role: string,
+ *   videoRole: string,
+ *   channelCount: number,
+ *   hdrLevel: string,
+ *   spatialAudio: boolean,
+ *   videoLayout: string,
+ *   audioLabel: string,
+ *   videoLabel: string,
+ *   preferredAudioCodecs: !Array<string>,
+ *   preferredAudioChannelCount: number,
  *   codecSwitchingStrategy: shaka.config.CodecSwitchingStrategy,
  *   audioCodec: string,
  *   activeAudioCodec: string,
@@ -75,6 +86,50 @@ shaka.extern.AdaptationSetCriteria.Factory;
  *   An ordered list of audio preferences used to filter variants.
  * @property {!Array<!shaka.extern.VideoPreference>} preferredVideo
  *   An ordered list of video preferences used to filter variants.
+ * @property {string} language
+ *   @deprecated Use preferredAudio instead.
+ *   The language used to filter variants. Populated from
+ *   preferredAudio[0].language.
+ * @property {string} role
+ *   @deprecated Use preferredAudio instead.
+ *   The adaptation audio role used to filter variants. Populated from
+ *   preferredAudio[0].role.
+ * @property {string} videoRole
+ *   @deprecated Use preferredVideo instead.
+ *   The adaptation video role used to filter variants. Populated from
+ *   preferredVideo[0].role.
+ * @property {number} channelCount
+ *   @deprecated Use preferredAudio instead.
+ *   The audio channel count used to filter variants. Populated from
+ *   preferredAudio[0].channelCount.
+ * @property {string} hdrLevel
+ *   @deprecated Use preferredVideo instead.
+ *   The HDR level used to filter variants. Populated from
+ *   preferredVideo[0].hdrLevel.
+ * @property {boolean} spatialAudio
+ *   @deprecated Use preferredAudio instead.
+ *   Whether should prefer audio tracks with spatial audio. Populated from
+ *   preferredAudio[0].spatialAudio.
+ * @property {string} videoLayout
+ *   @deprecated Use preferredVideo instead.
+ *   The video layout used to filter variants. Populated from
+ *   preferredVideo[0].layout.
+ * @property {string} audioLabel
+ *   @deprecated Use preferredAudio instead.
+ *   The audio label used to filter variants. Populated from
+ *   preferredAudio[0].label.
+ * @property {string} videoLabel
+ *   @deprecated Use preferredVideo instead.
+ *   The video label used to filter variants. Populated from
+ *   preferredVideo[0].label.
+ * @property {!Array<string>} preferredAudioCodecs
+ *   @deprecated Use preferredAudio instead.
+ *   The ordered list of audio codecs to filter variants. Populated from
+ *   preferredAudio[*].codec.
+ * @property {number} preferredAudioChannelCount
+ *   @deprecated Use preferredAudio instead.
+ *   The preferred audio channel count to filter variants. Populated from
+ *   preferredAudio[0].channelCount.
  * @property {shaka.config.CodecSwitchingStrategy} codecSwitchingStrategy
  *   The codec switching strategy used to filter variants.
  * @property {string} audioCodec
