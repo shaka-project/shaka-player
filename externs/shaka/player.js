2e732b539201ac82f5958637f0c51fa0285fe309
vlazh <vzh.inbox@gmail.com>
2026-03-06 15:04:00 +0500

    feat: Add clampAppendWindowToDuration option (#9699)
    Fixes #9674

--- externs/shaka/player.js
+++ externs/shaka/player.js
@@ -2003,6 +2003,7 @@ shaka.extern.SpeechToTextConfiguration;
  *   crossBoundaryStrategy: shaka.config.CrossBoundaryStrategy,
  *   returnToEndOfLiveWindowWhenOutside: boolean,
  *   stopFetchingOnPause: boolean,
+ *   clampAppendWindowToDuration: boolean,
  * }}
  *
  * @description
@@ -2254,6 +2255,13 @@ shaka.extern.SpeechToTextConfiguration;
  *   If true, stop fetching new segments on pause. This applies as long as
  *   there is something in the buffer; if there is nothing, we will allow the
  *   loading of the current segment.
+ *   <br>
+ *   Defaults to <code>false</code>.
+ * @property {boolean} clampAppendWindowToDuration
+ *   If true, limit the buffer to the presentation duration (cap append window
+ *   at it) so the buffer never extends past it (e.g. HLS when one track is
+ *   longer). Otherwise MediaSource.duration can grow and video never reaches
+ *   "ended" when seeking to end.
  *   <br>
  *   Defaults to <code>false</code>.
  * @exportDoc
