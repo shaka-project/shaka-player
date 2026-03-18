0dd79cfb30f7b7210c8d6503549bb3cc4a1518cf
Julian Domingo <juliandomingo@google.com>
2026-03-18 13:30:35 -0700

    Add integration test using muxed audio, MPEG-2 TS, HLS live stream

--- test/hls/hls_parser_integration.js
+++ test/hls/hls_parser_integration.js
@@ -241,4 +241,24 @@ describe('HlsParser', () => {
 
     await player.unload();
   });
+
+  it('plays bipbop 16x9 with muxed audio', async () => {
+    // This asset has muxed audio in the video stream (no separate audio URI
+    // for the default audio group).
+    const bipbopUrl = 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8';
+
+    await player.load(bipbopUrl);
+
+    // Verify that we are using the expected variant.
+    const variants = player.getVariantTracks();
+    expect(variants.length).toBeGreaterThan(0);
+
+    await video.play();
+
+    // Wait for the video to start playback.
+    await waiter.waitForMovementOrFailOnTimeout(video, 20);
+
+    // Play for a few seconds.
+    await waiter.waitUntilPlayheadReachesOrFailOnTimeout(video, 5, 40);
+  });
 });
