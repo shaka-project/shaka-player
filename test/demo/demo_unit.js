040214e88df9185a633db4041f1a9a2b59eec346
Álvaro Velad Galván <ladvan91@hotmail.com>
2026-03-30 12:29:41 +0200

    fix

--- test/demo/demo_unit.js
+++ test/demo/demo_unit.js
@@ -68,7 +68,7 @@ describe('Demo', () => {
 
       const raw = asset.toJSON();
 
-      expect(Object.hasOwnProperty('testPolluted')).toBe(false);
+      expect(raw.hasOwnProperty('testPolluted')).toBe(false);
     });
 
     it('does not accept dangerous keys when parsing saved assets', () => {
