b0e3015962b0d11d5a0021ed9747bb4aadb7288c
Álvaro Velad Galván <ladvan91@hotmail.com>
2026-04-22 10:13:53 +0200

    feat(UI): Allow custom format and imageQuality in takeScreenshot and copyVideoFrameToClipboard (#10012)

---
+++ externs/clipboard.js
@@ -0,0 +1,19 @@
+/*! @license
+ * Shaka Player
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Externs for Clipboard API which were missing in the
+ * Closure compiler.
+ *
+ * @externs
+ */
+
+
+/**
+ * @param {string} type
+ * @return {boolean}
+ */
+ClipboardItem.supports = function(type) {};
