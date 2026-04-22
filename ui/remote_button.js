17258115b9cde96ca8f323e2ab1217735e24e22c
KimKyuHoi <k546kh@gmail.com>
2026-04-23 00:11:44 +0900

    fix(ui): revert code and check null

--- ui/remote_button.js
+++ ui/remote_button.js
@@ -197,8 +197,8 @@ shaka.ui.RemoteButton = class extends shaka.ui.Element {
         this.callbackId_ = -1;
       }
     }
-    this.remoteButton_.ariaPressed = this.remote_.state == 'connected' ?
-    'true' : 'false';
+    this.remoteButton_.ariaPressed =
+        this.remote_?.state == 'connected' ? 'true' : 'false';
   }
 
 
