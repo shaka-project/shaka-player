6dcf8ba46372056f7c0bce2d7cebbca3449374f2
Álvaro Velad Galván <ladvan91@hotmail.com>
2026-05-21 09:41:45 +0200

    fix: Map the "audio/x-mpegurl" MIME type as HLS (#10114)

--- test/util/mime_utils_unit.js
+++ test/util/mime_utils_unit.js
@@ -80,6 +80,7 @@ describe('MimeUtils', () => {
 
     expect(isHlsType('application/x-mpegurl')).toBe(true);
     expect(isHlsType('application/vnd.apple.mpegurl')).toBe(true);
+    expect(isHlsType('audio/x-mpegurl')).toBe(true);
     expect(isHlsType('application/dash+xml')).toBe(false);
     expect(isHlsType('application/vnd.ms-sstr+xml')).toBe(false);
     expect(isHlsType('foo')).toBe(false);
