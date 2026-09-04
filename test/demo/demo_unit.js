ad7e7c724340529fc6ebc11da366b712ed6dffce
Álvaro Velad Galván <ladvan91@hotmail.com>
2026-09-04 16:57:38 +0200

    Fix test

--- test/demo/demo_unit.js
+++ test/demo/demo_unit.js
@@ -92,7 +92,8 @@ describe('Demo', () => {
           .add('accessibility.speechToText.languagesToTranslate')
           .add('drm.preferredKeySystems')
           .add('manifest.msf.namespaces')
-          .add('cmcd.eventTargets');
+          .add('cmcd.eventTargets')
+          .add('streaming.prefetchAudioLanguages');
       // We determine whether a config option has been made or not by looking at
       // which config values have been queried (via the fake main object's
       // |getCurrentConfigValue| method).
