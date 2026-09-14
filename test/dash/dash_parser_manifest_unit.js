d78a4dc0dfbe0ac4e3bf283fa7330f914940fa17
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2026-09-14 12:25:57 -0700

    feat(DASH): Tag xlink and linked-MPD requests with advanced request types

--- test/dash/dash_parser_manifest_unit.js
+++ test/dash/dash_parser_manifest_unit.js
@@ -5070,7 +5070,11 @@ describe('DashParser Manifest', () => {
           expect(fakeNetEngine.request).toHaveBeenCalledWith(
               shaka.net.NetworkingEngine.RequestType.MANIFEST,
               jasmine.objectContaining(
-                  {uris: ['https://imported/manifest.mpd']}));
+                  {uris: ['https://imported/manifest.mpd']}),
+              jasmine.objectContaining({
+                type: shaka.net.NetworkingEngine.AdvancedRequestType
+                    .LINKED_MPD,
+              }));
         });
 
         it('two linked periods produce correct total duration', async () => {
